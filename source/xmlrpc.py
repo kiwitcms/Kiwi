e5c81810be41f08db832cb554a359e9e5742cc07
Petr Šplíchal <psplicha@redhat.com>
2014-03-03 13:55:37 +0100

    Use NitrateError class for raising exceptions instead of plain str

--- source/xmlrpc.py
+++ source/xmlrpc.py
@@ -293,7 +293,7 @@ class NitrateXmlrpc(object):
         elif url.startswith('http://'):
             self._transport = CookieTransport()
         else:
-            raise "Unrecognized URL scheme"
+            raise NitrateError("Unrecognized URL scheme")
         
         self._transport.cookiejar = CookieJar()
         # print "COOKIES:", self._transport.cookiejar._cookies
@@ -400,7 +400,7 @@ class NitrateXmlrpc(object):
         If args is empty, then we raise an error.
         """
         if not args:
-            raise NitrateError, "At least one variable must be set."
+            raise NitrateError("At least one variable must be set.")
         return "{%s}" % ''.join(args)
     
     _options_ne_dict = _options_non_empty_dict
@@ -491,9 +491,10 @@ class NitrateKerbXmlrpc(NitrateXmlrpc):
         if url.startswith('https://'):
             self._transport = KerbTransport()
         elif url.startswith('http://'):
-            raise "Apache module mod_ssl is required by mod_auth_kerb for encrypt the communication."
+            raise NitrateError("Encrypted https communication required for "
+                    "Kerberos authentication.\nURL provided: {0}".format(url))
         else:
-            raise "Unrecognized URL scheme"
+            raise NitrateError("Unrecognized URL scheme: {0}".format(url))
         
         self._transport.cookiejar = CookieJar()
         # print "COOKIES:", self._transport.cookiejar._cookies
