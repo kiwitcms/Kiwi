4e95d919b00dfa6eb30b7c9795a8661d9ef935e1
Petr Šplíchal <psplicha@redhat.com>
2014-04-04 09:36:57 +0200

    Updated the create.py simple example code

--- examples/create.py
+++ examples/create.py
@@ -1,95 +1,94 @@
 #!/usr/bin/python
+"""
+Basic Example
 
-from nitrate import *
+Simple example showing the main features of the python-nitrate module.
+Covered are testplan, testrun, testcase creation, logging, setting the
+log level, cache level and color mode, test case linking, attributes
+adjustments and updating the changes to the server.
+"""
+
+import nitrate
+from nitrate import info, log
 
 # Set log level, cache level and color mode
-setLogLevel(log.DEBUG)
-setCacheLevel(CACHE_OBJECTS)
-setColorMode(COLOR_AUTO)
+nitrate.set_log_level(nitrate.LOG_DEBUG)
+nitrate.set_cache_level(nitrate.CACHE_OBJECTS)
+nitrate.set_color_mode(nitrate.COLOR_AUTO)
 
 # Initialize an existing test plan
 info("Initializing an existing test plan")
-generalPlan = TestPlan(3781)
-print generalPlan
+general_plan = nitrate.TestPlan(3781)
+print general_plan
 
 # Logging, catching errors
-setLogLevel(log.INFO)
+nitrate.set_log_level(nitrate.LOG_INFO)
 info("Initializing a bad test plan")
 try:
     log.info("Inspecting the test plan")
-    badPlan = TestPlan(1)
-    print badPlan
-except NitrateError, e:
-    log.error("Bad test plan id ({0})".format(e))
+    bad_plan = nitrate.TestPlan(1)
+    print bad_plan
+except nitrate.NitrateError, error:
+    log.error("Bad test plan id ({0})".format(error))
 
 # Iterate over all test plan's test cases
 info("Inspecting test plan's test cases")
-for testcase in generalPlan:
+for testcase in general_plan:
     print testcase
 
 # Create a new test plan
 info("Creating a new test plan")
-releasePlan = TestPlan(
-        parent=generalPlan,
-        name="python / rhel-6.2",
+release_plan = nitrate.TestPlan(
+        parent=general_plan,
+        name="python / rhel-6.2.0 / ER#11359",
         type="Release",
         product="Red Hat Enterprise Linux 6",
         version="6.2")
 
 # Link all automated test cases from the general plan
 info("Linking automated test cases from the general plan")
-for testcase in generalPlan:
+for testcase in general_plan:
     if testcase.automated:
-        releasePlan.testcases.add(testcase)
-releasePlan.update()
+        release_plan.testcases.add(testcase)
+release_plan.update()
 
 # Create a new test case
 info("Creating a new test case")
-testcase = TestCase(
+testcase = nitrate.TestCase(
         summary="New performance test",
         product="Red Hat Enterprise Linux 6",
         category="Performance",
-        script="/CoreOS/python/Performance/new-test")
+        script="/CoreOS/python/Performance/base")
 
 # Set status, priority, default tester, add tags, attach bugs, link plans
 info("Setting status, priority, tester, tags, bugs and plans")
-testcase.status = CaseStatus("CONFIRMED")
-testcase.priority = Priority("P1")
-testcase.tester = User("psplicha")
-testcase.tags.add(Tag("Tier1"))
-testcase.bugs.add(Bug(123))
-testcase.testplans.add([generalPlan, releasePlan])
+testcase.status = nitrate.CaseStatus("CONFIRMED")
+testcase.priority = nitrate.Priority("P1")
+testcase.tester = nitrate.User("psplicha")
+testcase.tags.add(nitrate.Tag("Tier1"))
+testcase.bugs.add(nitrate.Bug(bug=123))
+testcase.testplans.add([general_plan, release_plan])
 testcase.update()
-
-# Beware of caching (reload objects if necessary)
-info("Test plan synopsis (before update)")
-print generalPlan.synopsis
-print releasePlan.synopsis
-generalPlan = TestPlan(generalPlan.id)
-releasePlan = TestPlan(releasePlan.id)
-info("Test plan synopsis (after update)")
-print generalPlan.synopsis
-print releasePlan.synopsis
 
 # List all general plan's children including their status
 info("Listing child test plans of the general test plan")
-for testplan in TestPlan.search(parent=generalPlan.id):
+for testplan in general_plan.children:
     print testplan, testplan.status
 
 # Create a new test run
 info("Creating a new test run")
-testrun = TestRun(
-        testplan=releasePlan,
+testrun = nitrate.TestRun(
+        testplan=release_plan,
         build="RHEL6.2-20110815.n.1",
         summary="python-2.6.6-20.el6.x86_64 / ER#11359")
 
 # Get script and arguments for all IDLE performance caseruns, move to RUNNING
 info("Scheduling performance tests")
 for caserun in testrun:
-    if (caserun.status == Status("IDLE")
+    if (caserun.status == nitrate.Status("IDLE")
             and str(caserun.testcase.category) == "Performance"):
         print caserun.testcase.script, caserun.testcase.arguments
-        caserun.status = Status("RUNNING")
+        caserun.status = nitrate.Status("RUNNING")
         caserun.update()
 
 # Check case run status of the whole test run
