3fba291b9ae5cd3cea5533889e3d202bf44d8cdc
Petr Šplíchal <psplicha@redhat.com>
2014-04-09 13:43:45 +0200

    Initialize all plan-case tags in CACHE_OBJECTS level

--- source/containers.py
+++ source/containers.py
@@ -707,8 +707,15 @@ class PlanCases(Container):
         # If data initialized from the inset ---> we're done
         if Container._fetch(self, inset):
             return
-        log.info("Fetching {0}'s cases".format(self._identifier))
+        # Initialize all plan-case tags (skip when caching persistently
+        # as this an additional/unnecessary call in that case)
+        if config.get_cache_level() == config.CACHE_OBJECTS:
+            log.info("Fetching tags for all {0}'s test cases".format(
+                self._object.identifier))
+            for tag in self._server.TestPlan.get_all_cases_tags(self.id):
+                Tag(tag)
         # Fetch test cases from the server
+        log.info("Fetching {0}'s cases".format(self._identifier))
         injects = self._server.TestPlan.get_test_cases(self.id)
         log.xmlrpc("Fetched {0}".format(listed(injects, "inject")))
         self._current = set([TestCase(inject) for inject in injects])
