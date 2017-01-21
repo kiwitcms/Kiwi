11f82ac45b7b84218df8c1f00637d2f6f3e018ab
Mr. Senko <atodorov@mrsenko.com>
2017-01-21 18:38:30 +0200

    Don't use RawSQL in migrations

--- tcms/core/migrations/0001_django_comments__object_pk.py
+++ tcms/core/migrations/0001_django_comments__object_pk.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_comments', '0003_add_submit_date_index'),
+        ('django_comments', '__latest__'),
     ]
 
     operations = [
-        migrations.RunSQL('ALTER TABLE django_comments MODIFY object_pk INT NOT NULL;')
+        migrations.AlterField('comment', 'object_pk', models.IntegerField())
     ]
+
+    def __init__(self, name, app_label):
+        super(Migration, self).__init__(name, app_label)
+        self.app_label = 'django_comments'
