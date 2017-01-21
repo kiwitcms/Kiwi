cf711f5af6a8b5d9c0c1e50f17b1591eb8cb2610
Chenxiong Qi <qcxhome@gmail.com>
2017-01-21 17:56:35 +0800

    Fix database name in settings.devel
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/settings/devel.py
+++ tcms/settings/devel.py
@@ -10,7 +10,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nitrate_with_migrations',
+        'NAME': 'nitrate',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '',
