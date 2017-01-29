ff4ecc750364522fc114c2d0939a376c09f6a87f
Chenxiong Qi <qcxhome@gmail.com>
2017-01-30 07:26:16 +0800

    Refine Python package
    Major changes are
    
    * Install configration files into proper place.
    * Add keywords to distribution package.
    * Add missing tcms/locale/ directory.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 from setuptools import setup, find_packages
 
 
+def read_file(filename, mode='r'):
+    with open(filename, mode) as f:
+        for line in f:
+            yield line.strip()
+
+
 def get_version():
-    return open('VERSION.txt', 'r').read().strip('\r\n')
-
-
-def get_package_data():
-    # annoyingly, it appears that package_data has to list filenames; it can't
-    # cope with directories, so we have to figure this out for it:
-    result = {
-        '': [] + list(get_files_below('../templates'))
-               + list(get_files_below('../static'))
-               + list(get_files_below('../docs')),
-    }
-    return result
+    return read_file('VERSION.txt').next()
 
 
 def get_install_requires():
     requires = []
     links = []
-    for line in open('requirements/base.txt', 'r'):
-        line = line.strip()
+    for line in read_file('requirements/base.txt'):
         parts = line.split('#egg=')
         if len(parts) == 2:
             links.append(line)
@@ -35,20 +30,33 @@ def get_install_requires():
 install_requires, dependency_links = get_install_requires()
 
 
+def get_long_description():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+
 setup(
     name='nitrate',
     version=get_version(),
     description='Test Case Management System',
+    long_description=get_long_description(),
     author='Nitrate Team',
     maintainer='Chenxiong Qi',
     maintainer_email='qcxhome@gmail.com',
     url='https://github.com/Nitrate/Nitrate/',
     license='GPLv2+',
-    packages=find_packages(),
-    # package_data=get_package_data(),
+    keywords='test case',
+
     install_requires=install_requires,
     dependency_links=dependency_links,
+
+    packages=find_packages(),
     include_package_data=True,
+    data_files=[
+        ('/etc/httpd/conf.d/', ['contrib/conf/nitrate-httpd.conf']),
+        ('/etc/init.d', ['contrib/script/celeryd']),
+        ],
+
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
@@ -56,5 +64,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
-    ]
+    ],
 )
