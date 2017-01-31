456079fc67f16f3fd1c027c26b51fbc680c3b7b9
Mr. Senko <atodorov@mrsenko.com>
2017-01-31 12:36:35 +0200

    Add tests for `Replace GET_BUG_COUNT SQL with ORM query`
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>
    
    Conflicts:
    	tcms/testruns/tests.py

--- tcms/testruns/tests.py
+++ tcms/testruns/tests.py
@@ -5,12 +5,42 @@ import httplib
 from django import test
 from django.core.urlresolvers import reverse
 
+from tcms.testcases.models import TestCaseBugSystem
 from tcms.testruns.models import TestRun
 from tcms.testruns.models import TestCaseRun
 from tcms.tests import BaseCaseRun
+from tcms.tests.factories import TestRunFactory
 
 
-# ### Test case for View methods ###
+# ### Test cases for models ###
+
+
+class TestRunGetBugsCount(BaseCaseRun):
+    """Test TestRun.get_bug_count"""
+
+    @classmethod
+    def setUpTestData(cls):
+        super(TestRunGetBugsCount, cls).setUpTestData()
+
+        bug_tracker = TestCaseBugSystem.objects.first()
+        cls.empty_test_run = TestRunFactory(product_version=cls.version,
+                                            plan=cls.plan,
+                                            manager=cls.tester,
+                                            default_tester=cls.tester)
+
+        # Add bugs to case runs
+        cls.case_run_1.add_bug('12345', bug_tracker.pk)
+        cls.case_run_1.add_bug('909090', bug_tracker.pk)
+        cls.case_run_3.add_bug('4567890', bug_tracker.pk)
+
+    def test_get_bugs_count_if_no_bugs_added(self):
+        self.assertEqual(0, self.empty_test_run.get_bug_count())
+
+    def test_get_bugs_count(self):
+        self.assertEqual(3, self.test_run.get_bug_count())
+
+
+# ### Test cases for View methods ###
 
 
 class TestOrderCases(BaseCaseRun):
