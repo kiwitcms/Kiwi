1c3493607e5ff7d463fb4335ad19eb0071ff0664
Chenxiong Qi <qcxhome@gmail.com>
2017-02-21 01:03:37 +0800

    Let testcases tests run again
    Test case CaseTests is deleted. At the present time, I have no idea why
    that case was written in that way, and those tests are not clear enough
    to tell what they test. In addition, the assertions don't make sense.
    It is not worth to spend too much time on this case to make them run
    again. So, just remove this test case simply, and write new test cases
    for models, views (only the necessary part of views it's worth to
    test), and other part of testcases app.
    
    Related to #113
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/testcases/tests.py
+++ tcms/testcases/tests.py
@@ -1,164 +1,46 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 
-from django.test.client import Client
 from django.forms import ValidationError
+
 from fields import MultipleEmailField
 
+class TestMultipleEmailField(unittest.TestCase):
 
-class CaseTests(unittest.TestCase):
-    def setUp(self):
-        self.c = Client()
-        self.case_id = 12345
-        self.status_codes = [301, 302]
-
-    def test_cases(self):
-        response = self.c.get('/cases/')
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_new(self):
-        response = self.c.get('/case/new/')
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_clone(self):
-        response = self.c.get('/cases/clone/', {'case': 12197})
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_cases_changestatus(self):
-        response = self.c.get('/cases/changestatus/')
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_cases_priority(self):
-        response = self.c.get('/cases/priority/')
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_getcase(self):
-        location = '/case/%s' % self.case_id
-        response = self.c.get(location)
-        if response.status_code == 301:
-            print response.path
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            assert response.status_code in self.status_codes
-
-    def test_case_details(self):
-        location = '/case/%s/details' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            assert response.status_code in self.status_codes
-        #            self.assertEquals(response.status_code, 302)
-
-    def test_case_edit(self):
-        location = '/case/%s/edit/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_history(self):
-        location = '/case/%s/history/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_changecaseorder(self):
-        location = '/case/%s/changecaseorder/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_attachment(self):
-        location = '/case/%s/attachment/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_log(self):
-        location = '/case/%s/log/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_bug(self):
-        location = '/case/%s/bug/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-    def test_case_plan(self):
-        location = '/case/%s/plan/' % self.case_id
-        response = self.c.get(location)
-        try:
-            self.assertEquals(response.status_code, 200)
-        except AssertionError:
-            self.assertEquals(response.status_code, 302)
-
-
-class Test_MultipleEmailField(unittest.TestCase):
-    def setUp(self):
-        self.default_delimiter = ','
-        self.field = MultipleEmailField(delimiter=self.default_delimiter)
-
-        self.all_valid_emails = (
-            'cqi@redhat.com', 'cqi@yahoo.com', 'chen@gmail.com', )
-        self.include_invalid_emails = (
-            '', ' cqi@redhat.com', 'chen@sina.com', )
+    @classmethod
+    def setUpClass(cls):
+        super(TestMultipleEmailField, cls).setUpClass()
+        cls.default_delimiter = ','
+        cls.field = MultipleEmailField(delimiter=cls.default_delimiter)
 
     def test_to_python(self):
-        value = 'cqi@redhat.com'
+        value = u'zhangsan@localhost'
         pyobj = self.field.to_python(value)
-        self.assertEqual(pyobj, ['cqi@redhat.com'])
+        self.assertEqual(pyobj, [value])
 
-        value = 'cqi@redhat.com,,cqi@gmail.com,'
+        value = u'zhangsan@localhost,,lisi@example.com,'
         pyobj = self.field.to_python(value)
-        self.assertEqual(pyobj, ['cqi@redhat.com', 'cqi@gmail.com'])
+        self.assertEqual(pyobj, [u'zhangsan@localhost', u'lisi@example.com'])
 
         for value in ('', None, []):
             pyobj = self.field.to_python(value)
             self.assertEqual(pyobj, [])
 
     def test_clean(self):
-        value = 'cqi@redhat.com'
+        value = u'zhangsan@localhost'
         data = self.field.clean(value)
-        self.assertEqual(data, ['cqi@redhat.com'])
+        self.assertEqual(data, [value])
 
-        value = 'cqi@redhat.com,cqi@gmail.com'
+        value = u'zhangsan@localhost,lisi@example.com'
         data = self.field.clean(value)
-        self.assertEqual(data, ['cqi@redhat.com', 'cqi@gmail.com'])
+        self.assertEqual(data, [u'zhangsan@localhost', u'lisi@example.com'])
 
-        value = ',cqi@redhat.com, ,cqi@gmail.com, \n'
+        value = u',zhangsan@localhost, ,lisi@example.com, \n'
         data = self.field.clean(value)
-        self.assertEqual(data, ['cqi@redhat.com', 'cqi@gmail.com'])
+        self.assertEqual(data, [u'zhangsan@localhost', 'lisi@example.com'])
 
-        value = ',cqi,cqi@redhat.com, \n,cqi@gmail.com, '
+        value = ',zhangsan,zhangsan@localhost, \n,lisi@example.com, '
         self.assertRaises(ValidationError, self.field.clean, value)
 
         value = ''
@@ -169,7 +51,3 @@ class Test_MultipleEmailField(unittest.TestCase):
         self.field.required = False
         data = self.field.clean(value)
         self.assertEqual(data, [])
-
-
-if __name__ == '__main__':
-    unittest.main()
