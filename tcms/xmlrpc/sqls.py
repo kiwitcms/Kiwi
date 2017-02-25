b63cfc3fd4d91870e41f0de3afb1b2ab5de31e8a
Mr. Senko <atodorov@mrsenko.com>
2017-02-26 00:28:03 +0200

    Refactor TP_CLEAR_ENV_GROUP and TP_ADD_ENV_GROUP SQLs into ORM
    also the test added in the previous commit was generating an error
    which is now fixed. The error was:
    
    'module' object has no attribute 'commit_unless_managed'
    
    Signed-off-by: Mr. Senko <atodorov@mrsenko.com>

--- tcms/xmlrpc/sqls.py
+++ tcms/xmlrpc/sqls.py
@@ -1,11 +1,3 @@
-TP_CLEAR_ENV_GROUP = '''
-DELETE FROM `tcms_env_plan_map` where plan_id in (%s)
-'''
-
-TP_ADD_ENV_GROUP = '''
-INSERT INTO `tcms_env_plan_map` (`plan_id`, `group_id`) values %s
-'''
-
 TC_REMOVE_CC = '''
 DELETE contact
 FROM `testcases_testcaseemailsettings` eset
