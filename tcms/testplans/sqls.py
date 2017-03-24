ae6930d2449a4c5f964e1fb84ce080cb1d26275a
Mr. Senko <atodorov@mrsenko.com>
2017-03-24 23:57:52 +0200

    Refactor TP_PRINTABLE_CASE_TEXTS to ORM and improve tests
    this commit improves the existing test by actually asserting on
    a few more values. It also adds a test case when no plans were
    selected.
    
    Then it removes the SQL statement and replaces it with ORM query
    making sure to filter out earlier versions of the test case text!

--- tcms/testplans/sqls.py
+++ tcms/testplans/sqls.py
@@ -1,34 +1,4 @@
 # -*- coding: utf-8 -*-
-TP_PRINTABLE_CASE_TEXTS = '''
-SELECT t3.plan_id,
-       test_cases.case_id,
-       test_cases.summary,
-       test_case_texts.setup,
-       test_case_texts.action,
-       test_case_texts.effect,
-       test_case_texts.breakdown
-FROM   test_cases
-       INNER JOIN test_case_plans
-               ON ( test_case_plans.case_id = test_cases.case_id )
-       INNER JOIN test_case_texts
-               ON ( test_cases.case_id = test_case_texts.case_id )
-       INNER JOIN (SELECT t5.plan_id,
-                          t4.case_id,
-                          Max(t4.case_text_version) AS max_version
-                   FROM   test_case_texts t4
-                          INNER JOIN test_case_plans t5
-                                  ON ( t5.case_id = t4.case_id )
-                          INNER JOIN test_cases t6
-                                  ON ( t5.case_id = t6.case_id )
-                   WHERE  t5.plan_id IN (%s)
-                          AND t6.case_status_id IN (1,2,4)
-                   GROUP  BY t4.case_id, t5.plan_id) t3
-               ON ( test_case_texts.case_id = t3.case_id
-                    AND test_case_texts.case_text_version = t3.max_version
-                    AND test_case_plans.plan_id = t3.plan_id )
-WHERE test_case_plans.plan_id IN (%s)
-'''
-
 TP_EXPORT_ALL_CASES_META = '''
 SELECT `test_cases`.`case_id`,
        `test_cases`.`summary`,
