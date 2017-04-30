899c9510afb4118af86cdb1dc4bd3d3abee63622
Mr. Senko <atodorov@mrsenko.com>
2017-04-30 22:19:00 +0300

    Delete TestPlanPermissions model
    this model is not used anywhere in the code so remove it!
    
    Signed-off-by: Mr. Senko <atodorov@mrsenko.com>

---
+++ tcms/testplans/migrations/0003_delete_testplanpermissions_model.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def delete_stale_content_type(apps, schema_editor):
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    ContentType.objects.filter(model='testplanpermission').delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('testplans', '0002_add_initial_data'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='testplanpermission',
+            unique_together=set([]),
+        ),
+        migrations.RemoveField(
+            model_name='testplanpermission',
+            name='plan',
+        ),
+        migrations.DeleteModel(
+            name='TestPlanPermission',
+        ),
+        migrations.RunPython(
+            delete_stale_content_type
+        ),
+    ]
