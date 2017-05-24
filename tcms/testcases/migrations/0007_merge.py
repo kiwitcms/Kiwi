9ae9cba3d70fd9c90fbfd6cf72b37788f55e0f95
Mr. Senko <atodorov@mrsenko.com>
2017-05-24 12:17:56 +0300

    Merge migrations

---
+++ tcms/testcases/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('testcases', '0006_add_bugtracker_integration_fields'),
+        ('testcases', '0005_allow_null_to_testcaseattachment_case_run'),
+    ]
+
+    operations = [
+    ]
