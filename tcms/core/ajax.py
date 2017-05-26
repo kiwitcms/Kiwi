Nitrate/Nitrate

Simplify strip_parameters() function and add tests for it
this makes it easier to fix the usage of deprecated request.REQUEST
later

Signed-off-by: Mr. Senko <atodorov@mrsenko.com>

tcms/core/ajax.py
@@ -47,12 +47,15 @@ def check_permission(request, ctype):
     return False
 
 
-def strip_parameters(request, skip_parameters):
+def strip_parameters(request_dict, skip_parameters):
+    """
+    Helper method which will remove the dict items listed in skip_parameters
+    @return - dict
+    """
     parameters = {}
-    for dict_ in request.REQUEST.dicts:
-        for k, v in dict_.items():
-            if k not in skip_parameters and v:
-                parameters[str(k)] = v
+    for k, v in request_dict.items():
+        if k not in skip_parameters and v:
+            parameters[str(k)] = v
 
     return parameters
 
@@ -120,7 +123,7 @@ def info(request):
             )
 
         def tags(self):
-            query = strip_parameters(request, self.internal_parameters)
+            query = strip_parameters(request.GET, self.internal_parameters)
             tags = TestCaseTag.objects
             # Generate the string combination, because we are using
             # case sensitive table
@@ -137,7 +140,7 @@ def info(request):
         def users(self):
             from django.contrib.auth.models import User
 
-            query = strip_parameters(self.request, self.internal_parameters)
+            query = strip_parameters(self.request.GET, self.internal_parameters)
             return User.objects.filter(**query)
 
         def versions(self):
@@ -172,7 +175,7 @@ def form(request):
 
     # The parameters in internal_parameters will delete from parameters
     internal_parameters = ['app_form', 'format']
-    parameters = strip_parameters(request, internal_parameters)
+    parameters = strip_parameters(request.REQUEST, internal_parameters)
     q_app_form = request.REQUEST.get('app_form')
     q_format = request.REQUEST.get('format')
     if not q_format:

tcms/core/tests/test_ajax.py
@@ -1,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from tcms.core.ajax import strip_parameters
+
+
+class Test_strip_parameters(unittest.TestCase):
+    def setUp(self):
+        self.request_dict = {
+            'name__startswith': 'something',
+            'info_type': 'tags',
+            'format': 'ulli',
+            'case__plan': 1,
+            'field': 'tag__name',
+        }
+        self.internal_parameters = ('info_type', 'field', 'format')
+
+    def test_remove_parameters_in_dict(self):
+        simplified_dict = strip_parameters(self.request_dict, self.internal_parameters)
+        for p in self.internal_parameters:
+            self.assertFalse(p in simplified_dict)
+
+        self.assertEqual('something', simplified_dict['name__startswith'])
+        self.assertEqual(1, simplified_dict['case__plan'])
+
+    def test_remove_parameters_not_in_dict(self):
+        simplified_dict = strip_parameters(self.request_dict, ['non-existing-parameter'])
+        self.assertEqual(self.request_dict, simplified_dict)

kiwitcms/Kiwi

tcms/core/ajax.py
@@ -48,12 +48,15 @@ def check_permission(request, ctype):
     return False
 
 
-def strip_parameters(request, skip_parameters):
+def strip_parameters(request_data, skip_parameters):
+    """
+    Helper method which will remove the dict items listed in skip_parameters
+    @return - dict
+    """
     parameters = {}
-    for dict_ in request.REQUEST.dicts:
-        for k, v in dict_.items():
-            if k not in skip_parameters and v:
-                parameters[str(k)] = v
+    for k, v in request_data.items():
+        if k not in skip_parameters and v:
+            parameters[str(k)] = v
 
     return parameters
 
@@ -121,7 +124,7 @@ def info(request):
             )
 
         def tags(self):
-            query = strip_parameters(request, self.internal_parameters)
+            query = strip_parameters(request.GET, self.internal_parameters)
             tags = TestTag.objects
             # Generate the string combination, because we are using
             # case sensitive table
@@ -138,7 +141,7 @@ def info(request):
         def users(self):
             from django.contrib.auth.models import User
 
-            query = strip_parameters(self.request, self.internal_parameters)
+            query = strip_parameters(self.request.GET, self.internal_parameters)
             return User.objects.filter(**query)
 
         def versions(self):
@@ -174,7 +177,7 @@ def form(request):
 
     # The parameters in internal_parameters will delete from parameters
     internal_parameters = ['app_form', 'format']
-    parameters = strip_parameters(request, internal_parameters)
+    parameters = strip_parameters(request.GET, internal_parameters)
     q_app_form = request.GET.get('app_form')
     q_format = request.GET.get('format')
     if not q_format:

tcms/core/tests/test_ajax.py
@@ -1,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from tcms.core.ajax import strip_parameters
+
+
+class TestStripParameters(unittest.TestCase):
+
+    def setUp(self):
+        self.request_dict = {
+            'name__startswith': 'something',
+            'info_type': 'tags',
+            'format': 'ulli',
+            'case__plan': 1,
+            'field': 'tag__name',
+        }
+        self.internal_parameters = ('info_type', 'field', 'format')
+
+    def test_remove_parameters_in_dict(self):
+        simplified_dict = strip_parameters(self.request_dict, self.internal_parameters)
+        for p in self.internal_parameters:
+            self.assertFalse(p in simplified_dict)
+
+        self.assertEqual('something', simplified_dict['name__startswith'])
+        self.assertEqual(1, simplified_dict['case__plan'])
+
+    def test_remove_parameters_not_in_dict(self):
+        simplified_dict = strip_parameters(self.request_dict, ['non-existing-parameter'])
+        self.assertEqual(self.request_dict, simplified_dict)