21fbafccee6f2751840811ce0051bfad794b9f30
Chenxiong Qi <qcxhome@gmail.com>
2017-05-27 21:08:57 +0800

    Replace REQUEST with GET in testruns.views.get
    Tests are also added.
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/testruns/tests.py
+++ tcms/testruns/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import httplib
+from six.moves import http_client
 
 from django import test
 from django.core.urlresolvers import reverse
@@ -26,7 +26,7 @@ class TestOrderCases(BaseCaseRun):
         nonexisting_run_pk = TestRun.objects.count() + 1
         url = reverse('tcms.testruns.views.order_case', args=[nonexisting_run_pk])
         response = self.client.get(url)
-        self.assertEqual(httplib.NOT_FOUND, response.status_code)
+        self.assertEqual(http_client.NOT_FOUND, response.status_code)
 
     def test_prompt_if_no_case_run_is_passed(self):
         url = reverse('tcms.testruns.views.order_case', args=[self.test_run.pk])
@@ -48,3 +48,33 @@ class TestOrderCases(BaseCaseRun):
             TestCaseRun.objects.get(pk=self.case_run_3.pk).sortkey,
         ]
         self.assertEqual([10, 20, 30], test_sortkeys)
+
+
+class TestGetRun(BaseCaseRun):
+    """Test get view method"""
+
+    @classmethod
+    def setUpTestData(cls):
+        super(TestGetRun, cls).setUpTestData()
+
+    def test_404_if_non_existing_pk(self):
+        url = reverse('tcms.testruns.views.get', args=[99999999])
+        response = self.client.get(url)
+        self.assertEqual(http_client.NOT_FOUND, response.status_code)
+
+    def test_get_a_run(self):
+        url = reverse('tcms.testruns.views.get', args=[self.test_run.pk])
+        response = self.client.get(url)
+
+        self.assertEqual(http_client.OK, response.status_code)
+
+        for case_run in (self.case_run_1, self.case_run_2, self.case_run_3):
+            self.assertContains(
+                response,
+                '<a href="#caserun_{0}">#{0}</a>'.format(case_run.pk),
+                html=True)
+            self.assertContains(
+                response,
+                '<a id="link_{0}" href="#caserun_{0}" title="Expand test case">'
+                '{1}</a>'.format(case_run.pk, case_run.case.summary),
+                html=True)
--- tcms/testruns/views.py
+++ tcms/testruns/views.py
@@ -23,6 +23,7 @@ from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_GET
 from django.views.generic.base import TemplateView
 from django.views.generic.base import View
 
@@ -669,7 +670,7 @@ def open_run_get_case_runs(request, run):
     # Continue to search the case runs with conditions
     # 4. case runs preparing for render case runs table
     tcrs = tcrs.filter(**clean_request(request))
-    order_by = request.REQUEST.get('order_by')
+    order_by = request.GET.get('order_by')
     if order_by:
         tcrs = tcrs.order_by(order_by)
     else:
@@ -704,8 +705,10 @@ def open_run_get_users(case_runs):
     return (dict(testers.iterator()), dict(assignees.iterator()))
 
 
+@require_GET
 def get(request, run_id, template_name='run/get.html'):
-    '''Display testrun's detail'''
+    """Display testrun's detail"""
+
     SUB_MODULE_NAME = "runs"
 
     # Get the test run
@@ -739,7 +742,7 @@ def get(request, run_id, template_name='run/get.html'):
     ttags.sort()
 
     def walk_case_runs():
-        '''Walking case runs for helping rendering case runs table'''
+        """Walking case runs for helping rendering case runs table"""
         priorities = Priority.get_values()
         testers, assignees = open_run_get_users(tcrs)
         comments_subtotal = open_run_get_comments_subtotal(
