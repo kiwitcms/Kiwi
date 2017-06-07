b7d360193e96ba040069731a811906ac731e8aef
Mr. Senko <atodorov@mrsenko.com>
2017-06-07 11:16:23 +0300

    Refactoring of BugzillaThread integration

--- tcms/issuetracker/bugzilla_integration.py
+++ tcms/issuetracker/bugzilla_integration.py
@@ -9,30 +9,6 @@ import threading
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-__all__ = ['BugzillaThread']
-
-
-def _rpc_add_bug_to_bugzilla(rpc, testcase, bug):
-    """
-        Using Bugzilla's XML-RPC try to link the test case with
-        the bug!
-
-        @rpc - Bugzilla XML-RPC object
-        @testcase - TestCase object
-        @bug - TestCaseBug object
-    """
-
-    try:
-        text = """---- Bug confirmed via test case ----
-URL: %s
-Summary: %s""" % (settings.KIWI_BASE_URL + reverse('testcases-get', args=[testcase.pk]),
-                  testcase.summary)
-
-        rpc.update_bugs(bug.bug_id, {'comment': {'comment': text, 'is_private': False}})
-    except Exception, err:
-        message = '%s: %s' % (err.__class__.__name__, err)
-        warnings.warn(message)
-
 
 class BugzillaThread(threading.Thread):
     """
@@ -42,10 +18,30 @@ class BugzillaThread(threading.Thread):
     """
 
     def __init__(self, rpc, testcase, bug):
+        """
+            @rpc - Bugzilla XML-RPC object
+            @testcase - TestCase object
+            @bug - TestCaseBug object
+        """
+
         self.rpc = rpc
         self.testcase = testcase
         self.bug = bug
-        threading.Thread.__init__(self)
+        super(BugzillaThread, self).__init__()
 
     def run(self):
-        _rpc_add_bug_to_bugzilla(self.rpc, self.testcase, self.bug)
+        """
+            Using Bugzilla's XML-RPC try to link the test case with
+            the bug!
+        """
+
+        try:
+            text = """---- Bug confirmed via test case ----
+URL: %s
+Summary: %s""" % (settings.KIWI_BASE_URL + reverse('testcases-get', args=[self.testcase.pk]),
+                  self.testcase.summary)
+
+            self.rpc.update_bugs(self.bug.bug_id, {'comment': {'comment': text, 'is_private': False}})
+        except Exception, err:
+            message = '%s: %s' % (err.__class__.__name__, err)
+            warnings.warn(message)
