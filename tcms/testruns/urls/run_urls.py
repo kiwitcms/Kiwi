6f58d672be884167505315d3e0ca40b1a9a51729
Chenxiong Qi <qcxhome@gmail.com>
2017-06-18 19:44:35 +0800

    Replace REQUEST with POST in testrun update_case_run_text
    Relative to #173
    
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/testruns/urls/run_urls.py
+++ tcms/testruns/urls/run_urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
         name='add-cases-to-run'),
 
     url(r'^(?P<run_id>\d+)/cc/$', views.cc, name='testruns-cc'),
-    url(r'^(?P<run_id>\d+)/update/$', views.update_case_run_text),
+    url(r'^(?P<run_id>\d+)/update/$', views.update_case_run_text,
+        name='testruns-update_case_run_text'),
     url(r'^(?P<run_id>\d+)/export/$', views.export, name='testruns-export'),
 ]
