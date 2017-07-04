d732dad775431d0243a0f3193565b333be35a7fb
Chenxiong Qi <qcxhome@gmail.com>
2017-07-04 12:46:10 +0800

    Remove unused obj2dict.py
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- tcms/core/utils/obj2dict.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def obj2dict(obj):
-    memberlist = [m for m in dir(obj)]
-    _dict = {}
-    for m in memberlist:
-        if m[0] != "_" and not callable(m):
-            _dict[m] = getattr(obj, m)
-
-    return _dict
