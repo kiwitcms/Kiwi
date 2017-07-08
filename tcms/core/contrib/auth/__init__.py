484c46d0f5f4e107b18c095c420b31a9f2696ea1
Mr. Senko <atodorov@mrsenko.com>
2017-07-08 23:05:12 +0300

    Move initiate_user_with_default_setups to auth.backends
    this avoids premature loading of django.contrib.auth.models
    from tcms.core.contrib.auth.__init__ which isn't allowed in
    Django 1.9

--- tcms/core/contrib/auth/__init__.py
+++ tcms/core/contrib/auth/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.contrib.auth.models import Group
 from importlib import import_module
 
 
@@ -26,15 +25,3 @@ def get_backend(path):
 
 def get_using_backend():
     return get_backend(settings.AUTHENTICATION_BACKENDS[0])
-
-
-def initiate_user_with_default_setups(user):
-    '''
-    Add default groups, permissions, status to a newly
-    created user.
-    '''
-    default_groups = Group.objects.filter(name__in=settings.DEFAULT_GROUPS)
-    user.is_active = True
-    for grp in default_groups:
-        user.groups.add(grp)
-    user.save()
