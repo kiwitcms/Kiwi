704b675e543a47da08573431d77febc77e479eb7
Mr. Senko <atodorov@mrsenko.com>
2017-07-26 23:35:38 +0300

    Replace TC_EXPORT_ALL_CASES_COMPONENTS with ORM

--- tcms/utils/dict_utils.py
+++ tcms/utils/dict_utils.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-import itertools
-
-from tcms.core.db import SQLExecution
-
-
-def create_group_by_dict(sql, params, key_func):
-    '''
-
-    @param sql: the SQL query to execute
-    @type sql: basestring
-    @param params: parameters to bind with SQL
-    @type params: list or tuple
-    @param key_func: key function
-    @type key_func: callable object
-    @return: dict with SQL query results
-    @:rtype: dict
-    '''
-    result_set = SQLExecution(sql, params)
-    group_data = itertools.groupby(result_set.rows, key_func)
-    return dict((key, [v for v in values]) for key, values in group_data)
 
 
 def create_dict_from_query(query, key_field, skip_others=False):
