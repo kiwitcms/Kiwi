e15a929a9879ca1adbe84c740516e648a2edcf0d
Mr. Senko <atodorov@mrsenko.com>
2017-07-26 23:07:05 +0300

    Move common code into utils function

--- tcms/utils/dict_utils.py
+++ tcms/utils/dict_utils.py
@@ -19,3 +19,24 @@ def create_group_by_dict(sql, params, key_func):
     result_set = SQLExecution(sql, params)
     group_data = itertools.groupby(result_set.rows, key_func)
     return dict((key, [v for v in values]) for key, values in group_data)
+
+def create_dict_from_query(query, key_field):
+    """
+        Group values based on a particular field.
+
+        @param query: Django values() query, ordered by key_field
+        @param key_field: field name by which to grup
+
+        @return: dict where keys are key_field values and
+                 values are a list of the query records.
+    """
+    result_dict = {}
+    for record in query:
+        key_value = record[key_field]
+
+        if key_value in result_dict:
+            result_dict[key_value].append(record)
+        else:
+            result_dict[key_value] = [record]
+
+    return result_dict
