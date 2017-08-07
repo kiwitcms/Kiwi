d482275903df962e559d10fc6af9073d5b85e2a3
Mr. Senko <atodorov@mrsenko.com>
2017-08-07 12:48:22 +0300

    Migrate from MIDDLEWARE_CLASSES to MIDDLEWARE
    and temporarily convert existing middleware classes to
    Django's new-style middleware

--- tcms/core/middleware.py
+++ tcms/core/middleware.py
@@ -1,6 +1,15 @@
-# -*- coding: utf-8 -*-
+from django.utils.deprecation import MiddlewareMixin
+from pagination.middleware import PaginationMiddleware
 
 
-class CsrfDisableMiddleware(object):
+class MyPaginationMiddleware(MiddlewareMixin, PaginationMiddleware):
+    """
+        New-style middleware for Django 1.10 until we drop
+        django-pagination
+    """
+    pass
+
+
+class CsrfDisableMiddleware(MiddlewareMixin):
     def process_view(self, request, callback, callback_args, callback_kwargs):
         setattr(request, '_dont_enforce_csrf_checks', True)
