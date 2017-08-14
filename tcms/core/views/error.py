8dcb108cc65437dc172d5d3b09d63445c81b699f
Mr. Senko <atodorov@mrsenko.com>
2017-08-14 18:12:22 +0300

    Don't use deprecated RequestContext in 500 view

--- tcms/core/views/error.py
+++ tcms/core/views/error.py
@@ -1,5 +1,5 @@
 from django import http
-from django.template import (RequestContext, loader, TemplateDoesNotExist)
+from django.template import loader, TemplateDoesNotExist
 from django.views.decorators.csrf import requires_csrf_token
 
 
@@ -16,4 +16,4 @@ def server_error(request, template_name='500.html'):
     except TemplateDoesNotExist:
         return http.HttpResponseServerError('<h1>Server Error (500)</h1>')
     return http.HttpResponseServerError(
-        template.render(RequestContext(request)))
+        template.render({}, request))
