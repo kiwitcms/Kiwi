2b5e256747543634ea47672a94f81b01dfea0065
Mr. Senko <atodorov@mrsenko.com>
2017-08-30 14:58:48 +0300

    Set MySQL mode to STRICT_TRANS_TABLES

--- tcms/settings/test/mysql.py
+++ tcms/settings/test/mysql.py
@@ -6,4 +6,7 @@ DATABASES['default'].update({
     'USER': 'kiwi',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
 })
