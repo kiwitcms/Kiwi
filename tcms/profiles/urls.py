65da1be4cd308181fcdc762261792e62171a53d2
Mr. Senko <atodorov@mrsenko.com>
2017-09-20 16:22:28 +0300

    Add missing templates and view for password reset functionality

--- tcms/profiles/urls.py
+++ tcms/profiles/urls.py
@@ -16,20 +16,22 @@ urlpatterns = [
     url(r'^(?P<username>[\w.@+-]+)/recent/$', profiles_views.recent,
         name='tcms-recent'),
 
-    url(r'logout/$',
+    url(r'^logout/$',
         contrib_auth_views.LogoutView.as_view(
             next_page=reverse_lazy('core-views-index')),
         name='tcms-logout'),
-    url(r'register/$', auth_views.register, name='tcms-register'),
-    url(r'confirm/(?P<activation_key>[A-Za-z0-9\-]+)/$', auth_views.confirm,
+    url(r'^register/$', auth_views.register, name='tcms-register'),
+    url(r'^confirm/(?P<activation_key>[A-Za-z0-9\-]+)/$', auth_views.confirm,
         name='tcms-confirm'),
 
-    url(r'login/$', contrib_auth_views.LoginView.as_view(), name='tcms-login'),
-    url(r'changepassword/$', contrib_auth_views.PasswordChangeView.as_view(), name='tcms-password_change'),
-    url(r'changepassword/done/$', contrib_auth_views.PasswordChangeDoneView.as_view(),
+    url(r'^login/$', contrib_auth_views.LoginView.as_view(), name='tcms-login'),
+    url(r'^changepassword/$', contrib_auth_views.PasswordChangeView.as_view(), name='tcms-password_change'),
+    url(r'^changepassword/done/$', contrib_auth_views.PasswordChangeDoneView.as_view(),
         name='password_change_done'),
     url(r'^passwordreset/$', contrib_auth_views.PasswordResetView.as_view(), name='tcms-password_reset'),
     url(r'^passwordreset/done/$', contrib_auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^passwordreset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         contrib_auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^passwordreset/complete/$',
+        contrib_auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
