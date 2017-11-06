d275b23373d9b12d32758add9741d06ee403a914
Mr. Senko <atodorov@mrsenko.com>
2017-11-06 22:26:51 +0200

    Remove tcms.core.utils.test_template
    it doesn't make a lot of sense to have a view that renders a
    template without its required context.

--- tcms/core/utils/test_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render
-
-
-def test_template(request, template_name):
-    """
-    Test the template appearence
-    """
-    return render(request, template_name)
--- tcms/urls.py
+++ tcms/urls.py
@@ -111,11 +111,9 @@ urlpatterns = [
 if settings.DEBUG:
     try:
         import debug_toolbar
-        from tcms.core.utils import test_template
 
         urlpatterns += [
             url(r'^__debug__/', include(debug_toolbar.urls)),
-            url(r'^tt/(?P<template_name>.*)', test_template.test_template),
         ]
     # in case we're trying to debug in production
     # and debug_toolbar is not installed
