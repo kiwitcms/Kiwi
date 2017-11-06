cfdbb27a336578477f5cd20d16a240e4c8f5593a
Mr. Senko <atodorov@mrsenko.com>
2017-11-06 22:00:43 +0200

    Use TestRun.stats_caseruns_status() for progress bar on dashboard
    this commit removes the custom calculation of the progress bar
    for the home page dashboard and reuses the stats method and template
    that we made earlier.
    
    Also removes a leftover RawSQL query.

--- tcms/core/utils/raw_sql.py
+++ tcms/core/utils/raw_sql.py
@@ -25,31 +25,6 @@ class RawSQL:
         WHERE test_case_bugs.case_run_id = test_case_runs.case_run_id'
 
     # Following SQL use for test case run
-    completed_case_run_percent = 'SELECT ROUND(no_idle_count/total_count*100,0) \
-        FROM ( \
-            SELECT \
-                tr1.run_id AS run_id, \
-                count(tcr1.case_run_id) AS no_idle_count \
-            FROM test_runs tr1 \
-            LEFT JOIN test_case_runs tcr1 \
-            ON tr1.run_id=tcr1.run_id \
-            WHERE tcr1.case_run_status_id \
-            NOT IN(1,4,5,6) \
-            GROUP BY tr1.run_id \
-            ORDER BY tr1.run_id \
-        ) AS table1, ( \
-            SELECT \
-                tr2.run_id AS run_id, \
-                count(tcr2.case_run_id) AS total_count \
-            FROM test_runs tr2 \
-            LEFT JOIN test_case_runs tcr2 \
-            ON tr2.run_id=tcr2.run_id \
-            GROUP BY tr2.run_id \
-            ORDER BY tr2.run_id \
-        ) AS table2 \
-        WHERE table1.run_id=table2.run_id \
-        AND table1.run_id=test_runs.run_id'
-
     total_num_caseruns = 'SELECT COUNT(*) \
         FROM test_case_runs \
         WHERE test_case_runs.run_id = test_runs.run_id'
