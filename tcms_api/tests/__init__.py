454e2e685ce0d8e7c1e7317e4953ef3c3fa47c0e
Mr. Senko <atodorov@mrsenko.com>
2017-11-16 23:53:58 +0200

    Finally figure out how to create API test data properly
    - mark the test class for serialized rollback
    - prepare data in the private *_fixture_setup* method so that
      we can actually access the default groups and permissions
    - reset caches and config attributes

--- tcms_api/tests/__init__.py
+++ tcms_api/tests/__init__.py
@@ -1,12 +1,14 @@
 import os
 import random
 
+from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 import tcms_api
 
 from tcms.tests.factories import UserFactory
 from tcms.tests.factories import ProductFactory
+from tcms.tests.factories import VersionFactory
 from tcms.tests.factories import TestTagFactory
 from tcms.tests.factories import TestCaseCategoryFactory
 
@@ -19,95 +21,124 @@ class BaseAPIClient_TestCase(StaticLiveServerTestCase):
         prepare test data - create Test Cases, Test Plans and Test Runs
     """
 
+    # reload data which came from migrations like standard
+    # groups Admin and Tester
+    serialized_rollback = True
+
     # how many test objects to create
     num_cases = 1
     num_plans = 1
     num_runs = 1
 
-    @classmethod
-    def setUpClass(cls):
-        super(StaticLiveServerTestCase, cls).setUpClass()
+    def setUp(self):
+        # disable caching for every test to avoid hard to debug issues
+        # tests that need caching will enable it before they do
+        # anything else
+        tcms_api.config.set_cache_level(tcms_api.config.CACHE_NONE)
 
-        cls.api_user = UserFactory(username='api-tester')
-        cls.api_user.set_password('testing')
-        initiate_user_with_default_setups(cls.api_user)
+    # NOTE: we setup the required DB data and API objects here
+    # because this method is executed *AFTER* setUpClass() and the
+    # serialized rollback is not yet available during setUpClass()
+    # execution
+    def _fixture_setup(self):
+        # restore the serialized data from initial migrations
+        # this includes default groups and permissions
+        super(BaseAPIClient_TestCase, self)._fixture_setup()
+
+        # initial cache reset to avoid storing anything in cache
+        tcms_api.config.set_cache_level(tcms_api.config.CACHE_NONE)
+
+        # reset connection to server b/c the address changes for
+        # every test and the client caches this as a class attribute
+        tcms_api.Nitrate._connection = None
+        # also the config is a singleton so reset that too
+        # to force config reload
+        tcms_api.Config._instance = None
+
+        # API user
+        self.api_user, _ = User.objects.get_or_create(username='api-tester')
+        self.api_user.set_password('testing')
+        initiate_user_with_default_setups(self.api_user)
 
         # WARNING: for now we override the config file
         # until we can pass the testing configuration
+        # TODO: change config values instead of overwriting files on disk
         conf_path = os.path.expanduser('~/.tcms.conf')
         conf_fh = open(conf_path, 'w')
         conf_fh.write("""[nitrate]
 url = %s/xmlrpc/
 username = %s
 password = %s
-""" % (cls.live_server_url, cls.api_user.username, 'testing'))
+""" % (self.live_server_url, self.api_user.username, 'testing'))
         conf_fh.close()
 
         # create the product first so we can fetch it via API
-        f_product = ProductFactory(name="Kiwi TCMS")
-        cls.product = tcms_api.Product(name="Kiwi TCMS")
-        cls.version = tcms_api.Version(product=cls.product, version="unspecified")
-        cls.plantype = tcms_api.PlanType(name="Function")
+        f_product = ProductFactory()
+        self.product = tcms_api.Product(name=f_product.name)
+        f_version = VersionFactory(product=f_product)
+        self.version = tcms_api.Version(product=self.product, version=f_version.value)
+        self.plantype = tcms_api.PlanType(name="Function")
 
-        TestCaseCategoryFactory(name="Regression", product=f_product)
-        cls.category = tcms_api.Category(category="Regression", product=cls.product)
-        cls.component = tcms_api.Component(name='tcms_api', product=cls.product)
-        cls.CASESTATUS = tcms_api.CaseStatus("CONFIRMED")
-        cls.build = tcms_api.Build(product=cls.product, build="unspecified")
+        f_category = TestCaseCategoryFactory(product=f_product)
+        self.category = tcms_api.Category(category=f_category.name, product=self.product)
+
+        self.component = tcms_api.Component(name='tcms_api', product=self.product)
+        self.CASESTATUS = tcms_api.CaseStatus("CONFIRMED")
+        self.build = tcms_api.Build(product=self.product, build="unspecified")
 
         f_tags = [TestTagFactory() for i in range(20)]
-        cls.tags = [tcms_api.Tag(t.pk) for t in f_tags]
+        self.tags = [tcms_api.Tag(t.pk) for t in f_tags]
 
         f_users = [UserFactory() for i in range(50)]
-        cls.TESTERS = [tcms_api.User(u.pk) for u in f_users]
+        self.TESTERS = [tcms_api.User(u.pk) for u in f_users]
 
         # Create test cases
-        cls.cases = []
-        for case_count in range(cls.num_cases):
+        self.cases = []
+        for case_count in range(self.num_cases):
             testcase = tcms_api.TestCase(
-                category=cls.category,
-                product=cls.product,
+                category=self.category,
+                product=self.product,
                 summary="Test Case {0}".format(case_count + 1),
-                status=cls.CASESTATUS)
+                status=self.CASESTATUS)
             # Add a couple of random tags and the default tester
-            testcase.tags.add([random.choice(cls.tags) for counter in range(10)])
-            testcase.tester = random.choice(cls.TESTERS)
+            testcase.tags.add([random.choice(self.tags) for counter in range(10)])
+            testcase.tester = random.choice(self.TESTERS)
             testcase.update()
-            cls.cases.append(testcase)
+            self.cases.append(testcase)
 
         # Create master test plan (parent of all)
-        cls.master = tcms_api.TestPlan(
+        self.master = tcms_api.TestPlan(
             name="API client Test Plan",
             document='plan creted from API',
-            product=cls.product,
-            version=cls.version,
-            type=cls.plantype)
-        tcms_api.info("* {0}".format(cls.master))
-        cls.master.testcases.add(cls.cases)
-        cls.master.update()
+            product=self.product,
+            version=self.version,
+            type=self.plantype)
+        tcms_api.info("* {0}".format(self.master))
+        self.master.testcases.add(self.cases)
+        self.master.update()
 
         # Create child test plans
-        cls.testruns = []
-        for plan_count in range(cls.num_plans):
+        self.testruns = []
+        for plan_count in range(self.num_plans):
             testplan = tcms_api.TestPlan(
                 name="Test Plan {0}".format(plan_count + 1),
                 document='plan creted from API',
-                product=cls.product,
-                version=cls.version,
-                parent=cls.master,
-                type=cls.plantype)
+                product=self.product,
+                version=self.version,
+                parent=self.master,
+                type=self.plantype)
             # Link all test cases to the test plan
-            testplan.testcases.add(cls.cases)
+            testplan.testcases.add(self.cases)
             testplan.update()
             tcms_api.info("  * {0}".format(testplan))
 
             # Create test runs
-            for run_count in range(cls.num_runs):
+            for run_count in range(self.num_runs):
                 testrun = tcms_api.TestRun(
                     testplan=testplan,
-                    build=cls.build,
-                    product=cls.product,
+                    build=self.build,
+                    product=self.product,
                     summary="Test Run {0}".format(run_count + 1),
-                    version=cls.version)
+                    version=self.version.name)
                 tcms_api.info("    * {0}".format(testrun))
-                cls.testruns.append(testrun)
+                self.testruns.append(testrun)
