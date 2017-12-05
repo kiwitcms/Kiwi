6ff8fab44898ccedc775730b1812b2553516df1a
Mr. Senko <atodorov@mrsenko.com>
2017-12-05 16:24:17 +0200

    Log XML-RPC requests from anonymous users. Fixes #126
    we acomplish this by creating (if necessary) a special user object
    with username=='Anonymous'.
    
    Note: While it is possible to log the requests with user=None
    the admin interface needs to be modified to support
    displaying/filtering None values which is even more hassle.
    This is the lest intrusive way to fix this problem!

--- tcms/xmlrpc/tests/test_version.py
+++ tcms/xmlrpc/tests/test_version.py
@@ -1,12 +1,43 @@
 # -*- coding: utf-8 -*-
+from mock import patch
 from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+
+from tcms.xmlrpc.api import version
+from tcms.xmlrpc import XMLRPC_VERSION
+from tcms.xmlrpc.models import XmlRpcLog
+
+from tcms.tests.factories import UserFactory
+from tcms.xmlrpc.tests.utils import XmlrpcAPIBaseTest
+from tcms.xmlrpc.tests.utils import make_http_request
 
 
 class TestVersion(TestCase):
-
     def test_get_version(self):
-        from tcms.xmlrpc import XMLRPC_VERSION
-        from tcms.xmlrpc.api import version
-
         response = version.get(None)
         self.assertEqual(response, XMLRPC_VERSION)
+
+
+class TestXMLRPCLogging(XmlrpcAPIBaseTest):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.http_request = make_http_request(user=cls.user)
+
+    @patch('tcms.xmlrpc.decorators.create_log', new=XmlRpcLog.objects.create)
+    def test_logging_with_authenticated_user(self):
+        log_count = XmlRpcLog.objects.filter(user=self.user, method='Version.get').count()
+
+        version.get(self.http_request)
+        new_count = XmlRpcLog.objects.filter(user=self.user, method='Version.get').count()
+
+        self.assertEqual(new_count, log_count + 1)
+
+    @patch('tcms.xmlrpc.decorators.create_log', new=XmlRpcLog.objects.create)
+    def test_logging_with_anonymous_user(self):
+        log_count = XmlRpcLog.objects.filter(user__username='Anonymous', method='Version.get').count()
+
+        version.get(make_http_request(user=AnonymousUser()))
+        new_count = XmlRpcLog.objects.filter(user__username='Anonymous', method='Version.get').count()
+
+        self.assertEqual(new_count, log_count + 1)
