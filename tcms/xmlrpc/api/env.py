b87f62827956b5d33647ebd486c6272f13e81e95
Mr. Senko <atodorov@mrsenko.com>
2017-12-15 00:51:17 +0200

    Update documentation for XML-RPC Env methods
    - remove get_properties() and get_values() b/c they are
      a corner case of filter_properties() and filter_values()

--- tcms/xmlrpc/api/env.py
+++ tcms/xmlrpc/api/env.py
@@ -10,37 +10,39 @@ __all__ = (
     'filter_groups',
     'filter_properties',
     'filter_values',
-    'get_properties',
-    'get_values',
 )
 
-__xmlrpc_namespace__ = 'TestEnv'
+__xmlrpc_namespace__ = 'Env'
 
 
 @log_call(namespace=__xmlrpc_namespace__)
 def filter_groups(request, query):
     """
-    Description: Performs a search and returns the resulting list of env groups.
+    .. function:: XML-RPC Env.filter_groups(query)
 
-    Params:      $query - Hash: keys must match valid search fields.
+        Perform a search and return the resulting list of env groups.
+        Parameter ``query`` is dict which recognizes the following
+        keys:
 
-    +------------------------------------------------------------------+
-    |               Product Search Parameters                          |
-    +------------------------------------------------------------------+
-    |        Key          |          Valid Values                      |
-    | id                  | Integer: ID of env group                   |
-    | name                | String                                     |
-    | manager             | ForeignKey: Auth.user                      |
-    | modified_by         | ForeignKey: Auth.user                      |
-    | is_active           | Boolean                                    |
-    | property            | ForeignKey: TCMSEnvProperty                |
-    +------------------------------------------------------------------+
+        :param id: ID of env group
+        :type id: int
+        :param name: Name of env group
+        :type name: str
+        :param manager: Manager of this group. ForeignKey!
+        :type manager: int or ``settings.AUTH_USER_MODEL`` object
+        :param modified_by: Who modified the group. ForeignKey!
+        :type modified_by: int or ``settings.AUTH_USER_MODEL`` object
+        :param is_active: if this group is active or not
+        :type is_active: bool
+        :param property: Group property. ForeignKey
+        :type property: int or :class:`tcms.management.models.TCMSEnvProperty`
+        :returns: List of serialized env groups that match the query
+        :rtype: list(dict)
 
-    Returns:     Array: Matching env groups are retuned in a list of hashes.
+    Query keys support the double-underscore field lookups from Django.
+    For example to get all of env group with name containing 'Desktop'::
 
-    Example:
-    # Get all of env group name contains 'Desktop'
-    >>> Env.filter_groups({'name__icontains': 'Desktop'})
+        >>> Env.filter_groups({'name__icontains': 'Desktop'})
     """
     if 'is_active' in query:
         query['is_active'] = parse_bool_value(query['is_active'])
@@ -50,26 +52,28 @@ def filter_groups(request, query):
 @log_call(namespace=__xmlrpc_namespace__)
 def filter_properties(request, query):
     """
-    Description: Performs a search and returns the resulting list of env properties.
+    .. function:: XML-RPC Env.filter_properties(query)
 
-    Params:      $query - Hash: keys must match valid search fields.
+        Performs a search and returns the resulting list of env properties.
+        Parameter ``query`` is dict which recognizes the following
+        keys:
 
-    +------------------------------------------------------------------+
-    |               Product Search Parameters                          |
-    +------------------------------------------------------------------+
-    |        Key          |          Valid Values                      |
-    | id                  | Integer: ID of env properties              |
-    | name                | String                                     |
-    | is_active           | Boolean                                    |
-    | group               | ForeignKey: TCMSEnvGroup                   |
-    | value               | ForeignKey: TCMSEnvValues                   |
-    +------------------------------------------------------------------+
+        :param id: ID of env properties
+        :type id: int
+        :param name: Name of peroperty
+        :type name: str
+        :param is_active:
+        :type is_active: bool
+        :param group: ForeignKey: TCMSEnvGroup
+        :type group: int or :class:`tcms.management.models.TCMSEnvGroup`
+        :param value: ForeignKey: TCMSEnvValues
+        :type value: int or :class:`tcms.management.models.TCMSEnvValues`
+        :returns: List of serialized env properties matching the query
+        :rtype: list(dict)
 
-    Returns:     Array: Matching env properties are retuned in a list of hashes.
+        For example to get all env properties containing 'Desktop'::
 
-    Example:
-    # Get all of env properties name contains 'Desktop'
-    >>> Env.filter_properties({'name__icontains': 'Desktop'})
+            >>> Env.filter_properties({'name__icontains': 'Desktop'})
     """
     if 'is_active' in query:
         query['is_active'] = parse_bool_value(query['is_active'])
@@ -79,74 +83,27 @@ def filter_properties(request, query):
 @log_call(namespace=__xmlrpc_namespace__)
 def filter_values(request, query):
     """
-    Description: Performs a search and returns the resulting list of env properties.
+    .. function:: XML-RPC Env.filter_values(query)
 
-    Params:      $query - Hash: keys must match valid search fields.
+        Performs a search and returns the resulting list of env values.
+        Parameter ``query`` is dict which recognizes the following
+        keys:
 
-    +------------------------------------------------------------------+
-    |               Product Search Parameters                          |
-    +------------------------------------------------------------------+
-    |        Key          |          Valid Values                      |
-    | id                  | Integer: ID of env value                   |
-    | value               | String                                     |
-    | is_active           | Boolean                                    |
-    | property            | ForeignKey: TCMSEnvProperty                |
-    +------------------------------------------------------------------+
+        :param id: ID of env value
+        :type id: int
+        :param value: Object value
+        :type value: str
+        :param is_active:
+        :type is_active: bool
+        :param property: ForeignKey: TCMSEnvProperty
+        :type property: int or :class:`tcms.management.models.TCMSEnvProperty`
+        :returns: List of serialized env values matching the query
+        :rtype: list(dict)
 
-    Returns:     Array: Matching env values are retuned in a list of hashes.
+        For example to get all env values containing 'Desktop'::
 
-    Example:
-    # Get all of env values name contains 'Desktop'
-    >>> Env.filter_values({'name__icontains': 'Desktop'})
+            >>> Env.filter_values({'value__icontains': 'Desktop'})
     """
     if 'is_active' in query:
         query['is_active'] = parse_bool_value(query['is_active'])
-    return TCMSEnvValue.to_xmlrpc(query)
-
-
-@log_call(namespace=__xmlrpc_namespace__)
-def get_properties(request, env_group_id=None, is_active=True):
-    """
-    Description: Get the list of properties associated with this env group.
-
-    Params:      $env_group_id - Integer: env_group_id of the env group in the Database
-                                 Return all of properties when the argument is not specific.
-                 $is_active    - Boolean: True to only include builds where is_active is true.
-                                 Default: True
-    Returns:     Array: Returns an array of env properties objects.
-
-    Example:
-    # Get all of properties
-    >>> Env.get_properties()
-    # Get the properties in group 10
-    >>> Env.get_properties(10)
-    """
-    query = {'is_active': parse_bool_value(is_active)}
-    if env_group_id:
-        query['group__pk'] = env_group_id
-
-    return TCMSEnvProperty.to_xmlrpc(query)
-
-
-@log_call(namespace=__xmlrpc_namespace__)
-def get_values(request, env_property_id=None, is_active=True):
-    """
-    Description: Get the list of values associated with this env property.
-
-    Params:      $env_property_id - Integer: env_property_id of the env property in the Database
-                                    Return all of values when the argument is not specific.
-                 $is_active       - Boolean: True to only include builds where is_active is true.
-                                    Default: True
-    Returns:     Array: Returns an array of env values objects.
-
-    Example:
-    # Get all of properties
-    >>> Env.get_properties()
-    # Get the properties in group 10
-    >>> Env.get_properties(10)
-    """
-    query = {'is_active': parse_bool_value(is_active)}
-    if env_property_id:
-        query['property__pk'] = env_property_id
-
     return TCMSEnvValue.to_xmlrpc(query)
