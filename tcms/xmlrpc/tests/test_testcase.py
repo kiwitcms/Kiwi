d1516335d1130dc6fcf48cc86ab0e0c600dcd5a4
Mr. Senko <atodorov@mrsenko.com>
2017-12-20 21:22:02 +0200

    [tests] Fix RPC tests for TestCase

--- tcms/xmlrpc/tests/test_testcase.py
+++ tcms/xmlrpc/tests/test_testcase.py
@@ -1,93 +1,83 @@
 # -*- coding: utf-8 -*-
 
-from django import test
-
 from tcms.testcases.models import TestCasePlan
-from tcms.xmlrpc.api import testcase as XmlrpcTestCase
-from tcms.xmlrpc.tests.utils import make_http_request
 
 from tcms.tests.factories import TestCaseFactory
 from tcms.tests.factories import TestPlanFactory
-from tcms.tests.factories import UserFactory
+from tcms.xmlrpc.tests.utils import XmlrpcAPIBaseTest
 
 
-class TestNotificationRemoveCC(test.TestCase):
+class TestNotificationRemoveCC(XmlrpcAPIBaseTest):
     """ Tests the XML-RPC testcase.notication_remove_cc method """
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory()
-        cls.http_req = make_http_request(user=cls.user,
-                                         user_perm='testcases.change_testcase')
+    def _fixture_setup(self):
+        super(TestNotificationRemoveCC, self)._fixture_setup()
 
-        cls.default_cc = 'example@MrSenko.com'
-        cls.testcase = TestCaseFactory()
-        cls.testcase.emailing.add_cc(cls.default_cc)
+        self.default_cc = 'example@MrSenko.com'
+        self.testcase = TestCaseFactory()
+        self.testcase.emailing.add_cc(self.default_cc)
+
+    def tearDown(self):
+        super(TestNotificationRemoveCC, self).tearDown()
+        self.rpc_client.Auth.logout()
 
     def test_remove_existing_cc(self):
         # initially testcase has the default CC listed
         # and we issue XMLRPC request to remove the cc
-        XmlrpcTestCase.notification_remove_cc(self.http_req, self.testcase.pk, [self.default_cc])
+        self.rpc_client.TestCase.notification_remove_cc(self.testcase.pk, [self.default_cc])
 
         # now verify that the CC email has been removed
         self.assertEqual(0, self.testcase.emailing.cc_list.count())
 
 
-class TestUnlinkPlan(test.TestCase):
+class TestUnlinkPlan(XmlrpcAPIBaseTest):
     """ Test the XML-RPC method testcase.unlink_plan() """
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory()
-        cls.http_req = make_http_request(user=cls.user,
-                                         user_perm='testcases.delete_testcaseplan')
+    def _fixture_setup(self):
+        super(TestUnlinkPlan, self)._fixture_setup()
+        self.testcase_1 = TestCaseFactory()
+        self.testcase_2 = TestCaseFactory()
+        self.plan_1 = TestPlanFactory()
+        self.plan_2 = TestPlanFactory()
 
-        cls.testcase_1 = TestCaseFactory()
-        cls.testcase_2 = TestCaseFactory()
-        cls.plan_1 = TestPlanFactory()
-        cls.plan_2 = TestPlanFactory()
+        self.testcase_1.add_to_plan(self.plan_1)
 
-        cls.testcase_1.add_to_plan(cls.plan_1)
-
-        cls.testcase_2.add_to_plan(cls.plan_1)
-        cls.testcase_2.add_to_plan(cls.plan_2)
+        self.testcase_2.add_to_plan(self.plan_1)
+        self.testcase_2.add_to_plan(self.plan_2)
 
     def test_unlink_plan_from_case_with_single_plan(self):
-        result = XmlrpcTestCase.unlink_plan(self.http_req, self.testcase_1.pk, self.plan_1.pk)
+        result = self.rpc_client.TestCase.unlink_plan(self.testcase_1.pk, self.plan_1.pk)
         self.assertEqual(0, self.testcase_1.plan.count())
         self.assertEqual([], result)
 
     def test_unlink_plan_from_case_with_two_plans(self):
-        result = XmlrpcTestCase.unlink_plan(self.http_req, self.testcase_2.pk, self.plan_1.pk)
+        result = self.rpc_client.TestCase.unlink_plan(self.testcase_2.pk, self.plan_1.pk)
         self.assertEqual(1, self.testcase_2.plan.count())
         self.assertEqual(1, len(result))
         self.assertEqual(self.plan_2.pk, result[0]['plan_id'])
 
 
-class TestLinkPlan(test.TestCase):
+class TestLinkPlan(XmlrpcAPIBaseTest):
     """ Test the XML-RPC method testcase.link_plan() """
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory()
-        cls.http_req = make_http_request(user=cls.user,
-                                         user_perm='testcases.add_testcaseplan')
+    def _fixture_setup(self):
+        super(TestLinkPlan, self)._fixture_setup()
 
-        cls.testcase_1 = TestCaseFactory()
-        cls.testcase_2 = TestCaseFactory()
-        cls.testcase_3 = TestCaseFactory()
+        self.testcase_1 = TestCaseFactory()
+        self.testcase_2 = TestCaseFactory()
+        self.testcase_3 = TestCaseFactory()
 
-        cls.plan_1 = TestPlanFactory()
-        cls.plan_2 = TestPlanFactory()
-        cls.plan_3 = TestPlanFactory()
+        self.plan_1 = TestPlanFactory()
+        self.plan_2 = TestPlanFactory()
+        self.plan_3 = TestPlanFactory()
 
         # case 1 is already linked to plan 1
-        cls.testcase_1.add_to_plan(cls.plan_1)
+        self.testcase_1.add_to_plan(self.plan_1)
 
     def test_insert_ignores_existing_mappings(self):
         plans = [self.plan_1.pk, self.plan_2.pk, self.plan_3.pk]
         cases = [self.testcase_1.pk, self.testcase_2.pk, self.testcase_3.pk]
-        XmlrpcTestCase.link_plan(self.http_req, cases, plans)
+        self.rpc_client.TestCase.link_plan(cases, plans)
 
         # no duplicates for plan1/case1 were created
         self.assertEqual(
