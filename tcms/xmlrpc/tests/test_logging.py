8e0526346d1e1b40cb72117c1761aa0a55e46ead
Mr. Senko <atodorov@mrsenko.com>
2017-12-31 00:06:00 +0200

    [security] Don't log passwords sent via RPC
    for sensitive methods which contain user passwords we don't log
    the arguments to these methods

--- tcms/xmlrpc/tests/test_logging.py
+++ tcms/xmlrpc/tests/test_logging.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from mock import patch
 
+from django.db.models.functions import Length
+
 from tcms.xmlrpc.models import XmlRpcLog
 from tcms.xmlrpc.tests.utils import XmlrpcAPIBaseTest
 
 
 class TestXMLRPCLogging(XmlrpcAPIBaseTest):
+    def assertArgsNotLogged(self, rpc_method):
+        # verify that arguments for specific RPC method are not logged
+        log_count = XmlRpcLog.objects.annotate(
+            args_len=Length('args')
+        ).filter(
+            method=rpc_method,
+            args_len__gt=0
+        ).count()
+        self.assertEqual(0, log_count)
+
     @patch('tcms.xmlrpc.handlers.settings.DEBUG', new=False)
     def test_logging_with_authenticated_user(self):
         log_count = XmlRpcLog.objects.filter(user=self.api_user, method='Env.filter_groups').count()
@@ -26,3 +38,16 @@ class TestXMLRPCLogging(XmlrpcAPIBaseTest):
                                              method='Env.filter_groups').count()
 
         self.assertEqual(new_count, log_count + 1)
+
+    @patch('tcms.xmlrpc.handlers.settings.DEBUG', new=False)
+    def test_dont_log_passwords(self):
+        # the parent class will call Auth.login
+        # sending the user password!
+        self.assertArgsNotLogged('Auth.login')
+
+        # also call User.update which accepts passwords via dict
+        self.rpc_client.User.update({
+            'password': 'new-password',
+            'old_password': 'api-testing',
+        })
+        self.assertArgsNotLogged('User.update')
