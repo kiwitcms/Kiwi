42009309b29569715c46a21926eac6dc4522f401
Mr. Senko <atodorov@mrsenko.com>
2018-01-17 11:57:14 +0200

    Use django-grappelli for the admin templates
    this removes custom templates which are very old and brings in
    a modern look and feel which can be customized more easily.
    Also override the default admin/base_site.html template from
    grappelli to include Kiwi's navigation menu.

--- tcms/tests/storage.py
+++ tcms/tests/storage.py
@@ -17,9 +17,14 @@ def find_files():
 
 class RaiseWhenFileNotFound(StaticFilesStorage):
     _files_found = find_files()
+    # grappelli uses the following piece of code:
+    # window.__admin_media_prefix__ = "{% filter escapejs %}{% static "grappelli/" %}
+    # to establish a base URL for all of its assets. This causes this storage class
+    # to raise an exception b/c the finder only collects files, not directory names
+    _white_list = ["grappelli/"]
 
     def url(self, name):
-        if name not in self._files_found:
+        if (name not in self._files_found) and (name not in self._white_list):
             raise Exception('Static file "%s" does not exist and will cause 404 errors!' % name)
 
         return super(RaiseWhenFileNotFound, self).url(name)
