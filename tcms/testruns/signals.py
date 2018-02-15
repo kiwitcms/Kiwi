8ce87309dfa408ef44ed147bf572c5de4208b4bc
Mr. Senko <atodorov@mrsenko.com>
2018-02-15 23:23:04 +0200

    Remove double thread when sending email on post_save signal
    TestRun.mail() method already uses tcms.core.utils.mailto which
    itself uses threading. There's no need to spawn another thread
    just so we can spawn the email thread!
    
    Without this patch we get a DB lock for the auth_users table when
    signal initialization is moved to AppConfig.ready().

--- tcms/testruns/signals.py
+++ tcms/testruns/signals.py
@@ -1,49 +1,24 @@
-# -*- coding: utf-8 -*-
-# FIXME: Use signal to handle log
-import threading
-
-
-# Reference from
-# http://www.chrisdpratt.com/2008/02/16/signals-in-django-stuff-thats-not-documented-well/
-
-class NewRunEmailThread(threading.Thread):
-    def __init__(self, instance, is_created):
-        self.instance = instance
-        self.is_created = is_created
-        threading.Thread.__init__(self)
-
-    def run(self):
-        # run update
-        if self.is_created:
-            self.instance.mail(
-                # new_run.txt can use in testrun update
-                template='mail/update_run.txt',
-                subject='Test Run %s - %s has been updated' % (
-                    self.instance.pk, self.instance.summary
-                ),
-                context={'test_run': self.instance, }
-            )
-        # run create
-        else:
-            self.instance.mail(
-                template='mail/new_run.txt',
-                subject='New run create from plan %s: %s' % (
-                    self.instance.plan_id, self.instance.summary
-                ),
-                context={'test_run': self.instance, }
-            )
 
 
 def post_run_saved(sender, *args, **kwargs):
     instance = kwargs['instance']
+
     if kwargs.get('created'):
-        # Send the mail to default tester for alert him/her
-        is_created = None
-        NewRunEmailThread(instance, is_created).start()
+        instance.mail(
+            template='mail/new_run.txt',
+            subject='New run create from plan %s: %s' % (
+                instance.plan_id, instance.summary
+            ),
+            context={'test_run': instance, }
+        )
     else:
-        # FIXME: Log, Plugin and other editing functions
-        is_created = True
-        NewRunEmailThread(instance, is_created).start()
+        instance.mail(
+            template='mail/update_run.txt',
+            subject='Test Run %s - %s has been updated' % (
+                instance.pk, instance.summary
+            ),
+            context={'test_run': instance, }
+        )
 
 
 def post_case_run_saved(sender, *args, **kwargs):
