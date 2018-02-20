38c1f601f58e2528d8a32996ea263b367e4b877c
Mr. Senko <atodorov@mrsenko.com>
2018-02-20 12:55:29 +0200

    Display notification messages in the base template

--- tcms/core/templatetags/extra_filters.py
+++ tcms/core/templatetags/extra_filters.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import template
+from django.contrib.messages import constants as messages
 
 register = template.Library()
 
@@ -25,3 +26,18 @@ def timedelta2string(object):
 @register.filter(name='timedelta2seconds')
 def timedelta2seconds(object):
     return int(object.seconds + object.days * 86400)
+
+
+@register.filter(name='message_icon')
+def message_icon(msg):
+    """
+        Returns the string class name of a message icon
+        which feeds directly into Patternfly.
+    """
+    icons = {
+        messages.ERROR: 'error-circle-o',
+        messages.WARNING: 'warning-triangle-o',
+        messages.SUCCESS: 'ok',
+        messages.INFO: 'info',
+    }
+    return 'pficon-' + icons[msg.level]
