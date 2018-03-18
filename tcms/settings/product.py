355e2e2c35aaeea70aa98a34fb7b5f3e543af239
Anton Sankov <a.sankov96@gmail.com>
2018-03-18 23:28:13 +0200

    Fixed pylint issues in module tcms.settings.product

--- tcms/settings/product.py
+++ tcms/settings/product.py
@@ -1,4 +1,8 @@
-# Django settings for product env.
+# pylint: disable=wildcard-import, unused-wildcard-import
+
+"""
+    Django settings for product env.
+"""
 
 from .common import *  # noqa: F401,F403
 
