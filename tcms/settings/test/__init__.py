6ffe13592a876b4f3bbbb1d9076949204dd3b57e
Anton Sankov <a.sankov96@gmail.com>
2018-03-22 00:26:29 +0200

    Fixed pylint issues in module tcms.settings.test

--- tcms/settings/test/__init__.py
+++ tcms/settings/test/__init__.py
@@ -1,9 +1,14 @@
+# pylint: disable=wildcard-import, unused-wildcard-import
+
 import warnings
+
+from tcms.settings.devel import *  # noqa: F401,F403
+
+
 # silence resource warnings while testing, see
 # https://emptysqua.re/blog/against-resourcewarnings-in-python-3/
 warnings.simplefilter("ignore", ResourceWarning)
 
-from tcms.settings.devel import *  # noqa: F401,F403
 
 DATABASES = {
     'default': {
