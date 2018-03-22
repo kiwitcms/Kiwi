db9fc44ea893b23544c46b538e09363d48cde2cf
Mr. Senko <atodorov@mrsenko.com>
2018-03-22 14:03:57 +0200

    Move Bugzilla backend out of here. Refs #239
    The code continues to live under
    https://github.com/kiwitcms/kiwitcms-auth-bugzilla

--- tcms/core/contrib/auth/backends.py
+++ tcms/core/contrib/auth/backends.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-import xmlrpc.client
 
 from django.conf import settings
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.backends import ModelBackend, RemoteUserBackend
 
@@ -14,46 +11,6 @@ class DBModelBackend(ModelBackend):
     can_login = True
     can_register = True
     can_logout = True
-
-
-class BugzillaBackend(ModelBackend):
-    """
-    Bugzilla authorization backend for TCMS.
-
-    It's required bugzilla xmlrpc.
-    """
-    # Web UI Needed
-    can_login = True
-    can_register = False
-    can_logout = True
-
-    def authenticate(self, request, username=None, password=None):
-        server = xmlrpc.client.ServerProxy(settings.BUGZILLA3_RPC_SERVER)
-
-        try:
-            validate_email(username)
-        except ValidationError:
-            return None
-        else:
-            try:
-                server.bugzilla.login(username, password)
-            except xmlrpc.client.Fault:
-                return None
-
-            try:
-                user = User.objects.get(email=username)
-                user.set_password(password)
-                user.save()
-            except User.DoesNotExist:
-                user = User.objects.create_user(
-                    username=username.split('@')[0],
-                    email=username
-                )
-
-                user.set_unusable_password(password)
-
-        if user.check_password(password):
-            return user
 
 
 class KerberosBackend(ModelBackend):
