16f8837339f5bf83b9e43226ca01f139c190ed31
Anton Sankov <a.sankov96@gmail.com>
2018-04-04 23:35:44 +0300

    Fixed pylint issues in module tcms.core.middleware

--- tcms/core/middleware.py
+++ tcms/core/middleware.py
@@ -1,6 +1,8 @@
+# pylint: disable=no-self-use, too-few-public-methods
+
 from django.utils.deprecation import MiddlewareMixin
 
 
 class CsrfDisableMiddleware(MiddlewareMixin):
-    def process_view(self, request, callback, callback_args, callback_kwargs):
+    def process_view(self, request, _callback, _callback_args, _callback_kwargs):
         setattr(request, '_dont_enforce_csrf_checks', True)
