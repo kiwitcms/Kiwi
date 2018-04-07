300e675314bd77856cb0df66bb55b8ec77f61a7a
Anton Sankov <a.sankov96@gmail.com>
2018-04-08 01:16:33 +0300

    Fixed pylint issues in module tcms.core.helpers.cache

--- tcms/core/helpers/cache.py
+++ tcms/core/helpers/cache.py
@@ -3,21 +3,21 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 
 
-def cached_entities(ctype_name):
+def cached_entities(content_type_name):
     """
     Some entities are frequently used.\n
     Cache them for reuse.\n
     Retrieve using model names.
     """
-    ctype_key = 'ctt_type_' + ctype_name
-    c_type = cache.get(ctype_key)
-    if not c_type:
-        c_type = ContentType.objects.get(model__iexact=ctype_name)
-        cache.set(ctype_key, c_type)
-    model_class = c_type.model_class()
-    key = 'cached_' + ctype_name
+    content_type_key = 'ctt_type_' + content_type_name
+    content_type = cache.get(content_type_key)
+    if not content_type:
+        content_type = ContentType.objects.get(model__iexact=content_type_name)
+        cache.set(content_type_key, content_type)
+    model_class = content_type.model_class()
+    key = 'cached_' + content_type_name
     entities = cache.get(key)
     if not entities:
-        entities = model_class._default_manager.all()
+        entities = model_class.objects.all()
         cache.set(key, entities)
     return entities
