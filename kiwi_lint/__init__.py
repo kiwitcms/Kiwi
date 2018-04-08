156b1ce8075c265643d1b1ae343ff228e1497b2d
Mr. Senko <atodorov@mrsenko.com>
2018-04-08 12:20:46 +0300

    Add pylint checker for docstring quotes. Fix #296
    we want all docstring to be enclosed in triple double quotes

--- kiwi_lint/__init__.py
+++ kiwi_lint/__init__.py
@@ -5,8 +5,10 @@
 # NOTE: import order matches the numeric ID of the checker
 from .dunder_attributes import DunderClassAttributeChecker
 from .list_comprehension import ListComprehensionChecker
+from .docstring import DocstringChecker
 
 
 def register(linter):
     linter.register_checker(DunderClassAttributeChecker(linter))
     linter.register_checker(ListComprehensionChecker(linter))
+    linter.register_checker(DocstringChecker(linter))
