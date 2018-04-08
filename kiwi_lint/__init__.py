050f1326d0f91c00b37ac3f92e30a6d659018264
Mr. Senko <atodorov@mrsenko.com>
2018-04-08 11:40:54 +0300

    Add pylint checker for list comprehensions. Fix #270

--- kiwi_lint/__init__.py
+++ kiwi_lint/__init__.py
@@ -2,8 +2,11 @@
 
 # Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
+# NOTE: import order matches the numeric ID of the checker
 from .dunder_attributes import DunderClassAttributeChecker
+from .list_comprehension import ListComprehensionChecker
 
 
 def register(linter):
     linter.register_checker(DunderClassAttributeChecker(linter))
+    linter.register_checker(ListComprehensionChecker(linter))
