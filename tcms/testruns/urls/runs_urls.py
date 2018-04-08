a6b355bdaae002c1308f88ed15463eddcd46ddae
Anton Sankov <a.sankov96@gmail.com>
2018-04-09 00:10:26 +0300

    Fixed some pylint issues in module tcms.testruns.views

--- tcms/testruns/urls/runs_urls.py
+++ tcms/testruns/urls/runs_urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from .. import views
 
 urlpatterns = [
-    url(r'^$', views.all, name='testruns-all'),
+    url(r'^$', views.get_all, name='testruns-all'),
     url(r'^ajax/$', views.ajax_search, name='testruns-ajax_search'),
     url(r'^env_value/$', views.env_value, name='testruns-env_value'),
 ]
