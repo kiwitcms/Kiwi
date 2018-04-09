93b8c264c31a3332e7820f6167f0b0fd071713b5
Anton Sankov <a.sankov96@gmail.com>
2018-04-09 18:54:05 +0300

    Fixed pylint issues in module tcms.xmlrpc.api.env_group

--- tcms/xmlrpc/api/env_group.py
+++ tcms/xmlrpc/api/env_group.py
@@ -7,7 +7,7 @@ from tcms.xmlrpc.utils import parse_bool_value
 
 
 @rpc_method(name='Env.Group.filter')
-def filter(query):
+def filter(query):  # pylint: disable=invalid-name
     """
     .. function:: XML-RPC Env.Group.filter(query)
 
