eec369e01b12645d23c37fd6e95c871ec6279abf
Mr. Senko <atodorov@mrsenko.com>
2018-06-18 17:16:07 +0300

    Allow users to delete their accounts via the admin view
    Link to delete account is present in My profile page.

--- tcms/core/admin.py
+++ tcms/core/admin.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission, User
 from django.contrib.sites.models import Site
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.sites.admin import SiteAdmin
@@ -27,6 +27,42 @@ class KiwiSiteAdmin(SiteAdmin):
 class KiwiUserAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ('is_superuser', 'date_joined')
     ordering = ['-pk']  # same as -date_joined
+
+    def _modifying_myself(self, request, object_id):
+        return request.user.pk == int(object_id)
+
+    @admin.options.csrf_protect_m
+    def delete_view(self, request, object_id, extra_context=None):
+        # if trying to delete another user go directly to parent
+        if not self._modifying_myself(request, object_id):
+            return super().delete_view(request, object_id, extra_context)
+
+        # else allow deletion of the user own account
+        permission = Permission.objects.get(content_type__app_label='auth',
+                                            codename='delete_user')
+        try:
+            request.user.user_permissions.add(permission)
+            return super().delete_view(request, object_id, extra_context)
+        finally:
+            request.user.user_permissions.remove(permission)
+
+    def response_delete(self, request, obj_display, obj_id):
+        result = super().response_delete(request, obj_display, obj_id)
+
+        if not self._modifying_myself(request, obj_id):
+            return result
+
+        # user doesn't exist anymore so go to the index page
+        # instead of returning to the user admin page
+        return HttpResponseRedirect(reverse('core-views-index'))
+
+    def has_delete_permission(self, request, obj=None):
+        # allow to delete yourself without having 'delete' permission
+        # explicitly assigned
+        if self._modifying_myself(request, getattr(obj, 'pk', 0)):
+            return True
+
+        return super().has_delete_permission(request, obj)
 
 
 # we don't want comments to be accessible via the admin interface
