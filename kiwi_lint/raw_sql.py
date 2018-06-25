d823d08abee91df519eee3f503178fe0dd5eb141
Mr. Senko <atodorov@mrsenko.com>
2018-06-25 16:31:58 +0300

    Add linter for raw SQL, refs #36

---
+++ kiwi_lint/raw_sql.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2018 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+import astroid
+
+from pylint import interfaces
+from pylint import checkers
+from pylint.checkers import utils
+
+
+class RawSQLChecker(checkers.BaseChecker):
+    __implements__ = (interfaces.IAstroidChecker,)
+
+    name = 'raw-sql-checker'
+
+    msgs = {'R4431': ('Avoid using raw SQL',
+                      'avoid-raw-sql',
+                      'Avoid raw SQL, use Django ORM queries instead')}
+
+    @utils.check_messages('avoid-raw-sql')
+    def visit_attribute(self, node):
+        # looking for .extra(select={}) patterns
+        if node.attrname == 'extra' and isinstance(node.parent, astroid.Call):
+            for keyword in node.parent.keywords:
+                if keyword.arg == 'select':
+                    self.add_message('avoid-raw-sql', node=node)
+                    break
