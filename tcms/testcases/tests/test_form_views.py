b56a355c7a26a561bc42e31530f5fe60941ddd7e
Mr. Senko <atodorov@mrsenko.com>
2018-06-29 16:54:28 +0300

    login() before report and form tests

--- tcms/testcases/tests/test_form_views.py
+++ tcms/testcases/tests/test_form_views.py
@@ -5,9 +5,21 @@ from django.urls import reverse
 from django.conf import settings
 
 from tcms.testcases.forms import CaseAutomatedForm
+from tcms.tests.factories import UserFactory
 
 
 class TestForm_AutomatedView(test.TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.tester = UserFactory()
+        cls.tester.set_password('password')
+        cls.tester.save()
+
+    def setUp(self):
+        super().setUp()
+        self.client.login(username=self.tester.username,  # nosec:B106:hardcoded_password_funcarg
+                          password='password')
+
     def test_get_form(self):
         """Verify the view renders the expected HTML"""
         response = self.client.get(reverse('testcases-form-automated'))
