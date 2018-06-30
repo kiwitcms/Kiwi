25554832059e7937106459b4c67c58f0d47caf6e
Mr. Senko <atodorov@mrsenko.com>
2018-06-30 22:34:39 +0300

    Rename tables in hard-coded SQL
    after the rename of the DB tables we have to rename these
    hard-coded SQL queries because otherwise they fail. These will be
    removed soon so a quick fix is acceptable.
    
    NOTE: as these are not thorughly tested they could lead to bugs
    but we'll worry about that if/when we get there.

--- tcms/core/utils/raw_sql.py
+++ tcms/core/utils/raw_sql.py
@@ -5,54 +5,54 @@ class RawSQL:  # pylint: disable=too-few-public-methods
     """
     # Following SQL use for count case and run in plan
     num_cases = 'SELECT COUNT(*) \
-        FROM test_case_plans \
-        WHERE test_case_plans.plan_id = test_plans.plan_id'
+        FROM testcases_testcaseplan \
+        WHERE testcases_testcaseplan.plan_id = testplans_testplan.plan_id'
 
     num_runs = 'SELECT COUNT(*) \
-        FROM test_runs \
-        WHERE test_runs.plan_id = test_plans.plan_id'
+        FROM testruns_testrun \
+        WHERE testruns_testrun.plan_id = testplans_testplan.plan_id'
 
     num_plans = 'SELECT COUNT(*) \
-        FROM test_plans AS ch_plans\
-        WHERE ch_plans.parent_id = test_plans.plan_id'
+        FROM testplans_testplan AS ch_plans\
+        WHERE ch_plans.parent_id = testplans_testplan.plan_id'
 
     num_case_bugs = 'SELECT COUNT(*) \
-        FROM test_case_bugs \
-        WHERE test_case_bugs.case_id = test_cases.case_id'
+        FROM testcases_bug \
+        WHERE testcases_bug.case_id = testcases_testcase.case_id'
 
     num_case_run_bugs = 'SELECT COUNT(*) \
-        FROM test_case_bugs \
-        WHERE test_case_bugs.case_run_id = test_case_runs.case_run_id'
+        FROM testcases_bug \
+        WHERE testcases_bug.case_run_id = testruns_testcaserun.case_run_id'
 
 
 class ReportSQL(object):  # pylint: disable=too-few-public-methods
     # Index
     index_product_plans_count = 'SELECT COUNT(plan_id) \
-        FROM test_plans \
-        WHERE test_plans.product_id = products.id'
+        FROM testplans_testplan \
+        WHERE testplans_testplan.product_id = management_product.id'
 
     index_product_runs_count = 'SELECT COUNT(run_id) \
-        FROM test_runs \
-        INNER JOIN test_plans \
-        ON test_runs.plan_id = test_plans.plan_id \
-        WHERE test_plans.product_id = products.id'
+        FROM testruns_testrun \
+        INNER JOIN testplans_testplan \
+        ON testruns_testrun.plan_id = testplans_testplan.plan_id \
+        WHERE testplans_testplan.product_id = management_product.id'
 
     index_product_cases_count = 'SELECT COUNT(case_id) \
-        FROM test_case_plans \
-        INNER JOIN test_plans \
-        ON test_case_plans.plan_id = test_plans.plan_id \
-        WHERE test_plans.product_id = products.id'
+        FROM testcases_testcaseplan \
+        INNER JOIN testplans_testplan \
+        ON testcases_testcaseplan.plan_id = testplans_testplan.plan_id \
+        WHERE testplans_testplan.product_id = management_product.id'
 
     # Custom Search Zone
     # added by chaobin
     case_runs_count_by_status_under_run = 'SELECT COUNT(DISTINCT case_run_id) \
-        FROM test_case_runs \
-        WHERE test_case_runs.run_id = test_runs.run_id AND test_case_runs.case_run_status_id = %s'
+        FROM testruns_testcaserun \
+        WHERE testruns_testcaserun.run_id = testruns_testrun.run_id AND testruns_testcaserun.case_run_status_id = %s'
 
     custom_details_case_run_count = 'SELECT tcrs.name \
-        AS test_case_status, COUNT(tcr.case_id) AS case_run_count \
-        FROM test_case_run_status tcrs          \
-        LEFT JOIN test_case_runs tcr ON tcrs.case_run_status_id = tcr.case_run_status_id \
+        AS testcases_testcasestatus, COUNT(tcr.case_id) AS case_run_count \
+        FROM testruns_testcaserunstatus tcrs          \
+        LEFT JOIN testruns_testcaserun tcr ON tcrs.case_run_status_id = tcr.case_run_status_id \
         WHERE tcr.run_id = %s \
         GROUP BY tcrs.name \
         ORDER BY tcrs.sortkey'
