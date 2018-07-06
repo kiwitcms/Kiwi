ba7e4971501c6a9218221d8d3fdd9e79981e7e44
Mr. Senko <atodorov@mrsenko.com>
2018-07-06 14:30:47 +0300

    User TestCase.log(), TestCaseRun.log() instead of TCMSLogModel
    don't use the underlying implementation directly but instead use
    the existing interface that everyone else uses.

--- tcms/testcases/data.py
+++ tcms/testcases/data.py
@@ -5,7 +5,6 @@ from django.contrib.contenttypes.models import ContentType
 
 from django_comments.models import Comment
 
-from tcms.core.logs.models import TCMSLogModel
 from tcms.testcases.models import TestCase
 from tcms.testruns.models import TestCaseRun
 
@@ -13,18 +12,10 @@ from tcms.testruns.models import TestCaseRun
 class TestCaseViewDataMixin(object):
     """Mixin class to get view data of test case"""
 
-    def get_case_logs(self, testcase):
-        content_type = self.get_case_content_type()
-        logs = TCMSLogModel.objects.filter(content_type=content_type,
-                                           object_pk=testcase.pk,
-                                           site=settings.SITE_ID)
-        logs = logs.values('date', 'who__username', 'action')
-        return logs.order_by('date')
-
     def get_case_comments(self, case):
         """Get a case' comments"""
 
-        content_type = self.get_case_content_type()
+        content_type = ContentType.objects.get_for_model(TestCase)
         comments = Comment.objects.filter(content_type=content_type,
                                           object_pk=case.pk,
                                           site=settings.SITE_ID,
@@ -35,30 +26,15 @@ class TestCaseViewDataMixin(object):
         comments.order_by('pk')
         return comments
 
-    @staticmethod
-    def get_case_content_type():
-        return ContentType.objects.get_for_model(TestCase)
-
 
 class TestCaseRunViewDataMixin(object):
     """Mixin class to get view data of test case run"""
 
-    def get_case_run_logs(self, caserun):
-        content_type = self.get_case_run_content_type()
-        logs = TCMSLogModel.objects.filter(content_type=content_type,
-                                           object_pk=caserun.pk,
-                                           site_id=settings.SITE_ID)
-        return logs.values('date', 'who__username', 'action')
-
     def get_case_run_comments(self, caserun):
-        content_type = self.get_case_run_content_type()
+        content_type = ContentType.objects.get_for_model(TestCaseRun)
         comments = Comment.objects.filter(content_type=content_type,
                                           object_pk=caserun.pk,
                                           site_id=settings.SITE_ID,
                                           is_removed=False)
         return comments.values('user__username', 'submit_date', 'comment',
                                'pk', 'user__pk')
-
-    @staticmethod
-    def get_case_run_content_type():
-        return ContentType.objects.get_for_model(TestCaseRun)
