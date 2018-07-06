4e4cfece93c9a12070a62a03cfe936b010bb29fe
Mr. Senko <atodorov@mrsenko.com>
2018-07-06 10:07:28 +0300

    Remove unnecessary TCMSLogManager()
    now that we don't use the TCMSLogModel class directly we don't
    need a custom manager because all it does is filter the query set
    in the same way that TCMSActionModel.log() does.

--- tcms/core/logs/models.py
+++ tcms/core/logs/models.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.conf import settings
 
 from tcms.core.models.base import TCMSContentTypeBaseModel
-from .managers import TCMSLogManager
 
-
-# Create your models here.
 
 class TCMSLogModel(TCMSContentTypeBaseModel):
     who = models.ForeignKey(
@@ -15,16 +11,9 @@ class TCMSLogModel(TCMSContentTypeBaseModel):
     date = models.DateTimeField(auto_now_add=True)
     action = models.TextField()
 
-    objects = TCMSLogManager()
-
     class Meta:
         abstract = False
         index_together = (('content_type', 'object_pk', 'site'),)
 
     def __str__(self):
         return self.action
-
-    @classmethod
-    def get_logs_for_model(cls, model_class, object_pk):
-        content_type = ContentType.objects.get_for_model(model_class)
-        return cls.objects.filter(content_type=content_type, object_pk=object_pk)
