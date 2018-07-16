58a797e0994e6a8f925dbb2e871418692641c0f5
ivo0126 <ivayloivanov0926@gmail.com>
2018-07-16 19:37:38 +0300

    Fix the pylint errors in tcms.profiles.views

--- tcms/profiles/views.py
+++ tcms/profiles/views.py
@@ -23,22 +23,22 @@ from tcms.profiles.forms import UserProfileForm
 @csrf_protect
 def profile(request, username):
     """Edit the profiles of the user"""
-    u = get_object_or_404(User, username=username)
+    user = get_object_or_404(User, username=username)
 
     try:
-        up = UserProfile.get_user_profile(u)
+        user_profile = UserProfile.get_user_profile(user)
     except ObjectDoesNotExist:
-        up = UserProfile.objects.create(user=u)
-    form = UserProfileForm(instance=up)
+        user_profile = UserProfile.objects.create(user=user)
+    form = UserProfileForm(instance=user_profile)
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=up)
+        form = UserProfileForm(request.POST, instance=user_profile)
         if form.is_valid():
             form.save()
             messages.add_message(request,
                                  messages.SUCCESS,
                                  _('Information successfully updated'))
     context_data = {
-        'user_profile': up,
+        'user_profile': user_profile,
         'form': form,
     }
     return render(request, 'profile/info.html', context_data)
