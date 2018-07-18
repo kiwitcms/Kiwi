4f6c858297890e8c81602f19f99db8bdc7195dd0
Mr. Senko <atodorov@mrsenko.com>
2018-07-18 16:19:51 +0300

    Don't send emails for changes performed by myself. Fix #216
    this also requires tracking history for TestRun objects!

--- tcms/testplans/helpers/email.py
+++ tcms/testplans/helpers/email.py
@@ -15,16 +15,22 @@ def email_plan_update(plan):
 
 def get_plan_notification_recipients(plan):
     recipients = set()
-    if plan.owner:
-        if plan.emailing.auto_to_plan_owner:
+
+    if plan.owner and plan.emailing.auto_to_plan_owner:
             recipients.add(plan.owner.email)
+
     if plan.emailing.auto_to_plan_author:
         recipients.add(plan.author.email)
+
     if plan.emailing.auto_to_case_owner:
         case_authors = plan.case.values_list('author__email', flat=True)
         recipients.update(case_authors)  # pylint: disable=objects-update-used
+
     if plan.emailing.auto_to_case_default_tester:
         case_testers = plan.case.values_list('default_tester__email',
                                              flat=True)
         recipients.update(case_testers)  # pylint: disable=objects-update-used
+
+    # don't email author of last change
+    recipients.discard(getattr(plan.history.latest().history_user, 'email', ''))
     return list(filter(lambda e: bool(e), recipients))
