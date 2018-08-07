3a16025168ab5c7bf4371da45003ffb45a8e55c0
Anton Sankov <a.sankov96@gmail.com>
2018-08-07 19:08:41 +0300

    Fixed pylint issues in module tcms.xmlrpc.tests.test_version

--- tcms/xmlrpc/tests/test_version.py
+++ tcms/xmlrpc/tests/test_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=attribute-defined-outside-init, invalid-name
+# pylint: disable=attribute-defined-outside-init, invalid-name, avoid-list-comprehension
 
 from xmlrpc.client import ProtocolError
 from xmlrpc.client import Fault as XmlRPCFault
