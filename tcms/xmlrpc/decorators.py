da80461ee42f560aab7770b735e18284f18a3b8c
Mr. Senko <atodorov@mrsenko.com>
2018-09-20 22:59:44 +0300

    Remove useless else clause which assigns variable to itself

--- tcms/xmlrpc/decorators.py
+++ tcms/xmlrpc/decorators.py
@@ -5,8 +5,6 @@ def permissions_required(perm):
     def check_perms(request, permissions):
         if isinstance(permissions, str):
             permissions = (permissions, )
-        else:
-            permissions = permissions
 
         # check if the user has the permission (even anon users)
         return request.user.has_perms(permissions)
