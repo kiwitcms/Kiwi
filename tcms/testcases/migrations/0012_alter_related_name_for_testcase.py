6bf455810e441cb63cac7fa372323eb509cd2841
Mr. Senko <atodorov@mrsenko.com>
2018-10-17 14:22:59 +0300

    [db] Squash migrations for testcases app

--- tcms/testcases/migrations/0012_alter_related_name_for_testcase.py
+++ tcms/testcases/migrations/0012_alter_related_name_for_testcase.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('testcases', '0011_delete_attachments'),
+        ('testcases', '0002_squashed'),
     ]
 
     operations = [
