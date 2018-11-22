722778ed67bb77cd300431761cd03ebca981c167
Mr. Senko <atodorov@mrsenko.com>
2018-11-22 23:40:54 +0200

    Add tests for Docker container
    Note: install beakerlib from a fork b/c
    https://github.com/beakerlib/beakerlib/pull/11 is still not merged

---
+++ tests/runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Note: execute this file from the project root directory
+
+# setup
+rm -rf /var/tmp/beakerlib-*/
+export BEAKERLIB_JOURNAL=0
+
+# install beakerlib from a fork b/c
+# https://github.com/beakerlib/beakerlib/pull/11
+if [ ! -f "/usr/share/beakerlib/beakerlib.sh" ]; then
+    curl -o- https://raw.githubusercontent.com/atodorov/beakerlib/web-install/install.sh | bash
+fi
+
+# execute test scripts
+./tests/test_docker.sh
+
+
+# look for failures
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
+
+# explicit return code for Makefile
+exit 0
