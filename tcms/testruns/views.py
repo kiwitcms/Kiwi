kiwitcms/Kiwi

commit 3ab1657841e236932f9727ce26d5a02112563270

[pylint] white-list objects-update-used
because this checker is a bit verbose and sometimes produces
false positives!

also fix a couple of other pylint issues which help us not use
.update as often

tcms/testruns/views.py
@@ -773,12 +773,10 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
             self.request = request
             self.case_run = case_run
             self.template_name = template_name
-            self.default_ajax_response = {'rc': 0, 'response': 'ok'}
 
         def add(self):
             if not self.request.user.has_perm('testcases.add_bug'):
-                response = {'rc': 1, 'response': 'Permission denied'}
-                return self.ajax_response(response=response)
+                return JsonResponse({'rc': 1, 'response': 'Permission denied'})
 
             bug_id = request.GET.get('bug_id')
             bug_system_id = request.GET.get('bug_system_id')
@@ -786,10 +784,8 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
             try:
                 validate_bug_id(bug_id, bug_system_id)
             except ValidationError as error:
-                return self.ajax_response({
-                    'rc': 1,
-                    'response': str(error)
-                })
+                return JsonResponse({'rc': 1,
+                                     'response': str(error)})
 
             bz_external_track = True if request.GET.get('bz_external_track',
                                                         False) else False
@@ -800,21 +796,13 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                             bz_external_track=bz_external_track)
             except Exception as error:
                 msg = str(error) if str(error) else 'Failed to add bug %s' % bug_id
-                return self.ajax_response({
-                    'rc': 1,
-                    'response': msg
-                })
+                return JsonResponse({'rc': 1,
+                                     'response': msg})
 
-            self.default_ajax_response.update({
-                'run_bug_count': self.get_run_bug_count(),
-                'caserun_bugs_count': self.case_run.get_bugs_count(),
-            })
-            return self.ajax_response()
-
-        def ajax_response(self, response=None):
-            if not response:
-                response = self.default_ajax_response
-            return JsonResponse(response)
+            return JsonResponse({'rc': 0,
+                                 'response': 'ok',
+                                 'run_bug_count': self.get_run_bug_count(),
+                                 'caserun_bugs_count': self.case_run.get_bugs_count()})
 
         def file(self):
             bug_system_id = request.GET.get('bug_system_id')
@@ -824,11 +812,10 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                 tracker = IssueTrackerType.from_name(bug_system.tracker_type)(bug_system)
                 url = tracker.report_issue_from_testcase(self.case_run)
                 response = {'rc': 0, 'response': url}
-            else:
-                response = {'rc': 1, 'response': 'Enable reporting to this Issue Tracker '
-                                                 'by configuring its base_url!'}
 
-            return self.ajax_response(response)
+            response = {'rc': 1, 'response': 'Enable reporting to this Issue Tracker '
+                                             'by configuring its base_url!'}
+            return JsonResponse(response)
 
         def remove(self):
             if not self.request.user.has_perm('testcases.delete_bug'):
@@ -840,12 +827,11 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                 run_id = self.request.GET.get('case_run')
                 self.case_run.remove_bug(bug_id, run_id)
             except ObjectDoesNotExist as error:
-                response = {'rc': 1, 'response': str(error)}
-                return self.ajax_response(response=response)
+                return JsonResponse({'rc': 1, 'response': str(error)})
 
-            self.default_ajax_response[
-                'run_bug_count'] = self.get_run_bug_count()
-            return self.ajax_response()
+            return JsonResponse({'rc': 0,
+                                 'response': 'ok',
+                                 'run_bug_count': self.get_run_bug_count()})
 
         def render_form(self):
             form = CaseBugForm(initial={
@@ -871,9 +857,8 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                              template_name=template_name)
 
     if not request.GET.get('a') in crba.__all__:
-        return crba.ajax_response(response={
-            'rc': 1,
-            'response': 'Unrecognizable actions'})
+        return JsonResponse({'rc': 1,
+                             'response': 'Unrecognizable actions'})
 
     func = getattr(crba, request.GET['a'])
     return func()
@@ -1212,9 +1197,10 @@ def env_value(request):
 
             fragment = render(request, "run/get_environment.html",
                               {"test_run": self.test_runs[0], "is_ajax": True})
-            self.ajax_response.update({"fragment": str(fragment.content,
+            self.ajax_response.update({  # pylint: disable=objects-update-used
+                                       "fragment": str(fragment.content,
                                                        encoding=settings.DEFAULT_CHARSET)})
-            return HttpResponse(json.dumps(self.ajax_response))
+            return JsonResponse(self.ajax_response)
 
         def remove(self):
             chk_perm = self.has_no_perm('delete')
@@ -1226,12 +1212,12 @@ def env_value(request):
                     request.GET.get('env_value_id')
                 ))
 
-            return HttpResponse(json.dumps(self.ajax_response))
+            return JsonResponse(self.ajax_response)
 
         def change(self):
             chk_perm = self.has_no_perm('change')
             if chk_perm:
-                return HttpResponse(json.dumps(chk_perm))
+                return JsonResponse(chk_perm)
 
             for test_run in self.test_runs:
                 test_run.remove_env_value(env_value=self.get_env_value(
@@ -1241,7 +1227,7 @@ def env_value(request):
                     request.GET.get('new_env_value_id')
                 ))
 
-            return HttpResponse(json.dumps(self.ajax_response))
+            return JsonResponse(self.ajax_response)
 
         @staticmethod
         def get_env_value(env_value_id):
@@ -1251,7 +1237,7 @@ def env_value(request):
 
     if request.GET.get('a') not in run_env_actions.__all__:
         ajax_response = {'rc': 1, 'response': 'Unrecognizable actions'}
-        return HttpResponse(json.dumps(ajax_response))
+        return JsonResponse(ajax_response)
 
     func = getattr(run_env_actions, request.GET['a'])
     return func()

tcms/testcases/views.py
@@ -1272,7 +1272,7 @@ def text_history(request, case_id, template_name='case/history.html'):
                             'author__email',
                             'create_date').order_by('-case_text_version')
 
-    context_data = {
+    context = {
         'testplan': tp,
         'testcase': tc,
         'test_case_texts': tctxts.iterator(),
@@ -1286,7 +1286,7 @@ def text_history(request, case_id, template_name='case/history.html'):
                                            'setup',
                                            'breakdown')
 
-        context_data.update({
+        context.update({
             'select_case_text_version': case_text_version,
             'text_to_show': text_to_show.iterator(),
         })
@@ -1295,7 +1295,7 @@ def text_history(request, case_id, template_name='case/history.html'):
         # selected text history
         pass
 
-    return render(request, template_name, context_data)
+    return render(request, template_name, context)
 
 
 @permission_required('testcases.add_testcase')
@@ -1478,13 +1478,13 @@ def clone(request, template_name='case/clone.html'):
         search_plan_form.populate(product_id=tp.product_id)
 
     submit_action = request_data.get('submit', None)
-    context_data = {
+    context = {
         'test_plan': tp,
         'search_form': search_plan_form,
         'clone_form': clone_form,
         'submit_action': submit_action,
     }
-    return render(request, template_name, context_data)
+    return render(request, template_name, context)
 
 
 @require_POST
@@ -1519,22 +1519,22 @@ def attachment(request, case_id, template_name='case/attachment.html'):
     tc = get_object_or_404(TestCase, case_id=case_id)
     tp = plan_from_request_or_none(request)
 
-    context_data = {
+    context = {
         'testplan': tp,
         'testcase': tc,
         'limit': settings.FILE_UPLOAD_MAX_SIZE,
     }
-    return render(request, template_name, context_data)
+    return render(request, template_name, context)
 
 
 def get_log(request, case_id, template_name="management/get_log.html"):
     """Get the case log"""
     tc = get_object_or_404(TestCase, case_id=case_id)
 
-    context_data = {
+    context = {
         'object': tc
     }
-    return render(request, template_name, context_data)
+    return render(request, template_name, context)
 
 
 @permission_required('testcases.change_bug')
@@ -1561,11 +1561,11 @@ def bug(request, case_id, template_name='case/get_bug.html'):
             return HttpResponse(form.as_p())
 
         def render(self, response=None):
-            context_data = {
+            context = {
                 'test_case': self.case,
                 'response': response
             }
-            return render(request, template_name, context_data)
+            return render(request, template_name, context)
 
         def add(self):
             # FIXME: It's may use ModelForm.save() method here.
@@ -1625,31 +1625,31 @@ def plan(request, case_id):
     if request.GET.get('a'):
         # Search the plans from database
         if not request.GET.getlist('plan_id'):
-            context_data = {
+            context = {
                 'message': 'The case must specific one plan at leaset for '
                            'some action',
             }
             return render(
                 request,
                 'case/get_plan.html',
-                context_data)
+                context)
 
         tps = TestPlan.objects.filter(pk__in=request.GET.getlist('plan_id'))
 
         if not tps:
-            context_data = {
+            context = {
                 'testplans': tps,
                 'message': 'The plan id are not exist in database at all.'
             }
             return render(
                 request,
                 'case/get_plan.html',
-                context_data)
+                context)
 
         # Add case plan action
         if request.GET['a'] == 'add':
             if not request.user.has_perm('testcases.add_testcaseplan'):
-                context_data = {
+                context = {
                     'test_case': tc,
                     'test_plans': tps,
                     'message': 'Permission denied',
@@ -1657,7 +1657,7 @@ def plan(request, case_id):
                 return render(
                     request,
                     'case/get_plan.html',
-                    context_data)
+                    context)
 
             for tp in tps:
                 tc.add_to_plan(tp)
@@ -1665,7 +1665,7 @@ def plan(request, case_id):
         # Remove case plan action
         if request.GET['a'] == 'remove':
             if not request.user.has_perm('testcases.change_testcaseplan'):
-                context_data = {
+                context = {
                     'test_case': tc,
                     'test_plans': tps,
                     'message': 'Permission denied',
@@ -1673,7 +1673,7 @@ def plan(request, case_id):
                 return render(
                     request,
                     'case/get_plan.html',
-                    context_data)
+                    context)
 
             for tp in tps:
                 tc.remove_plan(tp)
@@ -1683,11 +1683,11 @@ def plan(request, case_id):
                              'type',
                              'product')
 
-    context_data = {
+    context = {
         'test_case': tc,
         'test_plans': tps,
     }
     return render(
         request,
         'case/get_plan.html',
-        context_data)
+        context)

tcms/core/models/fields.py
@@ -3,6 +3,6 @@ try:
     # may not be running under MySQL
     from MySQLdb.constants import FIELD_TYPE
     from django.db.backends.mysql.base import django_conversions
-    django_conversions.update({FIELD_TYPE.TIME: None})
+    django_conversions.update({FIELD_TYPE.TIME: None})  # pylint: disable=objects-update-used
 except ImportError:
     pass

tcms/core/utils/checksum.py
@@ -6,5 +6,5 @@ def checksum(value):
     if value is None:
         return ''
     _checksum = hashlib.sha256()
-    _checksum.update(value.encode("UTF-8"))
+    _checksum.update(value.encode("UTF-8"))  # pylint: disable=objects-update-used
     return _checksum.hexdigest()

tcms/settings/test/mysql.py
@@ -1,4 +1,4 @@
-# pylint: disable=wildcard-import, unused-wildcard-import
+# pylint: disable=wildcard-import, unused-wildcard-import, objects-update-used
 
 from tcms.settings.test import *  # noqa: F403
 

tcms/settings/test/postgresql.py
@@ -1,4 +1,4 @@
-# pylint: disable=wildcard-import, unused-wildcard-import
+# pylint: disable=wildcard-import, unused-wildcard-import, objects-update-used
 
 from tcms.settings.test import *  # noqa: F403
 

tcms/testcases/helpers/email.py
@@ -39,12 +39,12 @@ def get_case_notification_recipients(case):
         recipients.add(case.default_tester.email)
     if case.emailing.auto_to_run_manager:
         managers = case.case_run.values_list('run__manager__email', flat=True)
-        recipients.update(managers)
+        recipients.update(managers)  # pylint: disable=objects-update-used
     if case.emailing.auto_to_run_tester:
         run_testers = case.case_run.values_list('run__default_tester__email',
                                                 flat=True)
-        recipients.update(run_testers)
+        recipients.update(run_testers)  # pylint: disable=objects-update-used
     if case.emailing.auto_to_case_run_assignee:
         assignees = case.case_run.values_list('assignee__email', flat=True)
-        recipients.update(assignees)
+        recipients.update(assignees)  # pylint: disable=objects-update-used
     return list(filter(lambda e: bool(e), recipients))

tcms/testplans/helpers/email.py
@@ -22,9 +22,9 @@ def get_plan_notification_recipients(plan):
         recipients.add(plan.author.email)
     if plan.emailing.auto_to_case_owner:
         case_authors = plan.case.values_list('author__email', flat=True)
-        recipients.update(case_authors)
+        recipients.update(case_authors)  # pylint: disable=objects-update-used
     if plan.emailing.auto_to_case_default_tester:
         case_testers = plan.case.values_list('default_tester__email',
                                              flat=True)
-        recipients.update(case_testers)
+        recipients.update(case_testers)  # pylint: disable=objects-update-used
     return list(filter(lambda e: bool(e), recipients))

commit f4d2b835a9a6dd25b56d080f40caf0ab6f0972fc

Fix bug with report to Bugzilla, SO #53434949
this has been introduced in 3ab1657 and reported in
https://stackoverflow.com/questions/53434949/kiwi-reporting-to-bugzilla-issue

tcms/testruns/views.py
@@ -448,6 +448,9 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                                  'caserun_bugs_count': self.case_run.get_bugs_count()})
 
         def file(self):
+            response = {'rc': 1, 'response': 'Enable reporting to this Issue Tracker '
+                                             'by configuring its base_url!'}
+
             bug_system_id = request.GET.get('bug_system_id')
             bug_system = BugSystem.objects.get(pk=bug_system_id)
 
@@ -456,8 +459,6 @@ def bug(request, case_run_id, template_name='run/execute_case_run.html'):
                 url = tracker.report_issue_from_testcase(self.case_run)
                 response = {'rc': 0, 'response': url}
 
-            response = {'rc': 1, 'response': 'Enable reporting to this Issue Tracker '
-                                             'by configuring its base_url!'}
             return JsonResponse(response)
 
         def remove(self):