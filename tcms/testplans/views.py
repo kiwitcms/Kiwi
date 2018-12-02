ab10c83cf21a72c441fa1f7cfcf84ee679c4da0b
asankov <a.sankov96@gmail.com>
2018-12-02 17:56:18 +0200

    Create new TestPlan view with Patternfly

--- tcms/testplans/views.py
+++ tcms/testplans/views.py
@@ -53,7 +53,7 @@ def update_plan_email_settings(test_plan, form):
 
 @require_http_methods(['GET', 'POST'])
 @permission_required('testplans.add_testplan')
-def new(request, template_name='plan/new.html'):
+def new(request):
     """New testplan"""
 
     # If the form has been submitted...
@@ -87,7 +87,7 @@ def new(request, template_name='plan/new.html'):
     context_data = {
         'form': form,
     }
-    return render(request, template_name, context_data)
+    return render(request, 'testplans/mutable.html', context_data)
 
 
 @require_GET
