482824c21d056c7a3a23f906559f1c519afd64f9
asankov <a.sankov96@gmail.com>
2018-12-31 18:37:01 +0200

    Refactor testplans.views.new to class based view
    Convert function type view testplans/views::new to class-based view NewTestPlan

--- tcms/testplans/urls/plans_urls.py
+++ tcms/testplans/urls/plans_urls.py
@@ -4,11 +4,10 @@ from django.conf.urls import url
 
 from tcms.testplans import views
 
-
 urlpatterns = [
     url(r'^$', views.get_all, name='plans-all'),
     url(r'^search/$', views.search, name='plans-search'),
-    url(r'^new/$', views.new, name='plans-new'),
+    url(r'^new/$', views.NewTestPlanView.as_view(), name='plans-new'),
     url(r'^clone/$', views.clone, name='plans-clone'),
     url(r'^printable/$', views.printable, name='plans-printable'),
 ]
