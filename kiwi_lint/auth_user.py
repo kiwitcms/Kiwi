5d4c7933a12a2422c2503a03155ee4ce1f8fa808
Mr. Senko <atodorov@mrsenko.com>
2019-01-05 00:38:04 +0200

    Warn about importing the stock User model
    because this makes the application less customizeable

--- kiwi_lint/auth_user.py
+++ kiwi_lint/auth_user.py
@@ -15,7 +15,11 @@ class AuthUserChecker(checkers.BaseChecker):
     msgs = {'E4441': ("Hard-coded 'auth.User'",
                       'hard-coded-auth-user',
                       "Don't hard-code the auth.User model. "
-                      "Use settings.AUTH_USER_MODEL instead!")}
+                      "Use settings.AUTH_USER_MODEL instead!"),
+            'E4442': ("User model imported from django.contrib.auth.models",
+                      'imported-auth-user',
+                      "Don't import django.contrib.auth.models.User model. "
+                      "Use django.contrib.auth.get_user_model() instead!")}
 
     @utils.check_messages('hard-coded-auth-user')
     def visit_const(self, node):
@@ -23,3 +27,10 @@ class AuthUserChecker(checkers.BaseChecker):
         # because the user model should not be hard-coded anywhere
         if node.value == 'auth.User':
             self.add_message('hard-coded-auth-user', node=node)
+
+    def visit_importfrom(self, node):
+        if node.modname == 'django.contrib.auth.models':
+            for imported_names in node.names:
+                if imported_names[0] in ['*', 'User']:
+                    self.add_message('imported-auth-user', node=node)
+                    break
