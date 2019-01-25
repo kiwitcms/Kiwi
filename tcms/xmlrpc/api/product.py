c2385d577283194c4aa537c4dddc46a2364b6d1c
Mr. Senko <atodorov@mrsenko.com>
2019-01-25 23:47:37 +0200

    [api] New method Product.create()

--- tcms/xmlrpc/api/product.py
+++ tcms/xmlrpc/api/product.py
@@ -3,6 +3,30 @@
 from modernrpc.core import rpc_method
 
 from tcms.management.models import Product
+from tcms.xmlrpc.decorators import permissions_required
+
+
+__all__ = (
+    'create',
+    'filter',
+)
+
+
+@rpc_method(name='Product.create')
+@permissions_required('management.add_product')
+def create(values):
+    """
+    .. function:: XML-RPC Product.create(values)
+
+        Create a new Product object and store it in the database.
+
+        :param values: Field values for :class:`tcms.management.models.Product`
+        :type values: dict
+        :return: Serialized :class:`tcms.management.models.Product` object
+        :rtype: dict
+        :raises: PermissionDenied if missing *management.add_product* permission
+    """
+    return Product.objects.create(**values).serialize()
 
 
 @rpc_method(name='Product.filter')
