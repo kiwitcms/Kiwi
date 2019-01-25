fc2221c45d0f1b2112e8b1c525fcee9adc505062
Mr. Senko <atodorov@mrsenko.com>
2019-01-25 21:58:41 +0200

    [api] new method TestCaseRunStatus.filter()

--- tcms/settings/common.py
+++ tcms/settings/common.py
@@ -307,6 +307,7 @@ MODERNRPC_METHODS_MODULES = [
     'tcms.xmlrpc.api.tag',
     'tcms.xmlrpc.api.testcase',
     'tcms.xmlrpc.api.testcaserun',
+    'tcms.xmlrpc.api.testcaserunstatus',
     'tcms.xmlrpc.api.testcasestatus',
     'tcms.xmlrpc.api.testplan',
     'tcms.xmlrpc.api.testrun',
