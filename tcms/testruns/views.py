8c799fcecce2ed951ee5bf7af10f6fa5381d6fdf
Mr. Senko <atodorov@mrsenko.com>
2019-07-31 12:51:56 +0300

    TestRun page - TE row - update how we calculate & check for bugs
    - this is only used for visual cues on the page aka showing the
      bug icon for every TE row which has bugs reported.
    
    - show bug icon if URL is a defect
    
    - remove legacy section that displays bugs in TE container. Refs #475
    
    - update filter field

--- tcms/testruns/views.py
+++ tcms/testruns/views.py
@@ -33,6 +33,7 @@ from tcms.testruns.data import TestExecutionDataMixin
 from tcms.testruns.forms import NewRunForm, SearchRunForm, BaseRunForm
 from tcms.testruns.models import TestRun, TestExecution, TestExecutionStatus
 from tcms.issuetracker.types import IssueTrackerType
+from tcms.core.contrib.linkreference.models import LinkReference
 
 
 User = get_user_model()  # pylint: disable=invalid-name
@@ -154,10 +155,6 @@ def _open_run_get_executions(request, run):  # pylint: disable=missing-permissio
            'case__category__name'
            )
 
-    # Get the bug count for each execution
-    # 5. have to show the number of bugs of each execution
-    executions = executions.annotate(num_bug=Count('case_run_bug', distinct=True))
-
     # todo: is this last distinct necessary
     executions = executions.distinct()
 
@@ -247,13 +244,15 @@ def get(request,  # pylint: disable=missing-permission-required
         comments_subtotal = open_run_get_comments_subtotal(execution_pks)
         status = TestExecutionStatus.get_names()
 
-        for case_run in test_executions:
-            yield (case_run,
-                   testers.get(case_run.tested_by_id, None),
-                   assignees.get(case_run.assignee_id, None),
-                   priorities.get(case_run.case.priority_id),
-                   status[case_run.status_id],
-                   comments_subtotal.get(case_run.pk, 0))
+        for execution in test_executions:
+            yield (execution,
+                   testers.get(execution.tested_by_id, None),
+                   assignees.get(execution.assignee_id, None),
+                   priorities.get(execution.case.priority_id),
+                   status[execution.status_id],
+                   comments_subtotal.get(execution.pk, 0),
+                   LinkReference.objects.filter(is_defect=True,
+                                                execution=execution.pk).count())
 
     context_data = {
         'test_run': test_run,
