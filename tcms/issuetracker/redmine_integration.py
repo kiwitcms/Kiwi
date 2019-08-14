773bfb4ceb0ab4194ce9cf0e45417e06fe51f1a4
Mr. Senko <atodorov@mrsenko.com>
2019-08-14 22:10:38 +0300

    IssueTracker integration - point back to TR+TE instead of TC
    when adding a reverse relationship to existing defects use the
    TestRun ID/summary/URL and TestExecution ID/summary to provide
    more information how this issue was reproduced. This provides more
    detailed information about the reproducer (version, environment, etc)
    instead of just linking to a TestCase without any specific product
    details like we did in the past!

--- tcms/issuetracker/redmine_integration.py
+++ tcms/issuetracker/redmine_integration.py
@@ -14,27 +14,30 @@ class RedmineThread(threading.Thread):
         Executed from the IssueTracker interface methods.
     """
 
-    def __init__(self, rpc, testcase, bug):
+    def __init__(self, rpc, execution, bug_id):
         """
             @rpc - Redmine Python/API object
-            @testcase - TestCase object
+            @execution - TestExecution object
             @bug - Bug object
         """
-
         self.rpc = rpc
-        self.testcase = testcase
-        self.bug = bug
+        self.execution = execution
+        self.bug_id = bug_id
         super().__init__()
 
     def run(self):
         """
             Link the test case with the issue!
         """
-
         try:
-            text = """---- Issue confirmed via test case ----
-URL: %s
-Summary: %s""" % (self.testcase.get_full_url(), self.testcase.summary)
+            text = """---- Confirmed via test execution ----
+TR-%d: %s
+%s
+TE-%d: %s""" % (self.execution.run.pk,
+                self.execution.run.summary,
+                self.execution.run.get_full_url(),
+                self.execution.pk,
+                self.execution.case.summary)
 
             self.rpc.issue.get(self.bug.bug_id).save(notes=text)
         except Exception as err:  # pylint: disable=broad-except
