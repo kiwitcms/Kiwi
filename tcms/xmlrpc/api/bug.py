7dccb9a5b58b598ba9439b9ec8232e0d3b35b557
Mr. Senko <atodorov@mrsenko.com>
2019-08-25 16:46:01 +0300

    [API] New method Bug.details(). Refs #117
    this method together with the underlying IssueTracker.details()
    will be the foundation of how Kiwi TCMS fetches more details from
    the bug tracking system.
    
    At the moment the intention is for this information to be shown in
    the UI but it can also be consumed independently via the API.
    
    By default we're using OpenGraph protocol to collect the data that
    will be shown. The details() method can be overriden for each bug
    tracker that is supported and you may also override the entire class
    with something you have created yourself.

--- tcms/xmlrpc/api/bug.py
+++ tcms/xmlrpc/api/bug.py
@@ -6,11 +6,31 @@ from django.utils.translation import ugettext_lazy as _
 from tcms.testcases.models import BugSystem
 from tcms.testruns.models import TestExecution
 from tcms.issuetracker.types import IssueTrackerType
+from tcms.xmlrpc.api.utils import tracker_from_url
 
 
 __all__ = (
+    'details',
     'report',
 )
+
+
+@rpc_method(name='Bug.details')
+def details(url):
+    """
+    .. function:: XML-RPC Bug.details(url)
+
+        Returns details about bug at the given URL address. This method is
+        used when generating additional information which is shown in the UI.
+
+        :param url: URL address
+        :type url: str
+        :return: Detailed information about this URL. Depends on the underlying
+                 issue tracker.
+        :rtype: dict
+    """
+    tracker = tracker_from_url(url)
+    return tracker.details(url)
 
 
 @rpc_method(name='Bug.report')
