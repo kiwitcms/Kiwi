76a0d5149b8c7d5ccd90c75aa3f5d280c6941b32
Mr. Senko <atodorov@mrsenko.com>
2019-09-14 21:57:03 +0300

    Move TC Executions card into separate template

--- tcms/static/js/utils.js
+++ tcms/static/js/utils.js
@@ -149,3 +149,21 @@ function escapeHTML(unsafe) {
     })[m]
   });
 }
+
+
+function treeViewBind() {
+    // collapse all child rows
+    $('.tree-list-view-pf').find(".list-group-item-container").addClass('hidden');
+
+    // click the list-view heading then expand a row
+    $('.list-group-item-header').click(function (event) {
+      if(!$(event.target).is('button, a, input, .fa-ellipsis-v')) {
+        var $this = $(this);
+        $this.find('.fa-angle-right').toggleClass('fa-angle-down');
+        var $itemContainer = $this.siblings('.list-group-item-container');
+        if ($itemContainer.children().length) {
+          $itemContainer.toggleClass('hidden');
+        }
+      }
+    });
+}
