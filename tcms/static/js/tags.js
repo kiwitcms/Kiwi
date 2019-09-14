e1f16aa31bf8ed1e4ac4d57d0524f4f75317be52
Mr. Senko <atodorov@mrsenko.com>
2019-09-14 16:28:30 +0300

    Separate tags card from main template
    this is going to be used in several other places so it makes
    sense to separate it as a stand-alone piece which can be included
    where necessary.

---
+++ tcms/static/js/tags.js
@@ -0,0 +1,102 @@
+/*
+    Applies tag to the chosen model
+
+    @model - string - model name which accepts tags. There must
+        be a 'MM.add_tag' RPC function for this to work!
+    @object_id - int - PK of the object that will be tagged
+    @tag_input - jQuery object - usually an <input> element which
+        provides the value used for tagging
+    @to_table - DataTable object - the table which displays the results
+*/
+function addTag(model, object_id, tag_input, to_table) {
+    var tag_name = tag_input.value;
+
+    if (tag_name.length > 0) {
+        jsonRPC(model + '.add_tag', [object_id, tag_name], function(data) {
+            to_table.row.add({name: tag_name}).draw();
+            $(tag_input).val('');
+        });
+    }
+}
+
+
+/*
+    Displays the tags table inside a card and binds all buttons
+    and actions for it.
+
+    @model - string - model name which accepts tags. There must
+        be a 'MM.add_tag' RPC function for this to work!
+    @object_id - int - PK of the object that will be tagged
+    @display_filter - dict - passed directly to `Tag.filter` to display
+        tags for @object_id
+    @perm_remove - bool - if we have permission to remove tags
+
+*/
+function tagsCard(model, object_id, display_filter, perm_remove) {
+    // load the tags table
+    var tags_table = $('#tags').DataTable({
+        ajax: function(data, callback, settings) {
+            dataTableJsonRPC('Tag.filter', display_filter, callback);
+        },
+        columns: [
+            { data: "name" },
+            {
+                data: null,
+                sortable: false,
+                render: function (data, type, full, meta) {
+                    if (perm_remove) {
+                        return '<a href="#tags" class="remove-tag" data-name="' + data.name  + '"><span class="pficon-error-circle-o"></span></a>';
+                    }
+                    return '';
+                }
+            },
+        ],
+        dom: "t",
+        language: {
+            loadingRecords: '<div class="spinner spinner-lg"></div>',
+            processing: '<div class="spinner spinner-lg"></div>',
+            zeroRecords: "No records found"
+        },
+        order: [[ 0, 'asc' ]],
+    });
+
+
+    // remove tags button
+    tags_table.on('draw', function() {
+        $('.remove-tag').click(function() {
+            var tr = $(this).parents('tr');
+
+            jsonRPC(model + '.remove_tag', [object_id, $(this).data('name')], function(data) {
+                tags_table.row($(tr)).remove().draw();
+            });
+        });
+    });
+
+    // add tag button and Enter key
+    $('#add-tag').click(function() {
+        addTag(model, object_id, $('#id_tags')[0], tags_table);
+    });
+
+    $('#id_tags').keyup(function(event) {
+        if (event.keyCode === 13) {
+            addTag(model, object_id, $('#id_tags')[0], tags_table);
+        };
+    });
+
+    // tag autocomplete
+    $('#id_tags.typeahead').typeahead({
+        minLength: 3,
+        highlight: true
+        }, {
+        name: 'tags-autocomplete',
+        source: function(query, processSync, processAsync) {
+            jsonRPC('Tag.filter', {name__startswith: query}, function(data) {
+                var processedData = [];
+                data.forEach(function(element) {
+                    processedData.push(element.name);
+                });
+                return processAsync(processedData);
+            });
+        }
+    });
+}
--- tcms/testcases/static/testcases/js/get.js
+++ tcms/testcases/static/testcases/js/get.js
@@ -1,18 +1,6 @@
 const bug_details_cache = {}
 
 
-function addTag(module, object_id, tag_input, to_table) {
-    var tag_name = tag_input.value;
-
-    if (tag_name.length > 0) {
-        jsonRPC(module + '.add_tag', [object_id, tag_name], function(data) {
-            to_table.row.add({name: tag_name}).draw();
-            $(tag_input).val('');
-        });
-    }
-}
-
-
 function addComponent(object_id, _input, to_table) {
     var _name = _input.value;
 
@@ -37,72 +25,8 @@ $(document).ready(function() {
     var perm_remove_plan = $('#test_case_pk').data('perm-remove-plan') === 'True';
     var perm_remove_bug = $('#test_case_pk').data('perm-remove-bug') === 'True';
 
-
-    // tags table
-    var tags_table = $('#tags').DataTable({
-        ajax: function(data, callback, settings) {
-            dataTableJsonRPC('Tag.filter', {case: case_id}, callback);
-        },
-        columns: [
-            { data: "name" },
-            {
-                data: null,
-                sortable: false,
-                render: function (data, type, full, meta) {
-                    if (perm_remove_tag) {
-                        return '<a href="#tags" class="remove-tag" data-name="' + data.name  + '"><span class="pficon-error-circle-o"></span></a>';
-                    }
-                    return '';
-                }
-            },
-        ],
-        dom: "t",
-        language: {
-            loadingRecords: '<div class="spinner spinner-lg"></div>',
-            processing: '<div class="spinner spinner-lg"></div>',
-            zeroRecords: "No records found"
-        },
-        order: [[ 0, 'asc' ]],
-    });
-
-    // remove tags button
-    tags_table.on('draw', function() {
-        $('.remove-tag').click(function() {
-            var tr = $(this).parents('tr');
-
-            jsonRPC('TestCase.remove_tag', [case_id, $(this).data('name')], function(data) {
-                tags_table.row($(tr)).remove().draw();
-            });
-        });
-    });
-
-    // add tag button and Enter key
-    $('#add-tag').click(function() {
-        addTag('TestCase', case_id, $('#id_tags')[0], tags_table);
-    });
-
-    $('#id_tags').keyup(function(event) {
-        if (event.keyCode === 13) {
-            addTag('TestCase', case_id, $('#id_tags')[0], tags_table);
-        };
-    });
-
-    // tag autocomplete
-    $('#id_tags.typeahead').typeahead({
-        minLength: 3,
-        highlight: true
-        }, {
-        name: 'tags-autocomplete',
-        source: function(query, processSync, processAsync) {
-            jsonRPC('Tag.filter', {name__startswith: query}, function(data) {
-                var processedData = [];
-                data.forEach(function(element) {
-                    processedData.push(element.name);
-                });
-                return processAsync(processedData);
-            });
-        }
-    });
+    // bind everything in tags table
+    tagsCard('TestCase', case_id, {case: case_id}, perm_remove_tag);
 
     // components table
     var components_table = $('#components').DataTable({
