a15a4395f902a547dc9b10d1c98cabc6a56208f8
Mr. Senko <atodorov@mrsenko.com>
2019-10-18 21:13:20 +0300

    tests: Permission tests for bugs.views.New. Refs #1141

---
+++ tcms/bugs/tests/test_permissions.py
@@ -0,0 +1,46 @@
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from tcms import tests
+from tcms.tests import factories
+from tcms.bugs.models import Bug
+
+
+class NewTestCase(tests.PermissionsTestCase):
+    permission_label = 'bugs.add_bug'
+    url = reverse('bugs-new')
+    http_method_names = ['get', 'post']
+
+    @classmethod
+    def setUpTestData(cls):
+        version = factories.VersionFactory()
+        build = factories.BuildFactory(product=version.product)
+
+        cls.post_data = {
+            'summary': 'Bug created by test suite',
+            'product': version.product.pk,
+            'version': version.pk,
+            'build': build.pk,
+        }
+
+        super().setUpTestData()
+
+        # cls.tester is created after calling super() above
+        cls.post_data['reporter'] = cls.tester.pk
+
+    def verify_get_with_permission(self):
+        response = self.client.get(self.url)
+
+        self.assertContains(response, _('New bug'))
+        self.assertContains(response, 'bugs/js/mutable.js')
+
+    def verify_post_with_permission(self):
+        initial_count = Bug.objects.count()
+
+        response = self.client.post(self.url, self.post_data, follow=True)
+
+        last_bug = Bug.objects.last()
+        self.assertEqual(initial_count + 1, Bug.objects.count())
+        self.assertEqual(self.tester, last_bug.reporter)
+        self.assertContains(response, 'Bug created by test suite')
+        self.assertContains(response, 'BUG-%d' % last_bug.pk)
