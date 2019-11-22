f03c68f8479ef12af23c547f331e6efc1df0b52d
Mr. Senko <atodorov@mrsenko.com>
2019-11-22 21:43:19 +0200

    Cosmetic changes in automated test. Refs #959

--- tcms/core/tests/test_helpers.py
+++ tcms/core/tests/test_helpers.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django_comments.models import Comment
 
 from tcms.tests import BasePlanCase
+from tcms.tests.factories import UserFactory
 from tcms.core.helpers.comments import add_comment
 
 
@@ -13,11 +13,9 @@ class TestAddComments(BasePlanCase):
 
     @classmethod
     def setUpTestData(cls):
-        super(TestAddComments, cls).setUpTestData()
+        super().setUpTestData()
 
-        cls.reviewer = User.objects.create_user(
-            username='reviewer',
-            email='reviewer@example.com')
+        cls.reviewer = UserFactory()
 
     def test_add_comment_to_case(self):
         cases = [self.case_1, self.case_2]
@@ -27,9 +25,9 @@ class TestAddComments(BasePlanCase):
             case_ct = ContentType.objects.get_for_model(case.__class__)
             comments = Comment.objects.filter(content_type=case_ct,
                                               object_pk=case.pk)
-            self.assertEqual(1, len(comments))
+            self.assertEqual(1, comments.count())
 
-            comment = comments[0]
+            comment = comments.first()
             self.assertEqual('new comment', comment.comment)
             self.assertEqual(self.reviewer, comment.user)
             self.assertEqual(self.reviewer.username, comment.user_name)
