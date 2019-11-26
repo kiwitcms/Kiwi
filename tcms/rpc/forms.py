d28ce8dd05266728781bff354b7be3d6509b3a60
asankov <a.sankov96@gmail.com>
2019-11-26 10:09:20 +0200

    Fix redeclared class pylint warning

--- tcms/rpc/forms.py
+++ tcms/rpc/forms.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.forms.widgets import CheckboxInput
+from django.forms.widgets import CheckboxInput as DjangoCheckboxInput
 
 from tcms.rpc.utils import parse_bool_value
 
 
-class CheckboxInput(CheckboxInput):
+class CheckboxInput(DjangoCheckboxInput):
     def value_from_datadict(self, data, files, name):
         if name not in data:
             return False
