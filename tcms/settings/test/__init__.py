1671b34e26dbcae80d2300126135b8ab9a38dd78
Mr. Senko <atodorov@mrsenko.com>
2019-12-08 13:05:51 +0200

    Add missing __init__.py & adjust an import outside toplevel
    will cause test_discovery.py to be automatically executed and
    makes pylint happier

--- tcms/settings/test/__init__.py
+++ tcms/settings/test/__init__.py
@@ -3,14 +3,18 @@
 import os
 import warnings
 import pkg_resources
-from tcms.settings.devel import *  # noqa: F401,F403
-# prented there are telemetry plugins installed so we can check
+
+# pretend there are telemetry plugins installed so we can check
 # the plugin loading code in settings/common.py
 dist = pkg_resources.Distribution(__file__)
 entry_point = pkg_resources.EntryPoint.parse('a_fake_plugin = tcms.telemetry.tests.plugin',
                                              dist=dist)
 dist._ep_map = {'kiwitcms.telemetry.plugins': {'a_fake_plugin': entry_point}}
 pkg_resources.working_set.add(dist)
+
+# this needs to be here so that  discovery tests can work
+from tcms.settings.devel import *  # noqa: F401,F403 pylint: disable=import-outside-toplevel
+
 
 # silence resource warnings while testing, see
 # https://emptysqua.re/blog/against-resourcewarnings-in-python-3/
