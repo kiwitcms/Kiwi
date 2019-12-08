f8a5ccf282ac8c3c6b32f807d8e3191c56e022e1
Mr. Senko <atodorov@mrsenko.com>
2019-12-08 12:27:27 +0200

    Add helper functions for GitHub webhook secret validation
    will be used by external add-ons

---
+++ docs/source/modules/tcms.utils.github.rst
@@ -0,0 +1,7 @@
+tcms.utils.github module
+========================
+
+.. automodule:: tcms.utils.github
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ tcms/utils/github.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+
+import hmac
+import hashlib
+
+from django.http import HttpResponseForbidden
+
+
+def calculate_signature(secret, contents):
+    """
+        Calculate GitHub signature header.
+
+        WARNING: both parameters must be bytes, not string!
+    """
+    return 'sha1=' + hmac.new(
+        secret,
+        msg=contents,
+        digestmod=hashlib.sha1).hexdigest()
+
+
+def verify_signature(request, secret):
+    """
+        Verifies request comes from GitHub, see:
+        https://developer.github.com/webhooks/securing/
+    """
+    signature = request.headers.get('X-Hub-Signature', None)
+    if not signature:
+        return HttpResponseForbidden()
+
+    expected = calculate_signature(secret, request.body)
+
+    # due to security reasons do not use '==' operator
+    # https://docs.python.org/3/library/hmac.html#hmac.compare_digest
+    if not hmac.compare_digest(signature, expected):
+        return HttpResponseForbidden()
+
+    return True  # b/c of inconsistent-return-statements
