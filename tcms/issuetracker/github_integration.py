2d7e47caa41ce056f09de47afeefeb19a74f4772
Mr. Senko <atodorov@mrsenko.com>
2019-12-24 15:06:02 +0200

    Automatically report issues to GitHub. Refs #479
    teach Kiwi TCMS how to automatically report issues to GitHub when
    user clicks on the Report button. Part of 1-click bug report
    functionality!
    
    Falls back to manually creating the issue if something doesn't
    work.

--- tcms/issuetracker/github_integration.py
+++ tcms/issuetracker/github_integration.py
@@ -11,10 +11,14 @@ class GitHubThread(IntegrationThread):
 
         Executed from the IssueTracker interface methods.
     """
-
-    def __init__(self, rpc, bug_system, execution, bug_id):
+    @staticmethod
+    def repo_id(bug_system):
         repo_id = bug_system.base_url.strip().strip('/').lower()
         repo_id = repo_id.replace('https://', '').replace('http://', '').replace('github.com/', '')
+        return repo_id
+
+    def __init__(self, rpc, bug_system, execution, bug_id):
+        repo_id = self.repo_id(bug_system)
         self.repo = rpc.get_repo(repo_id)
 
         super().__init__(rpc, bug_system, execution, bug_id)
