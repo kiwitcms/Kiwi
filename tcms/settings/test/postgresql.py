45350a42dcc8f949ee4cea912bde6bcaccd16009
Mr. Senko <atodorov@mrsenko.com>
2020-01-15 13:51:40 +0200

    Django 3.0: replace removed postgresql_psycopg2 backend with postgresql

--- tcms/settings/test/postgresql.py
+++ tcms/settings/test/postgresql.py
@@ -3,7 +3,7 @@
 from tcms.settings.test import *  # noqa: F403
 
 DATABASES['default'].update({     # noqa: F405
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'kiwi',
     'USER': 'postgres',
     'HOST': 'localhost',
