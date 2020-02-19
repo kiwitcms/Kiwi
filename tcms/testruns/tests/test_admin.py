46bfda9b60bc586622840f133c0b96b2231b5920
Mariyan Garvanski <mgarvanski@gmail.com>
2020-02-19 22:52:00 +0200

    Add Test for TestRunAdmin.change_view()

--- tcms/testruns/tests/test_admin.py
+++ tcms/testruns/tests/test_admin.py
@@ -3,6 +3,7 @@
 from django.urls import reverse
 from tcms.tests import LoggedInTestCase
 from tcms.utils.permissions import initiate_user_with_default_setups
+from tcms.tests.factories import TestRunFactory
 
 
 class TestTestRunAdmin(LoggedInTestCase):
@@ -11,7 +12,13 @@ class TestTestRunAdmin(LoggedInTestCase):
     def setUpTestData(cls):
         super().setUpTestData()
         initiate_user_with_default_setups(cls.tester)
+        cls.test_run = TestRunFactory()
 
     def test_regular_user_can_not_add_testrun(self):
         response = self.client.get(reverse('admin:testruns_testrun_add'))
         self.assertRedirects(response, reverse('admin:testruns_testrun_changelist'))
+
+    def test_regular_user_can_not_change_testrun(self):
+        response = self.client.get(reverse('admin:testruns_testrun_change',
+                                           args=[self.test_run.pk]))
+        self.assertRedirects(response, reverse('testruns-get', args=[self.test_run.pk]))
