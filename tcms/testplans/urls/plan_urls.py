67b13af4b0aa300e77951d6d184d132c9645bfce
Mr. Senko <atodorov@mrsenko.com>
2020-02-23 17:13:35 +0200

    Remove legacy search/link cases to TP views in favor of Patternfly
    this will be implemented similarly to #1402 for the easier case
    (adding TC by ID and Summary). For advanced search & add we should
    reuse the existing test case search functionality.
    
    This commit removes all functionality related to these deprecated
    pages which is not used anywhere else.

--- tcms/testplans/urls/plan_urls.py
+++ tcms/testplans/urls/plan_urls.py
@@ -14,9 +14,5 @@ urlpatterns = [
 
     url(r'^(?P<pk>\d+)/reorder-cases/$', views.ReorderCasesView.as_view(),
         name='plan-reorder-cases'),
-    url(r'^(?P<pk>\d+)/link-cases/$', views.LinkCasesView.as_view(),
-        name='plan-link-cases'),
-    url(r'^(?P<pk>\d+)/link-cases/search/$', views.LinkCasesSearchView.as_view(),
-        name='plan-search-cases-for-link'),
     url(r'^update-parent/$', views.UpdateParentView.as_view()),
 ]
