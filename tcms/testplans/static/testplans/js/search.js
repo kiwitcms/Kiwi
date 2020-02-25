8b39f8e17e7088f635b796dfdc5e66b09c4da16a
Schwarzkrieger <karabojkov@kitbg.com>
2020-02-25 11:12:58 +0200

    Customize number of search results table rows
    The number of search results table rows can now be set by changing DEFAULT_PAGE_SIZE setting in tcms/settings/common.py
    Closes #1210

--- tcms/testplans/static/testplans/js/search.js
+++ tcms/testplans/static/testplans/js/search.js
@@ -9,7 +9,7 @@ function pre_process_data(data) {
 
 $(document).ready(function() {
     var table = $("#resultsTable").DataTable({
-        pageLength: 100,
+        pageLength: $('#navbar').data('defaultpagesize'),
         ajax: function(data, callback, settings) {
             var params = {};
 
