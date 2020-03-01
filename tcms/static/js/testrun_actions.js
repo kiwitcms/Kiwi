kiwitcms/Kiwi

commit 058a9bc9990a5dc3d3b712f986b7b8a437243400

[db] Rename TestExecution.case_run_id to TestExecution.id
INTRODUCES BACKWARDS INCOMPATIBLE CHANGES:

- API TestRun.get_cases() will return 'execution_id' instead
  of 'case_run_id' field.

- API methods returning serialized TestExecution objects rename
 the 'case_run_id' field to 'id'. The affected methods are:

  -- TestRun.add_case()
  -- TestExecution.create()
  -- TestExecution.filter()
  -- TestExecution.update()

tcms/static/js/testrun_actions.js
@@ -434,7 +434,7 @@ AddIssueDialog.prototype.get_data = function () {
 //// end of AddIssueDialog definition /////////
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-function fileCaseRunBug(run_id, title_container, container, case_id, case_run_id) {
+function fileCaseRunBug(run_id, title_container, container, case_id, execution_id) {
   var dialog = new AddIssueDialog({
     'action': 'Report',
     'onSubmit': function (e, dialog) {
@@ -442,7 +442,7 @@ function fileCaseRunBug(run_id, title_container, container, case_id, case_run_id
       e.preventDefault();
 
         var tracker_id = dialog.get_data()['bug_system_id'];
-        jsonRPC('Bug.report', [case_run_id, tracker_id], function(result) {
+        jsonRPC('Bug.report', [execution_id, tracker_id], function(result) {
             $('#dialog').hide();
 
             if (result.rc === 0) {
@@ -460,7 +460,7 @@ function fileCaseRunBug(run_id, title_container, container, case_id, case_run_id
 
 function delCaseRun(run_id) {
     if (window.confirm('Are you sure?')) {
-        const executions = $('#id_table_cases').find('input[name="case_run"]:checked');
+        const executions = $('#id_table_cases').find('input[name="execution"]:checked');
 
         executions.each(function() {
             var case_id = this.getAttribute('data-case_id');
@@ -594,9 +594,9 @@ function changeCaseRunAssignee() {
 function serializeCaseRunFromInputList(table, name) {
   var elements;
   if (typeof table === 'string') {
-    elements = jQ('#' + table).parent().find('input[name="case_run"]:checked');
+    elements = jQ('#' + table).parent().find('input[name="execution"]:checked');
   } else {
-    elements = jQ(table).parent().find('input[name="case_run"]:checked');
+    elements = jQ(table).parent().find('input[name="execution"]:checked');
   }
 
   var returnobj_list = [];
@@ -794,17 +794,6 @@ function initialize_addlink_dialog() {
 }
 
 
-/*
- * Toggle TestExecution panel to edit a case run in run page.
- *
- * Arguments:
- * options.casrunContainer:
- * options.expandPaneContainer:
- * options.caseId:
- * options.caserunId:
- * options.caseTextVersion:
- * options.callback:
- */
 function toggleTestExecutionPane(options) {
   var container = options.caserunRowContainer;
   var content_container = options.expandPaneContainer;
@@ -814,7 +803,7 @@ function toggleTestExecutionPane(options) {
 
   if (content_container.find('.ajax_loading').length) {
     var url = '/case/' + options.caseId + '/execution-detail-pane/';
-    var data = { case_run_id: options.caserunId, case_text_version: options.caseTextVersion };
+    var data = { execution_id: options.executionId, case_text_version: options.caseTextVersion };
 
     jQ.get(url, data, function(data, textStatus) {
       content_container.html(data);

tcms/rpc/api/testexecution.py
@@ -135,21 +135,21 @@ def filter(values):  # pylint: disable=redefined-builtin
 
 @permissions_required('testruns.change_testexecution')
 @rpc_method(name='TestExecution.update')
-def update(case_run_id, values, **kwargs):
+def update(execution_id, values, **kwargs):
     """
-    .. function:: XML-RPC TestExecution.update(case_run_id, values)
+    .. function:: XML-RPC TestExecution.update(execution_id, values)
 
         Update the selected TestExecution
 
-        :param case_run_id: PK of TestExecution to modify
-        :type case_run_id: int
+        :param execution_id: PK of TestExecution to modify
+        :type execution_id: int
         :param values: Field values for :class:`tcms.testruns.models.TestExecution`
         :type values: dict
         :return: Serialized :class:`tcms.testruns.models.TestExecution` object
         :raises: PermissionDenied if missing *testruns.change_testexecution* permission
     """
 
-    tcr = TestExecution.objects.get(pk=case_run_id)
+    tcr = TestExecution.objects.get(pk=execution_id)
     form = UpdateExecutionForm(values)
 
     if form.is_valid():

tcms/rpc/api/testrun.py
@@ -72,7 +72,7 @@ def get_cases(run_id):
         :param run_id: PK of TestRun to inspect
         :type run_id: int
         :return: Serialized list of :class:`tcms.testcases.models.TestCase` objects
-                 augmented with ``case_run_id`` and ``status`` information.
+                 augmented with ``execution_id`` and ``status`` information.
         :rtype: list(dict)
     """
     tcs_serializer = TestCase.to_xmlrpc(query={'case_run__run_id': run_id})
@@ -83,7 +83,7 @@ def get_cases(run_id):
 
     for case in tcs_serializer:
         info = extra_info[case['case_id']]
-        case['case_run_id'] = info['pk']
+        case['execution_id'] = info['pk']
         case['status'] = info['status__name']
 
     return tcs_serializer

tcms/rpc/serializer.py
@@ -420,7 +420,7 @@ class TestExecutionRPCSerializer(QuerySetBasedRPCSerializer):
     """Serializer for TestExecution"""
 
     values_fields_mapping = {
-        'case_run_id': ('case_run_id', do_nothing),
+        'id': ('id', do_nothing),
         'case_text_version': ('case_text_version', do_nothing),
         'close_date': ('close_date', datetime_to_str),
         'sortkey': ('sortkey', do_nothing),

tcms/rpc/tests/test_testexecution.py
@@ -35,8 +35,6 @@ class TestExecutionCreate(APITestCase):  # pylint: disable=too-many-instance-att
         self.status = TestExecutionStatus.objects.get(name='IDLE')
         self.case = TestCaseFactory(author=self.api_user, default_tester=None, plan=[self.plan])
 
-        self.execution_pks = []
-
     def test_create_with_no_required_fields(self):
         values = [
             {
@@ -67,7 +65,7 @@ class TestExecutionCreate(APITestCase):  # pylint: disable=too-many-instance-att
             "case_text_version": 15,
         })
         self.assertIsNotNone(tcr)
-        self.execution_pks.append(tcr['case_run_id'])
+        self.assertIsNotNone(tcr['id'])
         self.assertEqual(tcr['build_id'], self.build.pk)
         self.assertEqual(tcr['case_id'], self.case.pk)
         self.assertEqual(tcr['run_id'], self.test_run.pk)
@@ -83,7 +81,7 @@ class TestExecutionCreate(APITestCase):  # pylint: disable=too-many-instance-att
             "case_text_version": 3,
         })
         self.assertIsNotNone(tcr)
-        self.execution_pks.append(tcr['case_run_id'])
+        self.assertIsNotNone(tcr['id'])
         self.assertEqual(tcr['build_id'], self.build.pk)
         self.assertEqual(tcr['case_id'], self.case.pk)
         self.assertEqual(tcr['assignee_id'], self.api_user.pk)

tcms/rpc/tests/test_testrun.py
@@ -32,7 +32,7 @@ class TestAddCase(APITestCase):
         self.assertTrue(isinstance(result, dict))
 
         execution = TestExecution.objects.get(run=self.test_run.pk, case=self.test_case.pk)
-        self.assertEqual(execution.pk, result['case_run_id'])
+        self.assertEqual(execution.pk, result['id'])
         self.assertEqual(execution.case.pk, result['case_id'])
         self.assertEqual(execution.run.pk, result['run_id'])
 

tcms/static/js/testcase_actions.js
@@ -48,7 +48,7 @@ function toggleExpandArrow(options) {
 }
 
 
-function toggleTestCaseContents(template_type, container, content_container, object_pk, case_text_version, case_run_id, callback) {
+function toggleTestCaseContents(template_type, container, content_container, object_pk, case_text_version, execution_id, callback) {
   if (typeof container === 'string') {
     var container = jQ('#' + container)[0];
   }
@@ -64,7 +64,7 @@ function toggleTestCaseContents(template_type, container, content_container, obj
     var parameters = {
       template_type: template_type,
       case_text_version: case_text_version,
-      case_run_id: case_run_id
+      execution_id: execution_id
     };
 
     jQ.ajax({

tcms/templates/run/assign_case.html
@@ -82,7 +82,7 @@ Nitrate.Utils.after_page_load(Nitrate.TestRuns.AssignCase.on_load);
 					<tr id="row_{{ forloop.counter }}" class="{% cycle 'odd' 'even' %} selection_row">
 						<td align="left">
 							{# Hide the check box when it is in the exist_case_runs, and remove it from form submitation #}
-							<input type="checkbox" name="case"  {% if test_case.case in exist_case_run_ids %}value="{{ test_case.case }}" disabled="true"  checked="true"{% else %}value="{{ test_case.case }}"{% endif %} />
+							<input type="checkbox" name="case"  {% if test_case.case in exist_execution_ids %}value="{{ test_case.case }}" disabled="true"  checked="true"{% else %}value="{{ test_case.case }}"{% endif %} />
 						</td>
 						<td>
 							<a id="blind_link_{{ forloop.counter }}" class="blind_link js-toggle-button" data-param="{{ forloop.counter }}" href="javascript:void(0);">

commit ff276de232b3fe87558af136e14587452cf43730

Update selector for 'Select All' TEs in TR page. Fixes #1404
was broken in 058a9bc

tcms/static/js/testrun_actions.js
@@ -25,7 +25,7 @@ Nitrate.TestRuns.Details.on_load = function() {
 
   // Observe the interface buttons
   jQ('#id_check_all_button').bind('click', function(e) {
-    toggleAllCheckBoxes(this, 'id_table_cases', 'case_run');
+    toggleAllCheckBoxes(this, 'id_table_cases', 'execution');
   });
 
   if (jQ('#id_check_box_highlight').attr('checked')) {