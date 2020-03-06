1e709bb0e011605861dc0903f5fa3de14e54ff98
Mr. Senko <atodorov@mrsenko.com>
2020-03-07 01:05:32 +0200

    Remove TestCaseExecutionDetailPanelView
    will be replaced by RPC calls

--- tcms/testcases/urls/case_urls.py
+++ tcms/testcases/urls/case_urls.py
@@ -7,7 +7,4 @@ from .. import views
 urlpatterns = [
     re_path(r'^(?P<pk>\d+)/$', views.TestCaseGetView.as_view(), name='testcases-get'),
     re_path(r'^(?P<pk>\d+)/edit/$', views.EditTestCaseView.as_view(), name='testcases-edit'),
-    re_path(r'^(?P<case_id>\d+)/execution-detail-pane/$',
-            views.TestCaseExecutionDetailPanelView.as_view(),
-            name='execution-detail-pane'),
 ]
