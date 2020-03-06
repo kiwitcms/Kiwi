7fb2835a5975372081161e66d69df28b6930cce3
Mr. Senko <atodorov@mrsenko.com>
2020-03-07 01:37:12 +0200

    Remove testplans.views.printable() & testcases.views.printable()
    not needed anymore b/c the new pages can be styled for print
    using CSS

--- tcms/testcases/urls/cases_urls.py
+++ tcms/testcases/urls/cases_urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     url(r'^new/$', views.NewCaseView.as_view(), name='testcases-new'),
     url(r'^search/$', views.TestCaseSearchView.as_view(), name='testcases-search'),
     url(r'^clone/$', views.CloneTestCaseView.as_view(), name='testcases-clone'),
-    url(r'^printable/$', views.printable, name='testcases-printable'),
 ]
