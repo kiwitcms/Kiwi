f5b494bd4f3f9523e18cf5becc6b63207c23947f
Mr. Senko <atodorov@mrsenko.com>
2020-03-13 13:47:25 +0200

    import_local_settings now accepts an absolute dir name
    seeking override settings under site-pacakges/tcms_settings_dir/

--- tcms/settings/local_settings_test/installed_apps_update.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-
-settings.INSTALLED_APPS += ['test_me']
--- tcms/utils/tests/test_local_settings_dir.py
+++ tcms/utils/tests/test_local_settings_dir.py
@@ -2,11 +2,14 @@
 
 # Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
+import os
 from django.conf import settings
 from django.test import TestCase
 
 from tcms import __version__
 from tcms.utils.settings import import_local_settings
+
+from . import local_settings_test
 
 
 class ImportLocalSettingsTestCase(TestCase):
@@ -17,7 +20,8 @@ class ImportLocalSettingsTestCase(TestCase):
         self.assertNotIn('test_me', settings.INSTALLED_APPS)
 
         # perform the import which will override settings
-        import_local_settings('local_settings_test', scope=settings.__dict__)
+        import_local_settings(os.path.dirname(local_settings_test.__file__),
+                              scope=settings.__dict__)
 
         # assert settings values have changed
         self.assertEqual(settings.KIWI_VERSION, "%s-Under-Test" % __version__)
