dc9586b3992e0142fcd5b50c5567682a119f0fb9
asankov <a.sankov96@gmail.com>
2020-03-17 12:57:08 +0200

    Add Tags to TestRun page

--- tcms/testruns/static/testruns/js/get.js
+++ tcms/testruns/static/testruns/js/get.js
@@ -2,6 +2,12 @@ $(document).ready(() => {
 
     $('.bootstrap-switch').bootstrapSwitch();
 
+    const testRunId = $('#test_run_pk').data('pk')
+
+    const permRemoveTag = $('#test_run_pk').data('perm-remove-tag') === 'True';
+
+    // bind everything in tags table
+    tagsCard('TestRun', testRunId, { run: testRunId }, permRemoveTag);
 })
 
 
