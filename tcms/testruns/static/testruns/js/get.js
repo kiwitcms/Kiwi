c065bee82fae4e0fa445986bee73e8f94067f03a
asankov <a.sankov96@gmail.com>
2020-03-17 12:22:26 +0200

    Implement status control button

--- tcms/testruns/static/testruns/js/get.js
+++ tcms/testruns/static/testruns/js/get.js
@@ -4,6 +4,16 @@ $(document).ready(() => {
 
     const testRunId = $('#test_run_pk').data('pk')
 
+    $('#status_button').on('switchChange.bootstrapSwitch', (_event, state) => {
+        if (state) {
+            jsonRPC('TestRun.update', [testRunId, { 'stop_date': null }], () => { })
+        } else {
+            let now = new Date().toISOString().replace("T", " ")
+            now = now.slice(0, now.length-5)
+            jsonRPC('TestRun.update', [testRunId, { 'stop_date': now }], () => { })
+        }
+    });
+
     const permRemoveTag = $('#test_run_pk').data('perm-remove-tag') === 'True';
 
     // bind everything in tags table
