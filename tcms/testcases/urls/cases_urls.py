5f69759abb8446ce2b70e82119ea8d840aee7fa3
Mr. Senko <atodorov@mrsenko.com>
2020-04-11 21:31:36 +0300

    Remove legacy plan/get.html template and everything related to it
    - removes testplan_actions.js
    - lib/tablednd.js
    - lots of helper & unused functions removed
    
    The following views are also removed:
    
    - /plan/update-parent/
    - /ajax/update/cases-actor/
    - /cases/load-more/
    - /case/<pk>/readonly-pane/
    - testcases.views.list_all()

--- tcms/testcases/urls/cases_urls.py
+++ tcms/testcases/urls/cases_urls.py
@@ -6,9 +6,7 @@ from .. import views
 
 urlpatterns = [
     re_path(r'^new/$', views.NewCaseView.as_view(), name='testcases-new'),
-    re_path(r'^$', views.list_all, name='testcases-all'),
     re_path(r'^search/$', views.TestCaseSearchView.as_view(), name='testcases-search'),
-    re_path(r'^load-more/$', views.load_more_cases),
     re_path(r'^clone/$', views.CloneTestCaseView.as_view(), name='testcases-clone'),
     re_path(r'^printable/$', views.printable, name='testcases-printable'),
 ]
