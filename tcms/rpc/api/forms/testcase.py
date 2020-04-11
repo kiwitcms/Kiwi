844338af4e0974e322e3b050534e71c0ec9cdba2
Mr. Senko <atodorov@mrsenko.com>
2020-04-12 00:06:21 +0300

    Move a common code for API update forms into a mixin
    to avoid repeating this everywhere

--- tcms/rpc/api/forms/testcase.py
+++ tcms/rpc/api/forms/testcase.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.forms.utils import ErrorList
 
+from tcms.rpc.api.forms import UpdateModelFormMixin
 from tcms.testcases.models import TestCase
 
 
@@ -10,24 +10,7 @@ class NewForm(forms.ModelForm):
         exclude = ('reviewer', 'tag', 'component', 'plan')  # pylint: disable=modelform-uses-exclude
 
 
-class UpdateForm(forms.ModelForm):
+class UpdateForm(UpdateModelFormMixin, forms.ModelForm):
     class Meta:
         model = TestCase
         exclude = ('tag', 'component', 'plan')  # pylint: disable=modelform-uses-exclude
-
-    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None,
-                 initial=None, error_class=ErrorList, label_suffix=None,
-                 empty_permitted=False, instance=None, use_required_attribute=None,
-                 renderer=None):
-        super().__init__(
-            data, files, auto_id, prefix,
-            initial, error_class, label_suffix,
-            empty_permitted, instance, use_required_attribute,
-            renderer,
-        )
-
-        for field in self.fields:
-            self.fields[field].required = False
-            # will cause BaseForm._clean_fields() to reuse the value
-            # from self.initial (<-- self.instance) if not specified
-            self.fields[field].disabled = field not in data
