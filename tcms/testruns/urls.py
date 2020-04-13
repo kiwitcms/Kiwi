453cadd23552fb9060bd6ef0ab7c04ba6dfd7489
Mr. Senko <atodorov@mrsenko.com>
2020-04-13 12:16:39 +0300

    Remove unused HTML templates and views
    - almost all HTML templates under templates/run/
    - TestRunReportView - won't be needed, will print the page directly
    - data/TestExecutionDataMixin - used only in report view
    - AddCasesToRunView - will be using a new UI flow + RPC
    - ManageTestRunCC - needs to be reimplemented as JSON RPC
    - tests related to the removed views which are not skipped.
      Skipped tests will be fixed at other steps in this PR
    - static/js/testrun_actions.js - not used anywhere else
      NOTE: a number of functions will need to be reimplemented but
      ATM it looks like it will be easier/cleaner to reimplement them
      from scratch. If need be they can be copied from this commit or
      from the master branch.

--- tcms/testruns/urls.py
+++ tcms/testruns/urls.py
@@ -8,15 +8,8 @@ urlpatterns = [
     re_path(r'^(?P<pk>\d+)/clone/$', views.CloneTestRunView.as_view(), name='testruns-clone'),
     re_path(r'^(?P<pk>\d+)/edit/$', views.EditTestRunView.as_view(), name='testruns-edit'),
 
-    re_path(r'^(?P<pk>\d+)/report/$', views.TestRunReportView.as_view(),
-            name='run-report'),
-
     re_path(r'^(?P<pk>\d+)/changestatus/$', views.ChangeTestRunStatusView.as_view(),
             name='testruns-change_status'),
 
-    re_path(r'^(?P<pk>\d+)/assigncase/$', views.AddCasesToRunView.as_view(),
-            name='add-cases-to-run'),
-
-    re_path(r'^(?P<pk>\d+)/cc/$', views.ManageTestRunCC.as_view(), name='testruns-cc'),
     re_path(r'^search/$', views.SearchTestRunView.as_view(), name='testruns-search'),
 ]
