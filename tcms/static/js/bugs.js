b15e2335645db0b77ec312ebf1757f748163210f
asankov <a.sankov96@gmail.com>
2020-04-14 18:18:06 +0300

    Extract bugs table into reusable template

---
+++ tcms/static/js/bugs.js
@@ -0,0 +1,51 @@
+const bugDetailsCache = {}
+
+function loadBugs(selector, filter) {
+    const noRecordsFoundText = $('.bugs-table').data('no-records-found-text')
+
+    $(selector).DataTable({
+        ajax: (data, callback, settings) => {
+            dataTableJsonRPC('TestExecution.get_links', filter, callback);
+        },
+        columns: [
+            {
+                data: null,
+                render: (data, type, full, meta) => `<a href="${data.url}" class="bug-url">${data.url}</a>`
+            },
+            {
+                data: null,
+                render: function (data, type, full, meta) {
+                    return `<a href="#bugs" data-toggle="popover" data-html="true"
+                        data-content="undefined" data-trigger="focus" data-placement="top">
+                        <span class="fa fa-info-circle"></span>
+                        </a>`;
+                }
+            },
+        ],
+        dom: "t",
+        language: {
+            loadingRecords: '<div class="spinner spinner-lg"></div>',
+            processing: '<div class="spinner spinner-lg"></div>',
+            zeroRecords: noRecordsFoundText
+        },
+        order: [[0, 'asc']],
+    });
+
+    $(selector).on('draw.dt', () => {
+        $(selector).find('[data-toggle=popover]')
+            .popovers()
+            .on('show.bs.popover', (element) => {
+                fetchBugDetails($(element.target).parents('tr').find('.bug-url')[0],
+                    element.target,
+                    bugDetailsCache);
+            });
+    });
+
+    $('[data-toggle=popover]')
+        .popovers()
+        .on('show.bs.popover', (element) => {
+            fetchBugDetails($(element.target).parents('.list-view-pf-body').find('.bug-url')[0],
+                element.target,
+                bugDetailsCache);
+        });
+}
