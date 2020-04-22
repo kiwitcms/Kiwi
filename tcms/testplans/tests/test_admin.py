a165c0ed9eb0b1bce6842a824631f872170bb0e5
Mariyan Garvanski <mgarvanski@gmail.com>
2020-04-22 18:50:15 +0300

    Add test for TestPlanAdmin.response_delete()

--- tcms/testplans/tests/test_admin.py
+++ tcms/testplans/tests/test_admin.py
@@ -1,7 +1,11 @@
 from django.urls import reverse
-from tcms.tests import LoggedInTestCase
-from tcms.utils.permissions import initiate_user_with_default_setups
+from django.utils.translation import gettext_lazy as _
+
 from tcms.tests.factories import TestPlanFactory
+from tcms.tests import LoggedInTestCase
+
+from tcms.utils.permissions import initiate_user_with_default_setups
+from tcms.testplans.models import TestPlan
 
 
 class TestTestPlanAdmin(LoggedInTestCase):
@@ -21,3 +25,12 @@ class TestTestPlanAdmin(LoggedInTestCase):
                                            args=[self.test_plan.pk]))
         self.assertRedirects(response, reverse('test_plan_url_short', args=[self.test_plan.pk]),
                              target_status_code=301)
+
+    def test_users_can_delete_testplan(self):
+        response = self.client.get(reverse('admin:testplans_testplan_delete',
+                                           args=[self.test_plan.pk]))
+        self.assertContains(response, _("Yes, I'm sure"))
+        response = self.client.post(reverse('admin:testplans_testplan_delete',
+                                            args=[self.test_plan.pk]), {'post': 'yes'}, follow=True)
+        self.assertRedirects(response, reverse('core-views-index'))
+        self.assertEqual(TestPlan.objects.filter(pk=self.test_plan.pk).exists(), False)
