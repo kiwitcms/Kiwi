3e368dc8250840e7ec7274459fc792db122492ed
Mr. Senko <atodorov@mrsenko.com>
2020-05-10 00:10:25 +0300

    Combine all URL definitions into tcms.testplans.urls

--- tcms/testplans/urls.py
+++ tcms/testplans/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
 
     re_path(r'^(?P<pk>\d+)/edit/$', views.Edit.as_view(), name='plan-edit'),
     re_path(r'^(?P<pk>\d+)/clone/$', views.Clone.as_view(), name='plans-clone'),
+    re_path(r'^search/$', views.SearchTestPlanView.as_view(), name='plans-search'),
+    re_path(r'^new/$', views.NewTestPlanView.as_view(), name='plans-new'),
 ]
