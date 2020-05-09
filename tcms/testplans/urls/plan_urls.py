f93d57ee3327ac869a2f2581e7f3a49d95d7d2a0
Mr. Senko <atodorov@mrsenko.com>
2020-05-09 23:45:27 +0300

    Convert testplans.views.Clone to respond to GET requests
    - necessary because we can't display a POST page via the menu
    - remove help_text attribute from ClonePlanForm fields b/c not
      used in the HTML template
    - adjust tests
    
    Pass in forgotten argument to super().get_form(form_class) like
    in the previous commit.

--- tcms/testplans/urls/plan_urls.py
+++ tcms/testplans/urls/plan_urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
             name='test_plan_url'),
 
     re_path(r'^(?P<pk>\d+)/edit/$', views.Edit.as_view(), name='plan-edit'),
+    re_path(r'^(?P<pk>\d+)/clone/$', views.Clone.as_view(), name='plans-clone'),
 ]
