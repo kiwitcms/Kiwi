d13f53f782b4a2c58dc8176f7175496e53a421c8
Mr. Senko <atodorov@mrsenko.com>
2020-05-26 14:09:25 +0300

    Move test jobs away from Travis CI

--- tcms/settings/test/mariadb.py
+++ tcms/settings/test/mariadb.py
@@ -6,7 +6,7 @@ DATABASES['default'].update({     # noqa: F405
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'kiwi',
     'USER': 'kiwi',
-    'PASSWORD': '',
+    'PASSWORD': 'kiwi',
     'HOST': '127.0.0.1',
     'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
