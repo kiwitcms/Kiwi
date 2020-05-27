e5479f879afc198e8af02f75fcccbba6f056d991
Mr. Senko <atodorov@mrsenko.com>
2020-05-27 21:56:02 +0300

    db: Force creation of necessary permissions for m2m fields
    These are from the `tcms.bugs` app:
    
    bugs.add_bug_tags
    bugs.change_bug_tags
    bugs.delete_bug_tags
    bugs.view_bug_tags
    
    bugs.add_bug_executions
    bugs.change_bug_execution
    bugs.delete_bug_execution
    bugs.view_bug_executions
    
    WARNING: App admins will have to assign these by hand to
    users/groups who will be allowed to change tags on bugs.

---
+++ docs/source/modules/tcms.bugs.management.rst
@@ -0,0 +1,7 @@
+tcms.bugs.management module
+===========================
+
+.. automodule:: tcms.bugs.management
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- tcms/bugs/apps.py
+++ tcms/bugs/apps.py
@@ -1,13 +1,19 @@
 # pylint: disable=import-outside-toplevel
 from django.apps import AppConfig as DjangoAppConfig
+from django.db.models.signals import post_migrate
+from django.db.models.signals import post_save
 
 
 class AppConfig(DjangoAppConfig):
     name = 'tcms.bugs'
 
     def ready(self):
-        from django.db.models.signals import post_save
         from .models import Bug
+        from .management import create_permissions
         from tcms import signals
 
         post_save.connect(signals.handle_emails_post_bug_save, sender=Bug)
+        post_migrate.connect(
+            create_permissions,
+            dispatch_uid="tcms.bugs.management.create_permissions"
+        )
