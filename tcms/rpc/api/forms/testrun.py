352c6c8f27fea2a8c5aa6273a984b6bf698537da
Rosen Sasov <rosen.sasov@mail.com>
2020-07-18 00:48:23 +0300

    Refactor NewExecutionForm to ModelForm

--- tcms/rpc/api/forms/testrun.py
+++ tcms/rpc/api/forms/testrun.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from tcms.management.models import Build
-from tcms.testcases.models import TestCase
-from tcms.testplans.models import TestPlan
 from tcms.core.forms.fields import UserField
-from tcms.testruns.forms import BaseCaseRunForm, BaseRunForm
-from tcms.testruns.models import TestExecutionStatus, TestRun
+from tcms.management.models import Build
 from tcms.rpc.api.forms import UpdateModelFormMixin, DateTimeField
+from tcms.testplans.models import TestPlan
+from tcms.testruns.forms import BaseTestExecutionForm, BaseRunForm
+from tcms.testruns.models import TestExecution
+from tcms.testruns.models import TestExecutionStatus, TestRun
 
 User = get_user_model()  # pylint: disable=invalid-name
 
@@ -36,22 +36,13 @@ class UpdateForm(UpdateModelFormMixin, forms.ModelForm):
         self.fields['build'].queryset = Build.objects.filter(product_id=product_id, is_active=True)
 
 
-class NewExecutionForm(BaseCaseRunForm):
-    assignee = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
-    run = forms.ModelChoiceField(queryset=TestRun.objects.all())
-    case = forms.ModelChoiceField(queryset=TestCase.objects.all())
+class NewTestExecutionForm(forms.ModelForm):
+    class Meta:
+        model = TestExecution
+        fields = '__all__'
 
-    def clean_assignee(self):
-        data = self.cleaned_data.get('assignee')
-        if not data:
-            if self.cleaned_data.get('case') \
-                    and self.cleaned_data['case'].default_tester_id:
-                data = self.cleaned_data['case'].default_tester
-            elif self.cleaned_data.get('run') \
-                    and self.cleaned_data['run'].default_tester_id:
-                data = self.cleaned_data['run'].default_tester
-
-        return data
+    assignee = UserField(required=False)
+    tested_by = UserField(required=False)
 
     def clean_case_text_version(self):
         data = self.cleaned_data.get('case_text_version')
@@ -68,7 +59,7 @@ class NewExecutionForm(BaseCaseRunForm):
         return data
 
 
-class UpdateExecutionForm(BaseCaseRunForm):
+class UpdateExecutionForm(BaseTestExecutionForm):
     tested_by = forms.ModelChoiceField(queryset=User.objects.all(), required=False)
     build = forms.ModelChoiceField(queryset=Build.objects.all(), required=False)
     case_text_version = forms.CharField(required=False)
