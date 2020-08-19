ade1b97812d35cbec4b183a151355758c3a101b4
Mr. Senko <atodorov@mrsenko.com>
2020-08-19 13:17:38 +0300

    Connect handle_attachments_pre_delete() to pre_delete() for bugs
    - will help remove files when bugs are deleted
    - should have been done in f5709db3

--- tcms/bugs/apps.py
+++ tcms/bugs/apps.py
@@ -2,6 +2,7 @@
 from django.apps import AppConfig as DjangoAppConfig
 from django.db.models.signals import post_migrate
 from django.db.models.signals import post_save
+from django.db.models.signals import pre_delete
 
 
 class AppConfig(DjangoAppConfig):
@@ -14,6 +15,7 @@ class AppConfig(DjangoAppConfig):
 
         post_save.connect(signals.handle_emails_post_bug_save, sender=Bug)
         post_save.connect(signals.handle_attachments_post_save, sender=Bug)
+        pre_delete.connect(signals.handle_attachments_pre_delete, sender=Bug)
         post_migrate.connect(
             create_permissions,
             dispatch_uid="tcms.bugs.management.create_permissions"
