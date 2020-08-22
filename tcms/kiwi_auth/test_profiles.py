fa0cd781319e513685dd7443810acda37e12d761
Mr. Senko <atodorov@mrsenko.com>
2020-08-22 13:53:41 +0300

    Check auth.view_user permission for KiwiUserAdmin. Fix #1685
    this will prevent users on shared systems to see the profiles of
    others unless specifically granted the `auth.view_user` permission!
    
    This permission isn't granted by default! It must be assigned by
    a Kiwi TCMS administrator.
    
    Notes:
    - kiwi_auth.views.Profile just redirects to the admin page and doesn't
      check any permissions. Hence the pylint disable! It has tests though!

--- tcms/kiwi_auth/test_profiles.py
+++ tcms/kiwi_auth/test_profiles.py
@@ -4,9 +4,11 @@
 from http import HTTPStatus
 
 from django.test import TestCase
+from django.http import HttpResponseForbidden
 from django.urls import reverse
 
 from tcms.tests import create_request_user
+from tcms.tests import user_should_have_perm
 
 
 class TestProfilesView(TestCase):
@@ -32,7 +34,19 @@ class TestProfilesView(TestCase):
         self.assertContains(response, self.tester.email)
         self.assertContains(response, 'name="_save"')
 
-    def test_user_case_view_profile_of_another_user(self):
+    def test_user_cant_view_profile_of_another_user_without_permission(self):
+        logged_in = self.client.login(  # nosec:B106:hardcoded_password_funcarg
+            username=self.tester.username,
+            password='password')
+        self.assertTrue(logged_in)
+
+        url = reverse('tcms-profile', args=[self.somebody_else.username])
+        response = self.client.get(url, follow=True)
+        self.assertIsInstance(response, HttpResponseForbidden)
+
+    def test_user_can_view_profile_of_another_user_with_permission(self):
+        user_should_have_perm(self.tester, 'auth.view_user')
+
         logged_in = self.client.login(  # nosec:B106:hardcoded_password_funcarg
             username=self.tester.username,
             password='password')
@@ -41,7 +55,6 @@ class TestProfilesView(TestCase):
         url = reverse('tcms-profile', args=[self.somebody_else.username])
         response = self.client.get(url, follow=True)
 
-        self.assertEqual(HTTPStatus.OK, response.status_code)
         self.assertContains(response, self.somebody_else.username)
         self.assertContains(response, self.somebody_else.email)
         self.assertNotContains(response, 'name="_save"')
