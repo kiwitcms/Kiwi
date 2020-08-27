fcfd9287739416d6b46d60b2b02cdcfb04b0749c
Anton Sankov <a.sankov96@gmail.com>
2020-08-27 13:21:03 +0300

    Handler for adding hyperlinks to TestExecution
    * Add LinkReference form - will be used in the modal that creates hyperlink; validation is part of #1327
    * Add modal in HTML template
    * Will always try updating bug tracker if the URL points to a defect

---
+++ tcms/core/contrib/linkreference/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from tcms.core.contrib.linkreference.models import LinkReference
+
+
+class LinkReferenceForm(forms.ModelForm):
+    class Meta:
+        model = LinkReference
+        exclude = ['created_on']  # pylint: disable=modelform-uses-exclude
