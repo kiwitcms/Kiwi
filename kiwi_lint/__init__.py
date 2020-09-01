8a3b784097b93c80b492e796d116dc36f296f6b8
Bryan Mutai <mutaiwork@gmail.com>
2020-09-01 20:17:59 +0300

    Resolves #736. Add a lint checker class to check for use of db_column argument in model field definition.

--- kiwi_lint/__init__.py
+++ kiwi_lint/__init__.py
@@ -21,6 +21,7 @@ from .one_to_one_field import OneToOneFieldChecker
 from .views import ClassBasedViewChecker
 from .datetime import DatetimeChecker
 from .forms import FormFieldChecker, ModelFormChecker
+from .db_column import DbColumnChecker
 
 
 def register(linter):
@@ -43,3 +44,4 @@ def register(linter):
     linter.register_checker(DatetimeChecker(linter))
     linter.register_checker(FormFieldChecker(linter))
     linter.register_checker(ModelFormChecker(linter))
+    linter.register_checker(DbColumnChecker(linter))
