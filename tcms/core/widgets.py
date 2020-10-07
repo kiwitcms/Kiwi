93be1b01f36def32a5c29196dddd9cb04c470be8
Anton Sankov <a.sankov96@gmail.com>
2020-10-07 22:37:30 +0300

    Move SimpleMDE initialization to simplemde_security_overide.js
    - add documentation for expected parameters
    - add autoSave functionality missing from original commit in #1960
    - initialize widget only if elements exist on page
    - keep core/widgets.py compatible to minimize disruptions so this
      commit can be easily cherry-picked to master

--- tcms/core/widgets.py
+++ tcms/core/widgets.py
@@ -11,49 +11,18 @@ class SimpleMDE(forms.Textarea):
     """
         SimpleMDE widget for Django
     """
+    file_upload_id = 'simplemde-file-upload'
+
     def render(self, name, value, attrs=None, renderer=None):
         rendered_string = super().render(name, value, attrs, renderer)
         rendered_string += """
-<input id="simplemde-file-upload" type="file" style="display: none">
+<input id="%s" type="file" style="display: none">
 <script>
-var simplemde = new SimpleMDE({
-    element: document.getElementById("%s"),
-    autoDownloadFontAwesome: false,
-    renderingConfig: {
-        codeSyntaxHighlighting: true,
-    },
-    toolbar: [
-        "heading", "bold", "italic", "strikethrough", "|",
-        "ordered-list", "unordered-list", "table", "horizontal-rule", "code", "quote", "|",
-        "link",
-        {
-            // todo: standard shortcut is (Ctrl-Alt-I) but I can't find a way
-            // to assign shortcuts to customized buttons
-            name: "image",
-            action: function handler(editor){
-                $('#simplemde-file-upload').click();
-            },
-            className: "fa fa-picture-o",
-            title: "Insert Image",
-        },
-        {
-            name: "file",
-            action: function handler(editor){
-                $('#simplemde-file-upload').click();
-            },
-            className: "fa fa-paperclip",
-            title: "Attach File",
-        },
-        "|", "preview", "side-by-side", "fullscreen", "|", "guide"
-    ],
-    autosave: {
-        enabled: true,
-        uniqueId: window.location.toString(),
-        delay: 5000,
-    }
+$(document).ready(function() {
+    initSimpleMDE(document.getElementById('%s'), $('#%s'));
 });
 </script>
-""" % attrs['id']
+""" % (self.file_upload_id, attrs['id'], self.file_upload_id)
 
         return rendered_string
 
