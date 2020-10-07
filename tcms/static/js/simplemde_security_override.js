9197c0553bc865d4463d359d12f190778e964795
Anton Sankov <a.sankov96@gmail.com>
2020-10-07 22:37:30 +0300

    Move SimpleMDE initialization to simplemde_security_overide.js
    - add documentation for expected parameters
    - add autoSave functionality missing from original commit in #1960
    - initialize widget only if elements exist on page
    - keep core/widgets.py compatible to minimize disruptions so this
      commit can be easily cherry-picked to master

--- tcms/static/js/simplemde_security_override.js
+++ tcms/static/js/simplemde_security_override.js
@@ -12,7 +12,7 @@ function do_highlight(code, lang) {
 
 
 function parse_and_load_language(textarea) {
-    $(textarea).val().split("\n").forEach(function (line){
+    $(textarea).val().split("\n").forEach(function (line) {
         if (line.indexOf("```") === 0) {
             lang = line.trim().split("```")[1];
 
@@ -23,7 +23,7 @@ function parse_and_load_language(textarea) {
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     // marked.options.highlight is a synchronous operation *BUT*
     // autoloader.loadLanguages is async and we don't have much control over them
     if (window.Prism && window.Prism.plugins && window.Prism.plugins.autoloader) {
@@ -35,36 +35,10 @@ $(document).ready(function() {
 
         // keep parsing & trying to load languages as the user types b/c
         // they may be entering new text from scratch
-        $('textarea').keyup(function(event) {
+        $('textarea').keyup(function (event) {
             parse_and_load_language($(this));
         });
     }
-
-    $('#simplemde-file-upload').change(function (){
-        const attachment = this.files[0];
-              reader = new FileReader();
-
-        reader.onload = function (e){
-            const data_uri = e.target.result,
-                  mime_type = data_uri.split(':')[1],
-                  b64content = data_uri.split('base64,')[1];
-            jsonRPC('User.add_attachment', [attachment.name, b64content], function (data){
-                const cm = simplemde.codemirror,
-                      endPoint = cm.getCursor("end");
-                var text = `[${data.filename}](${data.url})\n`;
-
-                if (mime_type.startsWith('image') === true) {
-                    text = '!' + text;
-                }
-
-                cm.replaceSelection(text);
-                endPoint.ch += text.length;
-                cm.setSelection(endPoint, endPoint);
-                cm.focus();
-            });
-        };
-        reader.readAsDataURL(attachment);
-    });
 });
 
 
@@ -77,21 +51,97 @@ $(document).ready(function() {
     https://github.com/sparksuite/simplemde-markdown-editor/issues/721
     https://snyk.io/vuln/SNYK-JS-SIMPLEMDE-72570
 */
-SimpleMDE.prototype.markdown = function(text) {
+SimpleMDE.prototype.markdown = function (text) {
     var markedOptions = { sanitize: true };
 
-    if(this.options && this.options.renderingConfig && this.options.renderingConfig.singleLineBreaks === false) {
+    if (this.options && this.options.renderingConfig && this.options.renderingConfig.singleLineBreaks === false) {
         markedOptions.breaks = false;
     } else {
         markedOptions.breaks = true;
     }
 
-
-    if(this.options && this.options.renderingConfig && this.options.renderingConfig.codeSyntaxHighlighting === true && window.Prism) {
+    if (this.options && this.options.renderingConfig && this.options.renderingConfig.codeSyntaxHighlighting === true && window.Prism) {
         markedOptions.highlight = do_highlight;
     }
 
     marked.setOptions(markedOptions);
 
     return marked(text);
+}
+
+/*
+    textArea - a DOM element, not jQuery one, of a text area
+    fileUpload - a jQuery element of a hidden file upload field
+    autoSaveId - unique ID for autosave!
+*/
+function initSimpleMDE(textArea, fileUploadElement, autoSaveId=window.location.toString()) {
+    if (!textArea || !fileUploadElement) {
+        return null;
+    }
+
+    const simpleMDE = new SimpleMDE({
+        element: textArea,
+        autoDownloadFontAwesome: false,
+        renderingConfig: {
+            codeSyntaxHighlighting: true,
+        },
+        toolbar: [
+            "heading", "bold", "italic", "strikethrough", "|",
+            "ordered-list", "unordered-list", "table", "horizontal-rule", "code", "quote", "|",
+            "link",
+            {
+                // todo: standard shortcut is (Ctrl-Alt-I) but I can't find a way
+                // to assign shortcuts to customized buttons
+                name: "image",
+                action: () => {
+                    fileUploadElement.click()
+                },
+                className: "fa fa-picture-o",
+                title: "Insert Image",
+            },
+            {
+                name: "file",
+                action: () => {
+                    fileUploadElement.click()
+                },
+                className: "fa fa-paperclip",
+                title: "Attach File",
+            },
+            "|", "preview", "side-by-side", "fullscreen", "|", "guide"
+        ],
+        autosave: {
+            enabled: true,
+            uniqueId: autoSaveId,
+            delay: 5000,
+        }
+    })
+
+    fileUploadElement.change(function () {
+        const attachment = this.files[0]
+        const reader = new FileReader()
+
+        reader.onload = e => {
+            const dataUri = e.target.result
+            const mimeType = dataUri.split(':')[1]
+            const b64content = dataUri.split('base64,')[1]
+
+            jsonRPC('User.add_attachment', [attachment.name, b64content], data => {
+                const cm = simpleMDE.codemirror
+                const endPoint = cm.getCursor("end")
+                let text = `[${data.filename}](${data.url})\n`
+
+                if (mimeType.startsWith('image')) {
+                    text = '!' + text
+                }
+
+                cm.replaceSelection(text)
+                endPoint.ch += text.length
+                cm.setSelection(endPoint, endPoint)
+                cm.focus()
+            })
+        }
+        reader.readAsDataURL(attachment)
+    })
+
+    return simpleMDE
 }
