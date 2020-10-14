daf0ad92d90d4613e1d8564d989eae6ffd229da1
Mr. Senko <atodorov@mrsenko.com>
2020-10-14 15:48:31 +0300

    Add SimpleCommentForm() class
    can be used in HTML templates if you need a simple
    textarea+fileupload fields rendered via the Django templating engine!
    
    Fields/widget attributes can be customized if required:
    https://docs.djangoproject.com/en/3.1/ref/forms/widgets/#customizing-widget-instances

---
+++ tcms/core/forms/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2020 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+from django import forms
+
+from tcms.core.widgets import SimpleMDE
+
+
+class SimpleCommentForm(forms.Form):  # pylint: disable=must-inherit-from-model-form
+    """
+        A simple class for rendering comment forms which could be
+        then manipulated via the JSON-RPC API!
+    """
+    text = forms.CharField(
+        widget=SimpleMDE(),
+        required=False,
+    )
