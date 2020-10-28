c562e92eb74a255562ea83032f635fb343a18344
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-10-28 16:39:07 +0530

    fix #1624 add automated tests for testcasestatus

---
+++ tcms/rpc/tests/test_testcasestatus.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=attribute-defined-outside-init
+
+from xmlrpc.client import ProtocolError
+from tcms.rpc.tests.utils import APITestCase, APIPermissionsTestCase
+
+
+class TestCaseStatusFilter(APITestCase):
+    """Test TestCaseStatus.filter method"""
+
+    def test_filter_case_status(self):
+        case_statuses = self.rpc_client.TestCaseStatus.filter({})
+
+        self.assertGreater(len(case_statuses), 0)
+        for case_status in case_statuses:
+            self.assertIsNotNone(case_status['id'])
+            self.assertIsNotNone(case_status['name'])
+            self.assertIsNotNone(case_status['description'])
+
+
+class TestCaseStatusFilterPermissions(APIPermissionsTestCase):
+    """Test permission for TestCaseStatus.filter method"""
+
+    permission_label = "testcases.view_testcasestatus"
+
+    def verify_api_with_permission(self):
+        case_status = self.rpc_client.TestCaseStatus.filter({})
+        self.assertGreater(len(case_status), 0)
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(ProtocolError, "403 Forbidden"):
+            self.rpc_client.TestCaseStatus.filter({})
