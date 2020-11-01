992032854d656a4491846c4dd2ab39d9aa2c8964
awalvie <agrawal.vishesh.178@gmail.com>
2020-11-02 00:40:31 +0530

    add test for tcms.rpc.api.attachment . Fix #1619

---
+++ tcms/rpc/tests/test_attachment.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=attribute-defined-outside-init, invalid-name, objects-update-used
+
+from xmlrpc.client import ProtocolError
+
+from tcms.tests import user_should_have_perm
+from tcms.rpc.tests.utils import APITestCase, APIPermissionsTestCase
+from tcms.tests.factories import TestPlanFactory
+
+
+class TestRemoveAttachment(APITestCase):
+    """Test for Attachment.remove_attachment"""
+
+    def _fixture_setup(self):
+        super()._fixture_setup()
+
+        self.plan = TestPlanFactory()
+        file_name = 'attachment.txt'
+        self.rpc_client.TestPlan.add_attachment(self.plan.pk, file_name, 'a2l3aXRjbXM=')
+
+    def test_remove_attachment(self):
+        attachments = self.rpc_client.TestPlan.list_attachments(self.plan.pk)
+        self.rpc_client.Attachment.remove_attachment(attachments[0]['pk'])
+        attachments = self.rpc_client.TestPlan.list_attachments(self.plan.pk)
+
+        self.assertEqual(0, len(attachments))
+
+
+class TestRemoveAttachmentPermissions(APIPermissionsTestCase):
+    """Test for Attachment.remove_attachment permissions"""
+
+    permission_label = 'attachments.delete_attachment'
+
+    def _fixture_setup(self):
+        super()._fixture_setup()
+
+        self.plan = TestPlanFactory()
+        user_should_have_perm(self.tester, 'attachments.add_attachment')
+        user_should_have_perm(self.tester, 'attachments.view_attachment')
+
+    def verify_api_with_permission(self):
+        file_name = 'attachment.txt'
+        self.rpc_client.TestPlan.add_attachment(self.plan.pk, file_name, 'a2l3aXRjbXM=')
+        attachments = self.rpc_client.TestPlan.list_attachments(self.plan.pk)
+
+        self.assertEqual(1, len(attachments))
+
+        self.rpc_client.Attachment.remove_attachment(attachments[0]['pk'])
+        attachments = self.rpc_client.TestPlan.list_attachments(self.plan.pk)
+
+        self.assertEqual(0, len(attachments))
+
+    def verify_api_without_permission(self):
+        file_name = 'attachment.txt'
+        self.rpc_client.TestPlan.add_attachment(self.plan.pk, file_name, 'a2l3aXRjbXM=')
+        attachments = self.rpc_client.TestPlan.list_attachments(self.plan.pk)
+
+        self.assertEqual(1, len(attachments))
+
+        with self.assertRaisesRegex(ProtocolError, '403 Forbidden'):
+            self.rpc_client.Attachment.remove_attachment(attachments[0]['pk'])
