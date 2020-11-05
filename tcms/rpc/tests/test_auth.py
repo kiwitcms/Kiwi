2b060d439540fe6d6e79fc327854177b2f63cf07
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-11-05 16:33:04 +0530

    fix #1620 add automated tests for missing coverage in tcms.rpc.api.auth

---
+++ tcms/rpc/tests/test_auth.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=attribute-defined-outside-init
+
+from xmlrpc.client import Fault as XmlRPCFault, ProtocolError
+from tcms.rpc.tests.utils import APITestCase
+
+
+class TestAuthLogin(APITestCase):
+    """Test Auth.login method"""
+
+    def _fixture_setup(self):
+        super()._fixture_setup()
+
+        self.rpc_client.Auth.logout()
+
+    def test_login_with_username(self):
+        sesson_id = self.rpc_client.Auth.login(self.api_user.username, "api-testing")
+        self.assertIsNotNone(sesson_id)
+
+    def test_login_without_password(self):
+        with self.assertRaisesRegex(XmlRPCFault, "Username and password is required"):
+            self.rpc_client.Auth.login(self.api_user.username, "")
+
+    def test_login_with_incorrect_password(self):
+        with self.assertRaisesRegex(XmlRPCFault, "Wrong username or password"):
+            self.rpc_client.Auth.login(self.api_user.username, "kiwi-password")
+
+
+class TestAuthLogout(APITestCase):
+    """Test Auth.logout method"""
+
+    def test_logout(self):
+        self.rpc_client.Auth.logout()
+        with self.assertRaisesRegex(ProtocolError, "403 Forbidden"):
+            # this method requires a logged-in user
+            self.rpc_client.Bug.details("http://some.url")
