71ea73dd7a94bc5d41f0021d4fe4acf77aeabe02
Gagan Deep <pandafy.dev@gmail.com>
2020-11-12 21:36:37 +0530

    Fixes #1612 - Add test for tcms.utils.github

--- tcms/utils/tests/test_github.py
+++ tcms/utils/tests/test_github.py
@@ -8,6 +8,12 @@ from django.http import HttpResponseForbidden
 from tcms.utils import github
 
 
+class CalculateSignatureTestCase(test.TestCase):
+    def test_return_format(self):
+        self.assertEqual(github.calculate_signature(b'secret', b'content'),
+                         'sha1=0bd98d1a7514a85bbb8377bb8d750b6e01494056')
+
+
 class VerifySignatureTestCase(test.TestCase):
     def setUp(self):
         self.factory = test.RequestFactory()
@@ -27,3 +33,12 @@ class VerifySignatureTestCase(test.TestCase):
 
         self.assertIsInstance(result, HttpResponseForbidden)
         self.assertEqual(HTTPStatus.FORBIDDEN, result.status_code)
+
+    def test_valid_signature_header(self):
+        request = self.factory.post(self.url)
+        request.META['HTTP_X_HUB_SIGNATURE'] = (
+            github.calculate_signature(b'secret', request.body)
+        )
+        result = github.verify_signature(request, b'secret')
+        self.assertIsInstance(result, bool)
+        self.assertTrue(result)
