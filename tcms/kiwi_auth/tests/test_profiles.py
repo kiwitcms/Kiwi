270a8e4ad74bf5586a1a5758d622c844dcb4719a
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-12-10 23:54:36 +0530

    fix #1608 add test for tcms.kiwi_auth.views

--- tcms/kiwi_auth/tests/test_profiles.py
+++ tcms/kiwi_auth/tests/test_profiles.py
@@ -3,29 +3,22 @@
 
 from http import HTTPStatus
 
-from django.test import TestCase
 from django.http import HttpResponseForbidden
 from django.urls import reverse
 
-from tcms.tests import create_request_user
+from tcms.tests import create_request_user, LoggedInTestCase
 from tcms.tests import user_should_have_perm
 
 
-class TestProfilesView(TestCase):
+class TestProfilesView(LoggedInTestCase):
     """Test the profiles view functionality"""
     @classmethod
     def setUpClass(cls):
-        super(TestProfilesView, cls).setUpClass()
+        super().setUpClass()
 
-        cls.tester = create_request_user('tester', 'password')
         cls.somebody_else = create_request_user('somebody-else', 'password')
 
     def test_user_can_view_their_own_profile(self):
-        logged_in = self.client.login(  # nosec:B106:hardcoded_password_funcarg
-            username=self.tester.username,
-            password='password')
-        self.assertTrue(logged_in)
-
         url = reverse('tcms-profile', args=[self.tester.username])
         response = self.client.get(url, follow=True)
 
@@ -35,11 +28,6 @@ class TestProfilesView(TestCase):
         self.assertContains(response, 'name="_save"')
 
     def test_user_cant_view_profile_of_another_user_without_permission(self):
-        logged_in = self.client.login(  # nosec:B106:hardcoded_password_funcarg
-            username=self.tester.username,
-            password='password')
-        self.assertTrue(logged_in)
-
         url = reverse('tcms-profile', args=[self.somebody_else.username])
         response = self.client.get(url, follow=True)
         self.assertIsInstance(response, HttpResponseForbidden)
@@ -47,14 +35,13 @@ class TestProfilesView(TestCase):
     def test_user_can_view_profile_of_another_user_with_permission(self):
         user_should_have_perm(self.tester, 'auth.view_user')
 
-        logged_in = self.client.login(  # nosec:B106:hardcoded_password_funcarg
-            username=self.tester.username,
-            password='password')
-        self.assertTrue(logged_in)
-
         url = reverse('tcms-profile', args=[self.somebody_else.username])
         response = self.client.get(url, follow=True)
 
         self.assertContains(response, self.somebody_else.username)
         self.assertContains(response, self.somebody_else.email)
         self.assertNotContains(response, 'name="_save"')
+
+    def test_view_if_user_is_invalid(self):
+        response = self.client.get(reverse("tcms-profile", args=["non-existing-username"]))
+        self.assertEqual(response.status_code, 404)
