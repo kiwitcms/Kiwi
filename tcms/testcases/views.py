3707e1dc4cc8295e10908fc52b59ec6000431816
Mr. Senko <atodorov@mrsenko.com>
2020-12-15 22:24:18 +0200

    Remove unused methods & left-over views
    CC @asankov

--- tcms/testcases/views.py
+++ tcms/testcases/views.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.test import modify_settings
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
-from django.views.decorators.http import require_POST
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateView, View
 from django.views.generic.edit import CreateView, UpdateView
 from guardian.decorators import permission_required as object_permission_required
 
-from tcms.core.helpers.comments import get_comments
 from tcms.core.response import ModifySettingsTemplateResponse
 from tcms.testcases.forms import (
     CaseNotifyFormSet,
@@ -23,23 +20,8 @@ from tcms.testcases.forms import (
     SearchCaseForm,
     TestCaseForm,
 )
-from tcms.testcases.models import TestCase, TestCaseStatus
+from tcms.testcases.models import TestCase
 from tcms.testplans.models import TestPlan
-from tcms.testruns.models import TestExecution, TestExecutionStatus
-
-TESTCASE_OPERATION_ACTIONS = (
-    "search",
-    "sort",
-    "update",
-    "remove",  # including remove tag from cases
-    "add",  # including add tag to cases
-    "change",
-    "delete_cases",  # unlink cases from a TestPlan
-)
-
-
-# _____________________________________________________________________________
-# helper functions
 
 
 def plan_from_request_or_none(request):  # pylint: disable=missing-permission-required
@@ -105,118 +87,6 @@ class NewCaseView(CreateView):
         )
 
 
-def get_case_status(template_type):
-    """Get part or all TestCaseStatus according to template type"""
-    confirmed_status_name = "CONFIRMED"
-    if template_type == "case":
-        d_status = TestCaseStatus.objects.filter(name=confirmed_status_name)
-    elif template_type == "review_case":
-        d_status = TestCaseStatus.objects.exclude(name=confirmed_status_name)
-    else:
-        d_status = TestCaseStatus.objects.all()
-    return d_status
-
-
-@require_POST
-def build_cases_search_form(  # pylint: disable=missing-permission-required
-    request, populate=None, plan=None
-):
-    """Build search form preparing for quering TestCases"""
-    # Initial the form and template
-    action = request.POST.get("a")
-    if action in TESTCASE_OPERATION_ACTIONS:
-        search_form = SearchCaseForm(request.POST)
-        request.session["items_per_page"] = request.POST.get(
-            "items_per_page", settings.DEFAULT_PAGE_SIZE
-        )
-    else:
-        d_status = get_case_status(request.POST.get("template_type"))
-        d_status_ids = d_status.values_list("pk", flat=True)
-        items_per_page = request.session.get(
-            "items_per_page", settings.DEFAULT_PAGE_SIZE
-        )
-        search_form = SearchCaseForm(
-            initial={"case_status": d_status_ids, "items_per_page": items_per_page}
-        )
-
-    if populate:
-        if request.POST.get("product"):
-            search_form.populate(product_id=request.POST["product"])
-        elif plan and plan.product_id:
-            search_form.populate(product_id=plan.product_id)
-        else:
-            search_form.populate()
-
-    return search_form
-
-
-def query_testcases_from_request(
-    request, plan=None
-):  # pylint: disable=missing-permission-required
-    """Query TestCases according to criterias coming within REQUEST
-
-    :param request: the REQUEST object.
-    :type request: :class:`django.http.HttpRequest`
-
-    :param plan: instance of TestPlan to restrict only those TestCases belongs to
-                 the TestPlan. Can be None. As you know, query from all TestCases.
-    :type plan: :class:`tcms.testplans.models.TestPlan`
-
-    :return: Queryset with testcases and search form
-    :rtype: :class:`django.db.models.query.QuerySet`, dict
-    """
-    search_form = build_cases_search_form(request, True, plan)
-
-    action = request.POST.get("a")
-    if action == "initial":
-        # todo: build_cases_search_form will also check TESTCASE_OPERATION_ACTIONS
-        # and return slightly different values in case of initialization
-        # move the check there and just execute the query here if the data
-        # is valid
-        d_status = get_case_status(request.POST.get("template_type"))
-        tcs = TestCase.objects.filter(case_status__in=d_status)
-    elif action in TESTCASE_OPERATION_ACTIONS and search_form.is_valid():
-        tcs = TestCase.list(search_form.cleaned_data, plan)
-    else:
-        tcs = TestCase.objects.none()
-
-    # Search the relationship
-    if plan:
-        tcs = tcs.filter(plan=plan)
-
-    tcs = tcs.select_related(
-        "author", "default_tester", "case_status", "priority", "category", "reviewer"
-    )
-    return tcs, search_form
-
-
-def get_selected_testcases(request):  # pylint: disable=missing-permission-required
-    """Get selected TestCases from client side
-
-    TestCases are selected in two cases. One is user selects part of displayed
-    TestCases, where there should be at least one variable named case, whose
-    value is the TestCase Id. Another one is user selects all TestCases based
-    on previous filter criterias even through there are non-displayed ones. In
-    this case, another variable selectAll appears in the REQUEST. Whatever its
-    value is.
-
-    If neither variables mentioned exists, empty query result is returned.
-
-    :param request: django's HttpRequest
-    :type request: :class:`django.http.HttpRequest`
-
-    :return: Queryset with testcases
-    :rtype: :class:`django.db.models.query.QuerySet`
-    """
-    method = request.POST or request.GET
-    if method.get("selectAll", None):
-        plan = plan_from_request_or_none(request)
-        cases, _search_form = query_testcases_from_request(request, plan)
-        return cases
-
-    return TestCase.objects.filter(pk__in=method.getlist("case"))
-
-
 @method_decorator(permission_required("testcases.view_testcase"), name="dispatch")
 class TestCaseSearchView(TemplateView):
     """
@@ -235,52 +105,6 @@ class TestCaseSearchView(TemplateView):
         return {
             "form": form,
         }
-
-
-@method_decorator(permission_required("testruns.view_testexecution"), name="dispatch")
-class TestCaseExecutionDetailPanelView(TemplateView):
-    """Display execution detail in run page"""
-
-    template_name = "case/get_details_case_run.html"
-    execution_id = None
-    case_text_version = None
-
-    def get(self, request, *args, **kwargs):
-        try:
-            self.execution_id = int(request.GET.get("execution_id"))
-            self.case_text_version = int(request.GET.get("case_text_version"))
-        except (TypeError, ValueError):
-            raise Http404 from None
-
-        return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-
-        case = TestCase.objects.get(pk=kwargs["case_id"])
-        execution = TestExecution.objects.get(pk=self.execution_id)
-
-        # Data of TestCase
-        test_case_text = case.get_text_with_version(self.case_text_version)
-
-        # Data of TestExecution
-        execution_comments = get_comments(execution)
-
-        execution_status = TestExecutionStatus.objects.order_by("-weight", "name")
-
-        data.update(
-            {
-                "test_case": case,
-                "test_case_text": test_case_text,
-                "execution": execution,
-                "comments_count": len(execution_comments),
-                "execution_comments": execution_comments,
-                "execution_logs": execution.history.all(),
-                "execution_status": execution_status,
-            }
-        )
-
-        return data
 
 
 @method_decorator(
