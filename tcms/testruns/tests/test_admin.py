0921716bc384b7e71708d04347b22a490a94ef3c
Mariyan Garvanski <mgarvanski@gmail.com>
2021-01-08 16:54:06 +0200

    Add tests for delete_view method. Closes #1618

--- tcms/testruns/tests/test_admin.py
+++ tcms/testruns/tests/test_admin.py
@@ -2,8 +2,9 @@
 # pylint: disable=invalid-name
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from parameterized import parameterized
 
-from tcms.testruns.models import TestRun
+from tcms.testruns.models import TestExecutionStatus, TestRun
 from tcms.tests import LoggedInTestCase
 from tcms.tests.factories import TestRunFactory, UserFactory
 from tcms.utils.permissions import initiate_user_with_default_setups
@@ -90,4 +91,49 @@ class TestTestExecutionStatusAdmin(LoggedInTestCase):
             <span class="fa fa-check-circle-o" style="font-size: 18px; color: #92d400;"></span>
             """
             ),
+        )
+
+    @parameterized.expand(
+        [
+            ("positive", TestExecutionStatus.objects.all().filter(weight__gt=0)),
+            ("neural", TestExecutionStatus.objects.all().filter(weight=0)),
+            ("negative", TestExecutionStatus.objects.all().filter(weight__lt=0)),
+        ]
+    )
+    def test_test_execution_statuses_can_be_deleted_but_one_must_remain(
+        self, _name, exe_statuses
+    ):
+        for exe_status in exe_statuses[1:]:
+            response = self.client.get(
+                reverse(
+                    "admin:testruns_testexecutionstatus_delete", args=[exe_status.pk]
+                )
+            )
+            self.assertContains(response, _("Yes, I'm sure"))
+            response = self.client.post(
+                reverse(
+                    "admin:testruns_testexecutionstatus_delete", args=[exe_status.pk]
+                ),
+                {"post": "yes"},
+                follow=True,
+            )
+            self.assertRedirects(
+                response, reverse("admin:testruns_testexecutionstatus_changelist")
+            )
+            self.assertEqual(
+                TestExecutionStatus.objects.filter(pk=exe_status.pk).exists(), False
+            )
+
+        exe_status = exe_statuses.first()
+        response = self.client.get(
+            reverse("admin:testruns_testexecutionstatus_delete", args=[exe_status.pk]),
+            follow=True,
+        )
+        self.assertRedirects(
+            response, reverse("admin:testruns_testexecutionstatus_changelist")
+        )
+        self.assertContains(
+            response,
+            _("1 negative, 1 neutral & 1 positive status required!"),
+            html=True,
         )
