3f21f655a58df4dedd792a2e249a16ff9fcc60b1
Mr. Senko <atodorov@mrsenko.com>
2021-01-20 22:32:11 +0200

    Update tests for PlanType.filter() API method
    convert the functional test into functional + permissions test

--- tcms/rpc/tests/test_plantype.py
+++ tcms/rpc/tests/test_plantype.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=attribute-defined-outside-init
 
-from tcms.rpc.tests.utils import APITestCase
+from xmlrpc.client import ProtocolError
+
+from tcms.rpc.tests.utils import APIPermissionsTestCase
 from tcms.tests.factories import PlanTypeFactory
 
 
-class PlanTypeMethods(APITestCase):
+class TestPlanTypeFilter(APIPermissionsTestCase):
+    permission_label = "testplans.view_plantype"
+
     def _fixture_setup(self):
         super()._fixture_setup()
-
         self.plan_type = PlanTypeFactory(name="xmlrpc plan type", description="")
 
-    def test_filter(self):
+    def verify_api_with_permission(self):
         result = self.rpc_client.PlanType.filter({"name": self.plan_type.name})[0]
         self.assertEqual(self.plan_type.name, result["name"])
         self.assertEqual(self.plan_type.description, result["description"])
         self.assertEqual(self.plan_type.pk, result["id"])
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(ProtocolError, "403 Forbidden"):
+            self.rpc_client.PlanType.filter({"name": self.plan_type.name})
