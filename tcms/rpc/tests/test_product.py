65bd158648124b5db3bae66f352b162b9648ff01
Mr. Senko <atodorov@mrsenko.com>
2021-01-20 14:53:12 +0200

    tests: add tests for Product.create() API method

--- tcms/rpc/tests/test_product.py
+++ tcms/rpc/tests/test_product.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=attribute-defined-outside-init
 
-from tcms.rpc.tests.utils import APITestCase
-from tcms.tests.factories import ProductFactory
+from xmlrpc.client import ProtocolError
+
+from tcms.management.models import Product
+from tcms.rpc.tests.utils import APIPermissionsTestCase, APITestCase
+from tcms.tests.factories import ClassificationFactory, ProductFactory
 
 
 class TestFilter(APITestCase):
@@ -22,3 +25,37 @@ class TestFilter(APITestCase):
     def test_filter_non_existing(self):
         result = self.rpc_client.Product.filter({"name": "Non Existing"})
         self.assertEqual(0, len(result))
+
+
+class TestProductCreatePermissions(APIPermissionsTestCase):
+    permission_label = "management.add_product"
+
+    def _fixture_setup(self):
+        super()._fixture_setup()
+        self.classification = ClassificationFactory()
+
+    def verify_api_with_permission(self):
+        result = self.rpc_client.Product.create(
+            {
+                "name": "Product with Permissions",
+                "classification_id": self.classification.pk,
+            }
+        )
+
+        # verify the serialized result
+        self.assertEqual(result["name"], "Product with Permissions")
+        self.assertEqual(result["description"], "")
+        self.assertIn("classification", result)
+
+        # verify the object from the DB
+        product = Product.objects.get(pk=result["id"])
+        self.assertEqual(product.name, "Product with Permissions")
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(ProtocolError, "403 Forbidden"):
+            self.rpc_client.Product.create(
+                {
+                    "name": "Product with Permissions",
+                    "classification_id": self.classification.pk,
+                }
+            )
