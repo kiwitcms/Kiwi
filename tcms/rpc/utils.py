d37af5ec2ce0e58e94b3c81b25d6e6d8fa3b8b1e
Mr. Senko <atodorov@mrsenko.com>
2021-02-16 11:44:12 +0200

    Remove now unused Serializer and TCMSActionModel classes
    and supporting functions

--- tcms/rpc/utils.py
+++ tcms/rpc/utils.py
@@ -5,99 +5,11 @@ import time
 
 from attachments import views as attachment_views
 from attachments.models import Attachment
-from django.core.exceptions import FieldDoesNotExist
 from django.http import HttpRequest
 from django.middleware.csrf import get_token
 from mock import MagicMock
 
 from tcms.core.utils import request_host_link
-
-QUERY_DISTINCT = 1
-
-
-def _lookup_fields_in_model(cls, fields):
-    """Lookup ManyToMany fields in current table and related tables. For
-    distinct duplicate rows when using inner join
-
-    @param cls: table model class
-    @type cls: subclass of django.db.models.Model
-    @param fields: fields in where condition.
-    @type fields: list
-    @return: whether use distinct or not
-    @rtype: bool
-
-    Example:
-        cls is TestRun (<class 'tcms.testruns.models.TestRun'>)
-        fields is 'plan__cases__is_automated'
-                    |     |         |----- Normal Field in TestCase
-                    |     |--------------- ManyToManyKey in TestPlan
-                    |--------------------- ForeignKey in TestRun
-
-    1. plan is a ForeignKey field of TestRun and it will trigger getting the
-    related model TestPlan by django orm framework.
-    2. case is a ManyToManyKey field of TestPlan and it will trigger using
-    INNER JOIN to join TestCase, here will be many duplicated rows.
-    3. is_automated is a local field of TestCase only filter the rows (where
-    condition).
-
-    So this method will find out that case is a m2m field and notice the
-    outter method use distinct to avoid duplicated rows.
-    """
-    for field_name in fields:
-        try:
-            field = cls._meta.get_field(field_name)
-            if field.is_relation and field.many_to_many:
-                yield True
-            else:
-                if getattr(field, "remote_field", None):
-                    cls = field.remote_field.model
-        except FieldDoesNotExist:
-            pass
-
-
-def _need_distinct_m2m_rows(cls, fields):
-    """Check whether the query string has ManyToMany field or not, return
-    False if the query string is empty.
-
-    @param cls: table model class
-    @type cls: subclass of django.db.models.Model
-    @param fields: fields in where condition.
-    @type fields: list
-    @return: whether use distinct or not
-    @rtype: bool
-    """
-    return next(_lookup_fields_in_model(cls, fields), False) if fields else False
-
-
-def distinct_m2m_rows(cls, values, op_type):
-    """By django model field looking up syntax, loop values and check the
-    condition if there is a multi-tables query.
-
-    @param cls: table model class
-    @type cls: subclass of django.db.models.Model
-    @param values: fields in where condition.
-    @type values: dict
-    @param op_type:
-    @type op_type: int
-    @return: QuerySet
-    @rtype: :class:`django.db.models.query.QuerySet`
-    @raises TypeError:
-    """
-    flag = False
-    for field in values.keys():
-        if "__" in field:
-            if _need_distinct_m2m_rows(cls, field.split("__")):
-                flag = True
-                break
-
-    qs = cls.objects.filter(**values)
-    if op_type == QUERY_DISTINCT:
-        return qs.distinct() if flag else qs
-    raise TypeError("Not implement op type %s" % op_type)
-
-
-def distinct_filter(cls, values):
-    return distinct_m2m_rows(cls, values, op_type=QUERY_DISTINCT)
 
 
 def get_attachments_for(request, obj):
