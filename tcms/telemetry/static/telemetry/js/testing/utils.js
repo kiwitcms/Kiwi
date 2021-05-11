4c1e0eeae845e0ff8b166997e79fc913a429a11c
Shantanu Verma <vermashantanu@hotmail.com>
2021-05-11 16:45:25 +0530

    fix(telemetry): close multiselect widget when making single selections

--- tcms/telemetry/static/telemetry/js/testing/utils.js
+++ tcms/telemetry/static/telemetry/js/testing/utils.js
@@ -8,3 +8,12 @@ function loadInitialProduct(callback = () => {}) {
 function showOnlyRoundNumbers(number) {
     return number % 1 === 0 ? number : '';
 }
+
+// Close multiselect list when selecting an item
+// Iterate over all dropdown lists
+$("select:not([size])").each(function() {
+	$(this).on("change", function() {
+		// Clicking on the document closes the widget
+		$(document).click(); 
+	});
+});
