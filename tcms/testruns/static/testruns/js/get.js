37b539c59091e9d3ce43152a302766756f861153
Alexander Todorov <atodorov@mrsenko.com>
2021-05-19 15:22:45 +0300

    Simplify 2 UI buttons on TestRun page

--- tcms/testruns/static/testruns/js/get.js
+++ tcms/testruns/static/testruns/js/get.js
@@ -10,10 +10,6 @@ const allExecutionStatuses = {},
 
 $(document).ready(() => {
 
-    $('[data-toggle="tooltip"]').tooltip({
-        trigger: 'hover'
-    });
-
     permissions.removeTag = $('#test_run_pk').data('perm-remove-tag') === 'True'
     permissions.addComment = $('#test_run_pk').data('perm-add-comment') === 'True'
     permissions.removeComment = $('#test_run_pk').data('perm-remove-comment') === 'True'
