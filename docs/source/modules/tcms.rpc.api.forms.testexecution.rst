44d4fcc2e681832c6033bbacc0e0e938156f0560
Alexander Todorov <atodorov@mrsenko.com>
2021-07-01 19:38:29 +0300

    Apply code review, adjustments and fixes for the previous commit
    Changelog:
    
    When using API method TestExecution.add_link() it is recommended to use
    `execution` instead of `execution_id` in input field values.

---
+++ docs/source/modules/tcms.rpc.api.forms.testexecution.rst
@@ -0,0 +1,7 @@
+tcms.rpc.api.forms.testexecution module
+=======================================
+
+.. automodule:: tcms.rpc.api.forms.testexecution
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- tcms/rpc/api/forms/testexecution.py
+++ tcms/rpc/api/forms/testexecution.py
@@ -1,28 +1,9 @@
 from django import forms
-from django.contrib.auth import get_user_model
 
 from tcms.core.contrib.linkreference.models import LinkReference
-from tcms.core.forms.fields import UserField
-from tcms.rpc.api.forms import UpdateModelFormMixin, DateTimeField
-from tcms.testruns.models import TestExecution
-
-User = get_user_model()  # pylint: disable=invalid-name
 
 
-class UpdateExecutionForm(UpdateModelFormMixin, forms.ModelForm):
-    class Meta:
-        model = TestExecution
-        fields = '__all__'
-
-    assignee = UserField()
-    tested_by = UserField()
-    close_date = DateTimeField()
-
-
-class AddExecutionLinkForm(forms.ModelForm):
+class LinkReferenceForm(forms.ModelForm):
     class Meta:
         model = LinkReference
-        fields = '__all__'
-
-    def populate(self, execution_id):
-        self.fields['execution'].queryset = TestExecution.objects.filter(pk=execution_id)
+        fields = "__all__"
