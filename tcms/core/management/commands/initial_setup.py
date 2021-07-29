5c0cd5d8e608d39b2b0ebe37f07928d8a01e4774
Ivajlo Karabojkov <karabojkov@kitbg.com>
2021-07-29 23:57:43 +0300

    Use initial_setup to create public tenant.
    Refs https://github.com/kiwitcms/enterprise/issues/88

--- tcms/core/management/commands/initial_setup.py
+++ tcms/core/management/commands/initial_setup.py
@@ -1,5 +1,10 @@
+import datetime
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 
 class Command(BaseCommand):
@@ -19,5 +24,22 @@ class Command(BaseCommand):
 
         self.stdout.write("\n4. Setting permissions:")
         call_command("refresh_permissions", "--verbosity=%i" % kwargs["verbosity"])
+
+        if "tcms_tenants" in settings.INSTALLED_APPS:
+            self.stdout.write("\n5. Creating the public tenant:")
+            superuser = get_user_model().objects.filter(is_superuser=True).first()
+            paid_until = timezone.now() + datetime.timedelta(days=100 * 365)
+            call_command(
+                "create_tenant",
+                "--verbosity=%i" % kwargs["verbosity"],
+                "-schema_name public",
+                "--name 'Public tenant'",
+                "--paid_until %s" % paid_until.isoformat(),
+                "--publicly_readable False",
+                "--owner_id %i" % superuser.pk,
+                "--organization 'Testing department'",
+                "--domain-domain %s" % domain,
+                "--domain-is_primary True",
+            )
 
         self.stdout.write("\nInitial setup finished.")
