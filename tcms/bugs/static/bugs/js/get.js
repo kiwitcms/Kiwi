65191e62820296d252af6f9c12c5b71455a6aaa0
Anton Sankov <a.sankov96@gmail.com>
2021-08-20 21:32:29 +0300

    Fix eslint issues in `bugs/js/get.js`
    - run `eslint --fix`
    - rename manually the snake_case identifier to camelCase

--- tcms/bugs/static/bugs/js/get.js
+++ tcms/bugs/static/bugs/js/get.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-    var object_id = $('#object_pk').data('pk');
-    var perm_remove_tag = $('#object_pk').data('perm-remove-tag') === 'True';
+$(document).ready(function () {
+  const objectId = $('#object_pk').data('pk')
+  const permRemoveTag = $('#object_pk').data('perm-remove-tag') === 'True'
 
-    // bind everything in tags table
-    tagsCard('Bug', object_id, {bugs: object_id}, perm_remove_tag);
+  // bind everything in tags table
+  tagsCard('Bug', objectId, { bugs: objectId }, permRemoveTag)
 
-    // executions tree view
-    treeViewBind();
-});
+  // executions tree view
+  treeViewBind()
+})
