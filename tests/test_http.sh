fa192f7aea2800d378aa77e94875e36ca9715aed
Ivajlo Karabojkov <karabojkov@kitbg.com>
2021-09-11 16:32:44 +0300

    Add non-ssl test and fix ssl one.

--- tests/test_http.sh
+++ tests/test_http.sh
@@ -24,21 +24,31 @@ rlJournalStart
         assert_up_and_running
     rlPhaseEnd
 
-    rlPhaseStartTest "Check for SSL warning message"
+    rlPhaseStartTest "Should not display SSL warning for HTTPS connection"
         rlRun -t -c "docker exec -i kiwi_web /Kiwi/manage.py createsuperuser \
             --username testadmin --email testadmin@domain.com --noinput"
-        rlRun -t -c "cat tests/set_testadmin_pass | docker exec -i kiwi_web /Kiwi/manage.py shell"
+        rlRun -t -c "cat tests/set_testadmin_pass.py | docker exec -i kiwi_web /Kiwi/manage.py shell"
         IP_ADDRESS=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kiwi_web`
         rm -f /tmp/testcookies.txt
         rm -f /tmp/testdata.sslon
         rlRun -t -c "curl -k -L -o- -c /tmp/testcookies.txt https://$IP_ADDRESS:8443/"
         CSRF_TOKEN=`grep csrftoken /tmp/testcookies.txt | cut -f 7`
-        rlRun -t -c "curl -e https://$IP_ADDRESS:8443/ -d username=testadmin \
-            -d password=password -d csrfmiddlewaretoken=$CSRF_TOKEN -k -L -o /tmp/testdata.sslon \
+        rlRun -t -c "curl -e https://$IP_ADDRESS:8443/accounts/login/ -d username=testadmin \
+            -d password=password -d csrfmiddlewaretoken=$CSRF_TOKEN -k -L -i -o /tmp/testdata.sslon \
             -b /tmp/testcookies.txt https://$IP_ADDRESS:8443/accounts/login/"
         rlAssertGrep "<title>Kiwi TCMS - Dashboard</title>" /tmp/testdata.sslon
         rlAssertNotGrep "You are not using a secure connection." /tmp/testdata.sslon
     rlPhaseEnd
+
+    rlPhaseStartTest "Should display SSL warning for HTTP connection"
+        rm -f /tmp/testcookies.txt
+        rlRun -t -c "curl -k -L -o- -c /tmp/testcookies.txt http://$IP_ADDRESS:8080/"
+        CSRF_TOKEN=`grep csrftoken /tmp/testcookies.txt | cut -f 7`
+        rlRun -t -c "curl -e http://$IP_ADDRESS:8080/accounts/login/ -d username=testadmin \
+            -d password=password -d csrfmiddlewaretoken=$CSRF_TOKEN -k -L -i -o /tmp/testdata.ssloff \
+            -b /tmp/testcookies.txt http://$IP_ADDRESS:8080/accounts/login/"
+        rlAssertGrep "<title>Kiwi TCMS - Dashboard</title>" /tmp/testdata.ssloff
+        rlAssertGrep "You are not using a secure connection." /tmp/testdata.ssloff
 
     rlPhaseStartCleanup
         rlRun -t -c "docker-compose down"
