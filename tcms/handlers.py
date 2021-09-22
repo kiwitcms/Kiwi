92f1e5e81aa5cd10ffbdff604e8a7e7f002ea817
Alexander Todorov <atodorov@mrsenko.com>
2021-09-22 12:58:54 +0300

    API: serialize timedelta to seconds
    WARNINGS:
    
    - Renamed SafeJSONRPCHandler to KiwiTCMSJsonRpcHandler
    - New handler KiwiTCMSXmlRpcHandler
    - Updated MODERNRPC_HANDLERS setting !

--- tcms/handlers.py
+++ tcms/handlers.py
@@ -1,23 +1,28 @@
 # coding: utf-8
 import html
+from datetime import timedelta
 
-from modernrpc.handlers import JSONRPCHandler
+from modernrpc.handlers import JSONRPCHandler, XMLRPCHandler
 
 
-class SafeJSONRPCHandler(JSONRPCHandler):
+class KiwiTCMSJsonRpcHandler(JSONRPCHandler):
     @staticmethod
     def escape_dict(result_dict):
         for (key, value) in result_dict.items():
             if isinstance(value, str):
                 result_dict[key] = html.escape(value)
+            elif isinstance(value, timedelta):
+                result_dict[key] = value.total_seconds()
 
     @staticmethod
     def escape_list(result_list):
         for (index, item) in enumerate(result_list):
             if isinstance(item, str):
                 result_list[index] = html.escape(item)
+            elif isinstance(item, timedelta):
+                result_list[index] = item.total_seconds()
             elif isinstance(item, dict):
-                SafeJSONRPCHandler.escape_dict(item)
+                __class__.escape_dict(item)
 
     def execute_procedure(self, name, args=None, kwargs=None):
         """
@@ -30,6 +35,36 @@ class SafeJSONRPCHandler(JSONRPCHandler):
 
         if isinstance(result, str):
             result = html.escape(result)
+        elif isinstance(result, timedelta):
+            result = result.total_seconds()
+        elif isinstance(result, dict):
+            self.escape_dict(result)
+        elif isinstance(result, list):
+            self.escape_list(result)
+
+        return result
+
+
+class KiwiTCMSXmlRpcHandler(XMLRPCHandler):
+    @staticmethod
+    def escape_dict(result_dict):
+        for (key, value) in result_dict.items():
+            if isinstance(value, timedelta):
+                result_dict[key] = value.total_seconds()
+
+    @staticmethod
+    def escape_list(result_list):
+        for (index, item) in enumerate(result_list):
+            if isinstance(item, timedelta):
+                result_list[index] = item.total_seconds()
+            elif isinstance(item, dict):
+                __class__.escape_dict(item)
+
+    def execute_procedure(self, name, args=None, kwargs=None):
+        result = super().execute_procedure(name, args, kwargs)
+
+        if isinstance(result, timedelta):
+            result = result.total_seconds()
         elif isinstance(result, dict):
             self.escape_dict(result)
         elif isinstance(result, list):
