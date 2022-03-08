d13de2ab2e48bfc93f9347a5d0a44b719d216a82
Alexander Todorov <atodorov@mrsenko.com>
2022-03-08 22:13:53 +0200

    Changelog for v11.2

--- tcms/__init__.py
+++ tcms/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-__version__ = "11.1"
-__release_date__ = datetime(2022, 2, 2, 13, 0)
+__version__ = "11.2"
+__release_date__ = datetime(2022, 3, 9, 0, 1)
