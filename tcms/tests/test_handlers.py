9f8965731c2048c8a3b9768bacaab44f81893b4c
Antoine Lorence <contact@alorence.me>
2022-06-01 14:10:34 +0200

    Update RPC handlers and associated tests for django-modern-rpc 1.0.0

--- tcms/tests/test_handlers.py
+++ tcms/tests/test_handlers.py
@@ -1,9 +1,9 @@
 import html
 from datetime import timedelta
 from unittest import TestCase
-from unittest.mock import patch
 
-from django.test import RequestFactory
+from modernrpc.handlers.jsonhandler import JsonSuccessResult
+from modernrpc.handlers.xmlhandler import XmlSuccessResult
 
 from tcms.handlers import KiwiTCMSJsonRpcHandler, KiwiTCMSXmlRpcHandler
 
@@ -11,99 +11,103 @@ from tcms.handlers import KiwiTCMSJsonRpcHandler, KiwiTCMSXmlRpcHandler
 class TestKiwiTCMSJsonRpcHandler(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.base_exec_procedure = "modernrpc.handlers.JSONRPCHandler.execute_procedure"
-        cls.rpc_handler = KiwiTCMSJsonRpcHandler(
-            RequestFactory(), entry_point="/json-rpc/"
-        )
+        base_result = JsonSuccessResult(None)
+        base_result.set_jsonrpc_data(request_id=1, version="2.0", is_notification=False)
+        cls.base_result = base_result
+        cls.rpc_handler = KiwiTCMSJsonRpcHandler(entry_point="/json-rpc/")
 
     def test_html_escape(self):
         payload = "<html></html>"
-        with patch(self.base_exec_procedure, return_value=payload):
-            self.assertEqual(
-                self.rpc_handler.execute_procedure("method_name"), html.escape(payload)
-            )
+        self.base_result.data = payload
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": "%s"}' % html.escape(payload),
+        )
 
     def test_timedelta_to_seconds(self):
-        with patch(self.base_exec_procedure, return_value=timedelta(hours=1)):
-            self.assertEqual(self.rpc_handler.execute_procedure("method_name"), 3600.0)
+        self.base_result.data = timedelta(hours=1)
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": 3600.0}',
+        )
 
     def test_dict_escape(self):
-        with patch(self.base_exec_procedure, return_value={"html": "<html></html>"}):
-            self.assertDictEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                {"html": html.escape("<html></html>")},
-            )
+        self.base_result.data = {"html": "<html></html>"}
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": {"html": "%s"}}'
+            % html.escape("<html></html>"),
+        )
 
     def test_dict_with_timedelta(self):
-        with patch(
-            self.base_exec_procedure, return_value={"duration": timedelta(hours=1)}
-        ):
-            self.assertDictEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                {"duration": 3600.0},
-            )
+        self.base_result.data = {"duration": timedelta(hours=1)}
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": {"duration": 3600.0}}',
+        )
 
     def test_list_escape(self):
-        with patch(self.base_exec_procedure, return_value=["<html></html>"]):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [html.escape("<html></html>")],
-            )
-
-        with patch(self.base_exec_procedure, return_value=[{"html": "<html></html>"}]):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [{"html": html.escape("<html></html>")}],
-            )
+        self.base_result.data = ["<html></html>"]
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": ["%s"]}'
+            % html.escape("<html></html>"),
+        )
+        self.base_result.data = [{"html": "<html></html>"}]
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": [{"html": "%s"}]}'
+            % html.escape("<html></html>"),
+        )
 
     def test_list_with_timedelta(self):
-        with patch(self.base_exec_procedure, return_value=[timedelta(hours=1)]):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [3600.0],
-            )
+        self.base_result.data = [timedelta(hours=1)]
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": [3600.0]}',
+        )
 
-        with patch(
-            self.base_exec_procedure, return_value=[{"duration": timedelta(hours=1)}]
-        ):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [{"duration": 3600.0}],
-            )
+        self.base_result.data = [{"duration": timedelta(hours=1)}]
+        self.assertEqual(
+            self.rpc_handler.dumps_result(self.base_result),
+            '{"id": 1, "jsonrpc": "2.0", "result": [{"duration": 3600.0}]}',
+        )
 
 
 class TestKiwiTCMSXmlRpcHandler(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.base_exec_procedure = "modernrpc.handlers.XMLRPCHandler.execute_procedure"
-        cls.rpc_handler = KiwiTCMSXmlRpcHandler(
-            RequestFactory(), entry_point="/xml-rpc/"
-        )
+        cls.rpc_handler = KiwiTCMSXmlRpcHandler(entry_point="/xml-rpc/")
 
     def test_timedelta_to_seconds(self):
-        with patch(self.base_exec_procedure, return_value=timedelta(hours=1)):
-            self.assertEqual(self.rpc_handler.execute_procedure("method_name"), 3600.0)
+        result = XmlSuccessResult(data=timedelta(hours=1))
+        self.assertIn(
+            "<param>\n<value><double>3600.0</double></value>\n</param>",
+            self.rpc_handler.dumps_result(result),
+        )
 
     def test_dict_with_timedelta(self):
-        with patch(
-            self.base_exec_procedure, return_value={"duration": timedelta(hours=1)}
-        ):
-            self.assertDictEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                {"duration": 3600.0},
-            )
+        result = XmlSuccessResult(data={"duration": timedelta(hours=1)})
+        self.assertIn(
+            "<param>\n<value><struct>\n<member>\n<name>duration</name>\n"
+            "<value><double>3600.0</double></value>\n"
+            "</member>\n</struct></value>\n</param>",
+            self.rpc_handler.dumps_result(result),
+        )
 
     def test_list_with_timedelta(self):
-        with patch(self.base_exec_procedure, return_value=[timedelta(hours=1)]):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [3600.0],
-            )
+        result = XmlSuccessResult(data=[timedelta(hours=1)])
+        self.assertIn(
+            "<params>\n<param>\n<value><array><data>\n"
+            "<value><double>3600.0</double></value>\n"
+            "</data></array></value>\n</param>",
+            self.rpc_handler.dumps_result(result),
+        )
 
-        with patch(
-            self.base_exec_procedure, return_value=[{"duration": timedelta(hours=1)}]
-        ):
-            self.assertListEqual(
-                self.rpc_handler.execute_procedure("method_name"),
-                [{"duration": 3600.0}],
-            )
+        result = XmlSuccessResult(data=[{"duration": timedelta(hours=1)}])
+        self.assertIn(
+            "<param>\n<value><array><data>\n<value><struct>\n<member>\n<name>duration</name>\n"
+            "<value><double>3600.0</double></value>\n</member>\n</struct></value>\n"
+            "</data></array></value>\n</param>",
+            self.rpc_handler.dumps_result(result),
+        )
