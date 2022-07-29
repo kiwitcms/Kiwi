b293c57b238537a4794cb02cb4e70bdc3e265478
Alexander Todorov <atodorov@otb.bg>
2022-07-29 19:53:56 +0300

    Add bug.Severity model. Refs #2703
    Don't forget to execute ./manage.py refresh_permissions

--- tcms/bugs/models.py
+++ tcms/bugs/models.py
@@ -1,12 +1,27 @@
-# Copyright (c) 2019-2021 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2019-2022 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
+from colorfield.fields import ColorField
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from tcms.core.models.base import UrlMixin
+
+
+class Severity(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    weight = models.IntegerField(default=0)
+    icon = models.CharField(max_length=64)
+    color = ColorField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = _("Severity")
 
 
 class Bug(models.Model, UrlMixin):
@@ -29,6 +44,9 @@ class Bug(models.Model, UrlMixin):
     product = models.ForeignKey("management.Product", on_delete=models.CASCADE)
     version = models.ForeignKey("management.Version", on_delete=models.CASCADE)
     build = models.ForeignKey("management.Build", on_delete=models.CASCADE)
+    severity = models.ForeignKey(
+        Severity, on_delete=models.CASCADE, blank=True, null=True
+    )
 
     executions = models.ManyToManyField("testruns.TestExecution", related_name="bugs")
     tags = models.ManyToManyField("management.Tag", related_name="bugs")
