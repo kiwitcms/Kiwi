21f8a7a51a4320cb93d49f8cfbb4c1cbde3e2226
Alexander Todorov <atodorov@otb.bg>
2022-07-29 11:55:05 +0300

    Remove now unused overrides for SimpleMDE
    moving rendering to the backend API actually simplified the code

--- tcms/static/js/simplemde_security_override.js
+++ tcms/static/js/simplemde_security_override.js
@@ -1,47 +1,5 @@
 let markdownEditor = undefined
 
-function doHighlight (code, lang) {
-  const grammar = window.Prism.languages[lang]
-  if (grammar === undefined) {
-    // if this is a text with a newly added language the first time we try
-    // previewing highlight will fail but load the grammar anyway
-    window.Prism.plugins.autoloader.loadLanguages([lang])
-    console.error('Undefined Prism.js grammar for', lang)
-    return code
-  }
-  return window.Prism.highlight(code, grammar, lang)
-};
-
-function parseAndLoadLanguage (textarea) {
-  $(textarea).val().split('\n').forEach(function (line) {
-    if (line.indexOf('```') === 0) {
-      lang = line.trim().split('```')[1]
-
-      if (lang) {
-        window.Prism.plugins.autoloader.loadLanguages([lang])
-      }
-    }
-  })
-}
-
-$(document).ready(function () {
-  // marked.options.highlight is a synchronous operation *BUT*
-  // autoloader.loadLanguages is async and we don't have much control over them
-  if (window.Prism && window.Prism.plugins && window.Prism.plugins.autoloader) {
-    // parse all textarea's on the page and try to figure out
-    // what possible languages are there and load their grammars in advance!
-    $.find('textarea').forEach(function (textarea) {
-      parseAndLoadLanguage(textarea)
-    })
-
-    // keep parsing & trying to load languages as the user types b/c
-    // they may be entering new text from scratch
-    $('textarea').keyup(function (event) {
-      parseAndLoadLanguage($(this))
-    })
-  }
-})
-
 /*
     Override markdown rendering defaults for Simple MDE.
 
@@ -52,21 +10,7 @@ $(document).ready(function () {
     https://snyk.io/vuln/SNYK-JS-SIMPLEMDE-72570
 */
 SimpleMDE.prototype.markdown = function (text) {
-  const markedOptions = { sanitize: true }
-
-  if (this.options && this.options.renderingConfig && this.options.renderingConfig.singleLineBreaks === false) {
-    markedOptions.breaks = false
-  } else {
-    markedOptions.breaks = true
-  }
-
-  if (this.options && this.options.renderingConfig && this.options.renderingConfig.codeSyntaxHighlighting === true && window.Prism) {
-    markedOptions.highlight = doHighlight
-  }
-
-  marked.setOptions(markedOptions)
-
-  return marked(text)
+  alert('RuntimeError - markdown rendering is now backend side')
 }
 
 /*
