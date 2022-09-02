957ad1be3adf15d613bb125328a80294ea8b7f7c
Alexander Todorov <atodorov@otb.bg>
2022-09-02 22:20:29 +0300

    Add test for AnonymousViewBackend & auth. permissions

--- tcms/kiwi_auth/tests/test_backends.py
+++ tcms/kiwi_auth/tests/test_backends.py
@@ -1,6 +1,11 @@
+from http import HTTPStatus
+
 from django.contrib.auth.models import AnonymousUser
+from django.http import HttpResponseForbidden
 from django.test import TestCase, modify_settings
 from guardian.utils import get_anonymous_user
+
+from tcms.tests import LoggedInTestCase
 
 
 @modify_settings(
@@ -24,3 +29,13 @@ class TestAnonymousViewBackend(TestCase):
     def test_is_anonymous_not_has_perm(self):
         user = AnonymousUser()
         self.assertFalse(user.has_perm("testruns.add_testrun"))
+
+
+@modify_settings(
+    AUTHENTICATION_BACKENDS={"append": "tcms.kiwi_auth.backends.AnonymousViewBackend"}
+)
+class TestUserAdmin(LoggedInTestCase):
+    def test_regular_user_cant_view_list_of_all_users(self):
+        response = self.client.get("/admin/auth/user/")
+        self.assertIsInstance(response, HttpResponseForbidden)
+        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_code)
