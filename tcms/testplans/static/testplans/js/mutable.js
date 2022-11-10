da9dbf26e1539115ed9ef7975297f4eb726b05c2
Alexander Todorov <atodorov@otb.bg>
2022-11-10 22:29:19 +0200

    Remove duplicate populateProductVersion() function
    use the shared populateVersion() function instead

--- tcms/testplans/static/testplans/js/mutable.js
+++ tcms/testplans/static/testplans/js/mutable.js
@@ -7,7 +7,7 @@ $(() => {
   }
 
   if ($('#id_version').find('option').length === 0) {
-    populateProductVersion()
+    populateVersion()
   }
 
   $('#add_id_product').click(function () {
@@ -24,26 +24,10 @@ $(() => {
 
   document.getElementById('id_product').onchange = function () {
     $('#id_product').selectpicker('refresh')
-    populateProductVersion()
+    populateVersion()
   }
 
   document.getElementById('id_version').onchange = function () {
     $('#id_version').selectpicker('refresh')
   }
 })
-
-function populateProductVersion () {
-  const productId = $('#id_product').val()
-
-  if (productId === null) {
-    $('#add_id_version').addClass('disabled')
-  } else {
-    $('#add_id_version').removeClass('disabled')
-  }
-
-  const href = $('#add_id_version')[0].href
-  $('#add_id_version')[0].href = href.slice(0, href.indexOf('&product'))
-  $('#add_id_version')[0].href += `&product=${productId}`
-  $('#id_version').find('option').remove()
-  updateVersionSelectFromProduct()
-}
