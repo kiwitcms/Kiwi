f7d3ab190f86b680726fde68467d88f1a2fea7eb
Alexander Todorov <atodorov@otb.bg>
2022-11-10 22:29:19 +0200

    Remove duplicate populateProductVersion() function
    use the shared populateVersion() function instead

--- tcms/.eslintrc.js
+++ tcms/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 12
-  },
-  rules: {
-  }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {
+        indent: ['error', 4]
+    }
 }
