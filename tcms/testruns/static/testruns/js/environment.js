94b64bb633d12e5d1d9f79bb933616bbdb7b957b
Alexander Todorov <atodorov@otb.bg>
2022-11-11 23:30:54 +0200

    Merge testruns/js into one and convert .ready() handlers

--- tcms/testruns/static/testruns/js/environment.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-  if ($('#page-testruns-environment').length === 0) {
-    return
-  }
-
-  const objectId = $('#environment_pk').data('pk')
-
-  propertiesCard(objectId, 'environment', 'Environment.properties', 'Environment.add_property', 'Environment.remove_property')
-})
