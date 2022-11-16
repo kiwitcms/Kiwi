340dc1839c07fe22ac3bfbc5aa415107f8ba94de
Alexander Todorov <atodorov@otb.bg>
2022-11-16 22:07:41 +0200

    Move shared functions into utils.js
    this should have been part of da9dbf26e1539115ed9ef7975297f4eb726b05c2

--- tcms/bugs/static/bugs/js/mutable.js
+++ tcms/bugs/static/bugs/js/mutable.js
@@ -42,37 +42,3 @@ $(() => {
         populateVersion()
     }
 })
-
-// TODO: this entire file is mostly duplicate with testplans/js/mutable.js
-function populateVersion () {
-    const productId = $('#id_product').val()
-
-    if (productId === null) {
-        $('#add_id_version').addClass('disabled')
-        $('#add_id_build').addClass('disabled')
-    } else {
-        $('#add_id_version').removeClass('disabled')
-        $('#add_id_build').removeClass('disabled')
-    }
-
-    const href = $('#add_id_version')[0].href
-    $('#add_id_version')[0].href = href.slice(0, href.indexOf('&product'))
-    $('#add_id_version')[0].href += `&product=${productId}`
-    $('#id_version').find('option').remove()
-    updateVersionSelectFromProduct()
-}
-
-function populateBuild () {
-    const versionId = $('#id_version').val()
-
-    if (versionId === null) {
-        $('#add_id_build').addClass('disabled')
-    } else {
-        $('#add_id_build').removeClass('disabled')
-    }
-
-    const href = $('#add_id_build')[0].href
-    $('#add_id_build')[0].href = href.slice(0, href.indexOf('&version'))
-    $('#add_id_build')[0].href += `&version=${versionId}`
-    updateBuildSelectFromVersion()
-}
