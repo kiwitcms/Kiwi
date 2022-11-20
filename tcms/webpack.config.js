795fd9cc5ba8979de70b937e601844783d29a6ad
Alexander Todorov <atodorov@otb.bg>
2022-11-20 11:59:50 +0200

    Fix eslint warnings for webpack config file

--- tcms/webpack.config.js
+++ tcms/webpack.config.js
@@ -3,16 +3,10 @@ const path = require('path')
 module.exports = {
     mode: 'none',
     entry: [
-        './static/js/index.js',
+        './static/js/index.js'
     ],
-//    resolve: {
-//        modules: [
-//            path.join(__dirname, 'static'),
-//            'node_modules',
-//        ],
-//    },
     output: {
         path: path.join(__dirname, 'static', 'js'),
         filename: 'bundle.js'
-    },
+    }
 }
