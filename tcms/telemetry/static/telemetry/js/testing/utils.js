84eb016ecb7824d3d519f97c1df8b28a10c83538
Alexander Todorov <atodorov@otb.bg>
2022-12-28 13:36:17 +0200

    Refactor Telemetry Testing Breakdown for WebPack

--- tcms/telemetry/static/telemetry/js/testing/utils.js
+++ tcms/telemetry/static/telemetry/js/testing/utils.js
@@ -1,18 +1,13 @@
-function loadInitialProduct (callback = () => {}) {
+import { jsonRPC } from '../../../../../static/js/jsonrpc'
+import { updateSelect } from '../../../../../static/js/utils'
+
+export function loadInitialProduct (callback = () => {}) {
     jsonRPC('Product.filter', {}, data => {
         updateSelect(data, '#id_product', 'id', 'name', null)
         callback()
     })
 }
 
-function showOnlyRoundNumbers (number) {
+export function showOnlyRoundNumbers (number) {
     return number % 1 === 0 ? number : ''
 }
-
-// Close multiselect list when selecting an item
-// Iterate over all dropdown lists
-$('select[multiple]').each(function () {
-    $(this).on('change', function () {
-        $(this).parent('.bootstrap-select').removeClass('open')
-    })
-})
