13fb63d604f9836244ce0dafb84368dc27f7d2a8
Alexander Todorov <atodorov@otb.bg>
2023-04-16 13:47:00 +0300

    CSV, PDF, Print only visible columns

--- tcms/static/js/datatables_common.js
+++ tcms/static/js/datatables_common.js
@@ -1,21 +1,33 @@
 export const exportButtons = [
     {
         extend: 'csv',
+        exportOptions: {
+            columns: ':visible'
+        },
         text: '<i class="fa fa-th-list" aria-hidden="true"></i>',
         titleAttr: 'CSV'
     },
     {
         extend: 'excel',
+        exportOptions: {
+            columns: ':visible'
+        },
         text: '<i class="fa fa-file-excel-o" aria-hidden="true"></i>',
         titleAttr: 'Excel'
     },
     {
         extend: 'pdf',
+        exportOptions: {
+            columns: ':visible'
+        },
         text: '<i class="fa fa-file-pdf-o" aria-hidden="true"></i>',
         titleAttr: 'PDF'
     },
     {
         extend: 'print',
+        exportOptions: {
+            columns: ':visible'
+        },
         text: '<i class="fa fa-print" aria-hidden="true"></i>',
         titleAttr: 'Print'
     },
@@ -25,5 +37,5 @@ export const exportButtons = [
         columns: ':not(.noVis)',
         text: '<i class="fa fa-eye" aria-hidden="true"></i>',
         titleAttr: 'Column visibility'
-    },
+    }
 ]
