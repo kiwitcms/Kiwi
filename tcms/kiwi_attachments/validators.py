700e9f9b702228176b53c4262c6347229bab1858
Alexander Todorov <atodorov@otb.bg>
2023-05-30 19:04:05 +0300

    Use bleach_allowlist.generally_xss_unsafe
    when searching for potentially dangerous files.

--- tcms/kiwi_attachments/validators.py
+++ tcms/kiwi_attachments/validators.py
@@ -1,14 +1,19 @@
+from bleach_allowlist import generally_xss_unsafe
 from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 
 def deny_uploads_containing_script_tag(uploaded_file):
     for chunk in uploaded_file.chunks(2048):
-        if chunk.lower().find(b"<script") > -1:
-            raise ValidationError(_("File contains forbidden <script> tag"))
+        for tag_name in generally_xss_unsafe:
+            if chunk.lower().find(b"<" + tag_name.encode()) > -1:
+                raise ValidationError(_(f"File contains forbidden tag: <{tag_name}>"))
 
-        if chunk.lower().find(b"onload=") > -1:
-            raise ValidationError(_("File contains forbidden attribute:") + "onload")
+        for attr_name in ("onload",):
+            if chunk.lower().find(attr_name.encode() + b"=") > -1:
+                raise ValidationError(
+                    _(f"File contains forbidden attribute: `{attr_name}`")
+                )
 
 
 def deny_uploads_ending_in_dot_exe(uploaded_file):
