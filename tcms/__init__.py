2c6428607904ceee0be18824f72bfb9378d4a428
Alexander Todorov <atodorov@otb.bg>
2023-08-30 20:45:32 +0300

    Changelog for v12.6

--- tcms/__init__.py
+++ tcms/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-__version__ = "12.5"
-__release_date__ = datetime(2023, 7, 4, 16, 30)
+__version__ = "12.6"
+__release_date__ = datetime(2023, 8, 30, 21, 1)
