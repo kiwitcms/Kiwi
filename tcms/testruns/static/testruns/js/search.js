d626cb6469c2f85a775251fcfe5cb098463eded2
Alexander Todorov <atodorov@otb.bg>
2023-11-30 23:41:53 +0200

    Fix bug in search/display of Product on Search Test Run page as well
    the root cause of the bug is that the correct relationship here is
    TR -> Build -> Version -> Product which is independent of the relationship
    TP -> Product.
    
    WARNING: this is an API change which requires more patches!
    
    - Method ``TestRun.filter()`` no longer returns the field ``plan__product``
    - Method ``TestRun.filter()`` now returns the new field ``build__version__product`

--- tcms/testruns/static/testruns/js/search.js
+++ tcms/testruns/static/testruns/js/search.js
@@ -9,10 +9,10 @@ import {
 
 function preProcessData (data, callbackF) {
     const runIds = []
-    const planIds = []
+    const productIds = []
     data.forEach(function (element) {
         runIds.push(element.id)
-        planIds.push(element.plan)
+        productIds.push(element.build__version__product)
     })
 
     // get tags for all objects
@@ -29,7 +29,7 @@ function preProcessData (data, callbackF) {
             }
         })
 
-        jsonRPC('Product.filter', { plan__in: planIds }, function (products) {
+        jsonRPC('Product.filter', { pk__in: productIds }, function (products) {
             products = arrayToDict(products)
 
             // augment data set with additional info
@@ -39,8 +39,7 @@ function preProcessData (data, callbackF) {
                 } else {
                     element.tag = []
                 }
-
-                element.product_name = products[element.plan__product].name
+                element.product_name = products[element.build__version__product].name
             })
 
             callbackF({ data }) // renders everything
@@ -104,7 +103,7 @@ export function pageTestrunsSearchReadyHandler () {
             }
 
             if ($('#id_product').val()) {
-                params.plan__product = $('#id_product').val()
+                params.build__version__product = $('#id_product').val()
             };
 
             if ($('#id_version').val()) {
