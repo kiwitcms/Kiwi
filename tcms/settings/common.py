f43738d1bddb07604f478b4fcd0d72017e09fc70
Alexander Todorov <atodorov@otb.bg>
2024-02-21 17:25:01 +0200

    Configure the SECURE_SSL_REDIRECT setting to True
    except when in development mode, see
    https://docs.djangoproject.com/en/4.2/ref/settings/#secure-ssl-redirect

--- tcms/settings/common.py
+++ tcms/settings/common.py
@@ -2,6 +2,7 @@
 
 import os
 import pathlib
+import sys
 import tempfile
 from importlib import import_module
 
@@ -197,6 +198,10 @@ X_FRAME_OPTIONS = "DENY"
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#secure-content-type-nosniff
 SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Kiwi TCMS doesn't serve plain/text HTTP anymore
+# https://docs.djangoproject.com/en/4.2/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = "runserver" not in sys.argv and "test" not in sys.argv
 
 # See https://github.com/kiwitcms/Kiwi/issues/2717
 # and tcms/issuetracker/azure_boards.py
