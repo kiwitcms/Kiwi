837cf4ad9e7f872c565bb60a10ec87dec7b83970
Alexander Todorov <atodorov@otb.bg>
2024-03-07 18:52:23 +0200

    Do not execute Docker image tests as root
    - because newer versions of Firefox and Chrome can't deal with that
    - and because we shouldn't have to anyway
    
    - move beakerlib setup inside GHA because this is where it belongs

--- tests/runner.sh
+++ tests/runner.sh
@@ -6,14 +6,6 @@
 rm -rf /var/tmp/beakerlib-*/
 export BEAKERLIB_JOURNAL=0
 
-# install beakerlib from source b/c beakerlib doesn't ship
-# .deb packages
-if [ ! -f "/usr/share/beakerlib/beakerlib.sh" ]; then
-    sudo apt-get update
-    sudo apt-get install git make
-    git clone https://github.com/beakerlib/beakerlib.git
-    make -C beakerlib/ install
-fi
 
 # execute test scripts
 ./tests/test_docker.sh
