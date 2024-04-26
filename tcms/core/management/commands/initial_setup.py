85670a1e98d9e1e00e902aa8ceee985168dd1f66
Alexander Todorov <atodorov@otb.bg>
2024-04-26 12:13:13 +0300

    Automatically configure domain during initial_setup
    if the KIWI_TENANTS_DOMAIN environment variable was found

--- tcms/core/management/commands/initial_setup.py
+++ tcms/core/management/commands/initial_setup.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
@@ -14,7 +16,9 @@ class Command(BaseCommand):
         call_command("createsuperuser", f"--verbosity={kwargs['verbosity']}")
 
         self.stdout.write("\n3. Setting the domain name:")
-        domain = input("Enter Kiwi TCMS domain: ")  # nosec
+        domain = os.environ.get("KIWI_TENANTS_DOMAIN")
+        if not domain:
+            domain = input("Enter Kiwi TCMS domain: ")  # nosec
         call_command("set_domain", domain=domain)
 
         if "tcms_tenants" in settings.INSTALLED_APPS:
