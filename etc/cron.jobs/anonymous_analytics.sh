c047cb3da2f6accec5e5b993aa94d9e23e895203
Alexander Todorov <atodorov@otb.bg>
2024-06-06 15:01:23 +0300

    Record custom properties via regular page view
    which means we don't need a custom cron job

--- etc/cron.jobs/anonymous_analytics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make sure Python is enabled
-export VIRTUAL_ENV=/venv
-export PATH=/venv/bin:${PATH}
-
-# execute the actual Python script
-cat /venv/lib64/python3.11/site-packages/tcms/cron/anonymous_analytics.py | /Kiwi/manage.py shell
