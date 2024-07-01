ca102890222371c99a45641e476a86d5cbcd3186
Alexander Todorov <atodorov@otb.bg>
2024-07-01 23:33:09 +0300

    Enable EthicalAds
    - EthicalAds is a GDPR-compliant ad network for devs
    - No cookie banners, and only dev-focused ads.

--- tcms/core/middleware.py
+++ tcms/core/middleware.py
@@ -29,7 +29,7 @@ class ExtraHeadersMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if settings.DEBUG:
             response.headers["Content-Security-Policy"] = (
-                "script-src 'self' cdn.crowdin.com plausible.io;"
+                "script-src 'self' cdn.crowdin.com *.ethicalads.io plausible.io;"
             )
 
             if request.path.find("/uploads/") > -1:
