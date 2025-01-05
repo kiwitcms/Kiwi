ea4ddb1a7651bcf9313507eed89538841b049d47
Alexander Todorov <atodorov@otb.bg>
2025-01-05 13:51:37 +0200

    Explicitly define the STORAGES setting
    so we can override it later.
    
    Note that DEFAULT_FILE_STORAGE and STATICFILES_STORAGE settings
    have been deprecated since Django 4.2 and have been removed in Django 5.1!

--- tcms/settings/common.py
+++ tcms/settings/common.py
@@ -169,6 +169,17 @@ CACHES = {
     }
 }
 
+# https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-STORAGES
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
