62565d13ef09a1231c4187e701fe2f493fa74bf3
Alexander Todorov <atodorov@otb.bg>
2025-01-08 15:51:43 +0200

    Add an example performance test written with Locust
    - a base class which can login into Kiwi TCMS (keeps cookies) and
      provides a helper method for JSON-RPC API calls
    
    - an example test case which GETs the Dashboard page and
      calls the User.filter API method!

---
+++ tests/performance/base.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2025 Alexander Todorov <atodorov@otb.bg>
+#
+# Licensed under GNU Affero General Public License v3 or later (AGPLv3+)
+# https://www.gnu.org/licenses/agpl-3.0.html
+
+from locust import FastHttpUser, between, task
+from requests.utils import dict_from_cookiejar
+
+
+class LoggedInTestCase(FastHttpUser):
+    abstract = True
+
+    username = "testadmin"
+    password = "password"
+    login_url = "/accounts/login/"
+
+    def on_start(self):
+        with self.client.get(self.login_url, catch_response=True):
+            cookies = dict_from_cookiejar(self.client.cookiejar)
+            csrf_middleware_token = cookies["csrftoken"]
+
+            self.client.post(
+                self.login_url,
+                data={
+                    "username": self.username,
+                    "password": self.password,
+                    "csrfmiddlewaretoken": csrf_middleware_token,
+                },
+                headers={"Referer": self.host},
+            )
+
+    def json_rpc(self, rpc_method, rpc_args, **kwargs):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": rpc_method,
+            "params": rpc_args,
+            "id": "jsonrpc",
+        }
+        return self.client.post("/json-rpc/", json=payload, **kwargs)
+
+
+class ExampleTestCase(LoggedInTestCase):
+    wait_time = between(1, 5)
+
+    @task
+    def rpc_user_filter(self):
+        self.json_rpc("User.filter", {})
+
+    @task
+    def visit_dashboard_page(self):
+        self.client.get("/")
