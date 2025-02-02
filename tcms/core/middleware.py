72405426de6e814b7f4629462c02521acaa2315a
Alexander Todorov <atodorov@otb.bg>
2025-02-02 21:26:07 +0200

    Add Scarf.sh pixel - open source analytics & marketing intelligence

--- tcms/core/middleware.py
+++ tcms/core/middleware.py
@@ -29,7 +29,7 @@ class ExtraHeadersMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if settings.DEBUG:
             response.headers["Content-Security-Policy"] = (
-                "script-src 'self' cdn.crowdin.com *.ethicalads.io plausible.io;"
+                "script-src 'self' cdn.crowdin.com *.ethicalads.io plausible.io static.scarf.sh;"
             )
 
             if request.path.find("/uploads/") > -1:
