3e2c380f3dcfe1d2f55500082d8345d89e03f0ae
Alexander Todorov <atodorov@otb.bg>
2025-05-26 22:19:28 +0300

    Alert when no TPs are selected inside function getSelectedTestPlans()
    makes more sense to alert inside the function instead of delegating this
    to the caller. This is how selectedCheckboxes() works too and this
    function is kind of a duplicate almost!

--- tcms/testplans/static/testplans/js/search.js
+++ tcms/testplans/static/testplans/js/search.js
@@ -54,7 +54,6 @@ export function pageTestplansSearchReadyHandler () {
         action: function (e, dt, node, config) {
             const selectedTestPlans = getSelectedTestPlans()
             if (selectedTestPlans.length === 0) {
-                alert($('#main-element').data('trans-no-testplans-selected'))
                 return false
             }
 
@@ -243,6 +242,11 @@ function getSelectedTestPlans () {
         // Check if the row has collapsed children and add their IDs
         tpIds.push(...getChildRows(el))
     })
+
+    if (tpIds.length === 0) {
+        alert($('#main-element').data('trans-no-testplans-selected'))
+    }
+
     return tpIds
 }
 
