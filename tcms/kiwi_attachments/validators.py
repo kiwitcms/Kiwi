71639330cfad692ef44a5e55db76a2a481246f23
Alexander Todorov <atodorov@otb.bg>
2025-09-04 19:17:47 +0300

    Prefix harmful HTML attributes with space when scanning uploaded files
    avoids broader matches. Fixes #4074

--- tcms/kiwi_attachments/validators.py
+++ tcms/kiwi_attachments/validators.py
@@ -87,7 +87,7 @@ def deny_uploads_containing_script_tag(uploaded_file):
             "data:",
             "javascript:",
         ):
-            if chunk.lower().find(attr_name.encode()) > -1:
+            if chunk.lower().find(b" " + attr_name.encode()) > -1:
                 raise ValidationError(
                     _(f"File contains forbidden attribute: `{attr_name}`")
                 )
