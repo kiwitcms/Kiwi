40e2748b1a550293d5b2d1723b1c1278e02b11f7
Alexander Todorov <atodorov@otb.bg>
2025-11-23 22:56:18 +0200

    Add ``TestExecutionStatus.create()`` API method

--- tcms/rpc/api/testexecutionstatus.py
+++ tcms/rpc/api/testexecutionstatus.py
@@ -1,9 +1,11 @@
-# Copyright (c) 2019,2021 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2019-2025 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 2.0: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
+from django.forms.models import model_to_dict
 from modernrpc.core import rpc_method
 
+from tcms.rpc.api.forms.testrun import TestExecutionStatusForm
 from tcms.rpc.decorators import permissions_required
 from tcms.testruns.models import TestExecutionStatus
 
@@ -26,3 +28,29 @@ def filter(query):  # pylint: disable=redefined-builtin
         .values("id", "name", "weight", "icon", "color")
         .distinct()
     )
+
+
+@permissions_required("testruns.add_testexecutionstatus")
+@rpc_method(name="TestExecutionStatus.create")
+def create(values):
+    """
+    .. function:: RPC TestExecutionStatus.create(values)
+
+        Create a new TestExecutionStatus object and store it in the database.
+
+        :param values: Field values for :class:`tcms.testruns.models.TestExecutionStatus`
+        :type values: dict
+        :return: Serialized :class:`tcms.testruns.models.TestExecutionStatus` object
+        :rtype: dict
+        :raises ValueError: if input values don't validate
+        :raises PermissionDenied: if missing *testruns.add_testexecutionstatus* permission
+
+    .. versionadded:: 15.2
+    """
+    form = TestExecutionStatusForm(values)
+
+    if form.is_valid():
+        status = form.save()
+        return model_to_dict(status)
+
+    raise ValueError(list(form.errors.items()))
--- tcms/rpc/tests/test_testexecutionstatus.py
+++ tcms/rpc/tests/test_testexecutionstatus.py
@@ -3,6 +3,7 @@
 
 
 from tcms.rpc.tests.utils import APIPermissionsTestCase, APITestCase
+from tcms.testruns.models import TestExecutionStatus
 from tcms.xmlrpc_wrapper import XmlRPCFault
 
 
@@ -40,3 +41,45 @@ class TestFilterPermission(APIPermissionsTestCase):
             'Authentication failed when calling "TestExecutionStatus.filter"',
         ):
             self.rpc_client.TestExecutionStatus.filter({"weight__lt": 0})
+
+
+class TestExecutionStatusCreate(APIPermissionsTestCase):
+    permission_label = "testruns.add_testexecutionstatus"
+
+    def verify_api_with_permission(self):
+        result = self.rpc_client.TestExecutionStatus.create(
+            {
+                "name": "EMBARGOED",
+                "weight": -50,
+                "icon": "fa fa-ban",
+                "color": "#ff0f1f",
+            }
+        )
+
+        # verify the serialized result
+        self.assertIn("id", result)
+        self.assertEqual(result["name"], "EMBARGOED")
+        self.assertEqual(result["weight"], -50)
+        self.assertEqual(result["icon"], "fa fa-ban")
+        self.assertEqual(result["color"], "#ff0f1f")
+
+        # verify the object from the DB
+        status = TestExecutionStatus.objects.get(pk=result["id"])
+        self.assertEqual(status.name, result["name"])
+        self.assertEqual(status.weight, result["weight"])
+        self.assertEqual(status.icon, result["icon"])
+        self.assertEqual(status.color, result["color"])
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(
+            XmlRPCFault,
+            'Authentication failed when calling "TestExecutionStatus.create"',
+        ):
+            self.rpc_client.TestExecutionStatus.create(
+                {
+                    "name": "NOT GOOD",
+                    "weight": 0,
+                    "icon": "fa fa-meh-o",
+                    "color": "#e3dada",
+                }
+            )
