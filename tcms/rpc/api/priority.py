393cf2a97273b724513f2d63f25613f1ba51b2f5
Alexander Todorov <atodorov@otb.bg>
2025-11-23 21:30:51 +0200

    Add ``Priority.create()`` API method

--- tcms/rpc/api/priority.py
+++ tcms/rpc/api/priority.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.forms.models import model_to_dict
 from modernrpc.core import rpc_method
 
 from tcms.management.models import Priority
+from tcms.rpc.api.forms.management import PriorityForm
 from tcms.rpc.decorators import permissions_required
 
 
@@ -22,3 +24,29 @@ def filter(query):  # pylint: disable=redefined-builtin
     return list(
         Priority.objects.filter(**query).values("id", "value", "is_active").distinct()
     )
+
+
+@permissions_required("management.add_priority")
+@rpc_method(name="Priority.create")
+def create(values):
+    """
+    .. function:: RPC Priority.create(values)
+
+        Create a new Priority object and store it in the database.
+
+        :param values: Field values for :class:`tcms.management.models.Priority`
+        :type values: dict
+        :return: Serialized :class:`tcms.management.models.Priority` object
+        :rtype: dict
+        :raises ValueError: if input values don't validate
+        :raises PermissionDenied: if missing *management.add_priority* permission
+
+    .. versionadded:: 15.2
+    """
+    form = PriorityForm(values)
+
+    if form.is_valid():
+        priority = form.save()
+        return model_to_dict(priority)
+
+    raise ValueError(list(form.errors.items()))
--- tcms/rpc/tests/test_priority.py
+++ tcms/rpc/tests/test_priority.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=attribute-defined-outside-init
 
-
+from tcms.management.models import Priority
 from tcms.rpc.tests.utils import APIPermissionsTestCase, APITestCase
 from tcms.xmlrpc_wrapper import XmlRPCFault
 
@@ -33,3 +33,36 @@ class TestPriorityFilterPermissions(APIPermissionsTestCase):
             XmlRPCFault, 'Authentication failed when calling "Priority.filter"'
         ):
             self.rpc_client.Priority.filter({})
+
+
+class TestPriorityCreate(APIPermissionsTestCase):
+    permission_label = "management.add_priority"
+
+    def verify_api_with_permission(self):
+        result = self.rpc_client.Priority.create(
+            {
+                "value": "P99",
+                "is_active": True,
+            }
+        )
+
+        # verify the serialized result
+        self.assertIn("id", result)
+        self.assertEqual(result["value"], "P99")
+        self.assertEqual(result["is_active"], True)
+
+        # verify the object from the DB
+        priority = Priority.objects.get(pk=result["id"])
+        self.assertEqual(priority.value, result["value"])
+        self.assertEqual(priority.is_active, result["is_active"])
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(
+            XmlRPCFault, 'Authentication failed when calling "Priority.create"'
+        ):
+            self.rpc_client.Priority.create(
+                {
+                    "value": "P1000",
+                    "is_active": False,
+                }
+            )
