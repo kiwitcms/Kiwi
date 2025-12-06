a949e04821a03484fa290c4421189a40d7e9b595
Alexander Todorov <atodorov@otb.bg>
2025-12-06 09:40:45 +0200

    Changelog for v15.2

--- tcms/__init__.py
+++ tcms/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-__version__ = "15.1"
-__release_date__ = datetime(2025, 10, 24, 11, 30)
+__version__ = "15.2"
+__release_date__ = datetime(2025, 12, 6, 11, 30)
