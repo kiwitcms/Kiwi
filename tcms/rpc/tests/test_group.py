ea0833a511a2285700e72e99da43b928e969c021
Alexander Todorov <atodorov@otb.bg>
2025-12-16 22:24:02 +0200

    Add ``Group.permissions()`` RPC method

--- tcms/rpc/tests/test_group.py
+++ tcms/rpc/tests/test_group.py
@@ -4,9 +4,9 @@
 #
 # pylint: disable=attribute-defined-outside-init
 
-from django.contrib.auth.models import Group
-
 from tcms.rpc.tests.utils import APIPermissionsTestCase
+from tcms.tests.factories import UserFactory
+from tcms.utils.permissions import initiate_user_with_default_setups
 from tcms.xmlrpc_wrapper import XmlRPCFault
 
 
@@ -26,3 +26,35 @@ class TestGroupFilter(APIPermissionsTestCase):
             XmlRPCFault, 'Authentication failed when calling "Group.filter"'
         ):
             self.rpc_client.Group.filter({})
+
+
+class TestGroupPermissions(APIPermissionsTestCase):
+    permission_label = "auth.view_group"
+
+    @classmethod
+    def _fixture_setup(cls):
+        super()._fixture_setup()
+
+        # initialize permissions for default groups
+        initiate_user_with_default_setups(UserFactory())
+
+    def verify_api_with_permission(self):
+        result = self.rpc_client.Group.permissions("Tester")
+        self.assertGreater(len(result), 0)
+
+        self.assertIn("attachments.view_attachment", result)
+        self.assertIn("management.add_product", result)
+        self.assertIn("testcases.change_testcase", result)
+        self.assertIn("testplans.delete_testplan", result)
+
+    def verify_api_without_permission(self):
+        with self.assertRaisesRegex(
+            XmlRPCFault, 'Authentication failed when calling "Group.permissions"'
+        ):
+            self.rpc_client.Group.permissions("Administrator")
+
+
+class TestGroupPermissionsWithNonExistentGroup(TestGroupPermissions):
+    def verify_api_with_permission(self):
+        with self.assertRaisesRegex(XmlRPCFault, "Group matching query does not exist"):
+            self.rpc_client.Group.permissions("NonExistentGroup")
