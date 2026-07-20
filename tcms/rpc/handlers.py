11e3200f25aa8904e50539e35ce6f671354e0097
Alexander Todorov <atodorov@otb.bg>
2026-07-20 12:47:44 +0000

    Restrict field arguments for RPC calls

--- tcms/rpc/handlers.py
+++ tcms/rpc/handlers.py
@@ -1,6 +1,7 @@
 import html
 from datetime import timedelta
 
+from modernrpc.exceptions import RPCInvalidRequest
 from modernrpc.jsonrpc.handler import JsonRpcHandler
 from modernrpc.xmlrpc.handler import XmlRpcHandler
 
@@ -27,6 +28,40 @@ class KiwiTCMSHandlerMixin:
         else:
             return __class__.escape_value(value)
         return value
+
+    @staticmethod
+    def inspect_rpc_args(rpc_request):
+        restricted_fields = (
+            "password",
+            "token",
+            "secret",
+            "activation_key",
+            "api_key",
+            "apikey",
+        )
+        keys_to_check = []
+        for arg in rpc_request.args:
+            if isinstance(arg, dict):
+                keys_to_check.extend(arg.keys())
+        kwargs = getattr(rpc_request, "kwargs", None)
+        if isinstance(kwargs, dict):
+            keys_to_check.extend(kwargs.keys())
+
+        for key in keys_to_check:
+            key_str = str(key).lower()
+            if any(
+                key_str == field or key_str.startswith(f"{field}__")
+                for field in restricted_fields
+            ):
+                raise RPCInvalidRequest("Unsupported field")
+
+    def process_single_request(self, rpc_request, context):
+        self.inspect_rpc_args(rpc_request)
+        return super().process_single_request(rpc_request, context)
+
+    async def aprocess_single_request(self, rpc_request, context):
+        self.inspect_rpc_args(rpc_request)
+        return await super().aprocess_single_request(rpc_request, context)
 
     def build_success_result(self, request, data):
         data = __class__.escape(data)
