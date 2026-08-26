cecd2a5ffa5e59a0678566e991c07b4183a27dd7
Alexander Todorov <atodorov@otb.bg>
2026-08-26 16:29:08 +0300

    Add --database option to initial_setup & set_domain commands

--- tcms/core/management/commands/initial_setup.py
+++ tcms/core/management/commands/initial_setup.py
@@ -3,10 +3,19 @@ import os
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+from django.db import DEFAULT_DB_ALIAS, connections
 
 
 class Command(BaseCommand):
     help = "Performs initial setup of *NEW* Kiwi TCMS installation."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--database",
+            default=DEFAULT_DB_ALIAS,
+            choices=tuple(connections),
+            help='Specifies the database to use. Default is "default".',
+        )
 
     def handle(self, *args, **kwargs):
         self.stdout.write("\n1. Applying migrations:")
